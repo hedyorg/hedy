af625cb560b9fddae26f04eebc41c23eddfe9002
Felienne <felienne@gmail.com>
2020-02-17 17:08:20 +0100

    ruzie met JSON, altijd leuk :/

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import hedy
+import json
 
 # app.py
 from flask import Flask, request, jsonify, render_template
@@ -19,10 +20,26 @@ def respond():
         response["Error"] = "no code found, please send code."
     # is so, parse
     else:
-        response["Code"] = hedy.transpile(lines)
+        try:
+            result = hedy.transpile(lines)
+            response["Code"] = result
+        except:
+            print(f"error transpiling {lines}")
 
     # Return the response in json format
-    return jsonify(response)
+    # origineel gebruikte we de eerste dat werkt opeens niet meer om
+    # onduidelijke redenen! Het heeft vermoedelijk iets met strings en "" te maken
+    # json_version_2 = jsonify(response) <- dit was het origineel
+    # ook direct dumpen doet het niet
+    # json_version = json.dump(response) <- ook foutmelding!
+    # dus maken we er maar even met de hand geldige JSON van met str() Niet echt ideaal zo!!
+
+    # er is trouwens niet eens perse JSON nodig we kunnen ook wel een string met
+    # code passen
+
+    str_versie = str(response)
+    str_versie = str_versie.replace("'Code'",'"Code"')
+    return str_versie
 
 
 # @app.route('/post/', methods=['POST'])
--- tests.py
+++ tests.py
@@ -1,23 +1,37 @@
 import unittest
 import hedy
+import json
 
 class TestLevel1(unittest.TestCase):
 
-    def test_print_1(self):
+    def test_parse_print_1(self):
         parser_level_1 = hedy.create_parser_level_1()
         tree = parser_level_1.parse("print felienne 123")
         self.assertEqual(tree.data, 'print')
 
-    def test_ask_1(self):
+    def test_parse_ask_1(self):
         parser_level_1 = hedy.create_parser_level_1()
         tree = parser_level_1.parse("ask felienne 123")
         self.assertEqual(tree.data, 'ask')
 
-    def test_other_1(self):
+    def test_parse_other_1(self):
         parser_level_1 = hedy.create_parser_level_1()
         tree = parser_level_1.parse("abc felienne 123")
         self.assertEqual(tree.data, 'invalid')
 
+    def test_transpile_print_1(self):
+        result = hedy.transpile("print Hallo welkom bij Hedy")
+        self.assertEqual(result, "print('Hallo welkom bij Hedy')")
+
+    def json_tester(self):
+        response = {}
+        response["Code"]= "print(\\'Hallo wereld\\')"
+
+        # convert into JSON:
+        y = json.dumps(response)
+
+        # the result is a JSON string:
+        self.assertEqual(y, 'test')
 
 if __name__ == '__main__':
     unittest.main()
