ad17dcef2dc0d93cd4bfa81321c3b95cd33b4e38
Rico Huijbers <rix0rrr@gmail.com>
2020-02-29 20:26:42 +0100

    Make CSS smaller

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,7 +1,27 @@
+// Control the Tailwind file size by disabling stuff we don't need.
 module.exports = {
   theme: {
     extend: {},
+    screens: {
+      // We only need a few breakpoints
+      sm: '640px',
+      // md: '768px',
+    },
   },
   variants: {},
+  corePlugins: {
+    opacity: false,
+    // No animations
+    transitionDuration: false,
+    transitionProperty: false,
+    transitionTimingFunction: false,
+
+    // No fancy CSS graphics
+    scale: false,
+    rotate: false,
+    translate: false,
+    skew: false,
+    transformOrigin: false,
+  },
   plugins: [],
 }
