5d776587fbce4195d81fab422a0b24227cdc8fef
Rico Huijbers <rix0rrr@gmail.com>
2020-06-14 12:31:49 +0200

    Remove trajectories, replace with levels/steps
    Steps are alternatingly called "steps" and "assignment"
    in the code.

--- courses.py
+++ courses.py
@@ -5,6 +5,7 @@ import yaml
 
 import docs
 
+
 class LevelDefaults:
   def __init__(self, language):
     self.language = language
@@ -15,13 +16,12 @@ class LevelDefaults:
 
     Returns: Default
     """
-    level_data = copy.deepcopy(self.levels.get(int(level), {}))
-    level_data['commands'] = [Command(**c) for c in level_data.get('commands', [])]
-    return Defaults(**level_data)
+    return copy.deepcopy(self.levels.get(int(level), {}))
+
 
 class NoSuchDefaults:
   def get_defaults(self, level):
-    return Defaults()
+    return {}
 
 
 class Course:
@@ -29,44 +29,77 @@ class Course:
     self.course_name = course_name
     self.language = language
     self.defaults = defaults
-    self.course = load_yaml(f'coursedata/course/{course_name}/{language}.yaml')
+    self.course = load_yaml(f'coursedata/course/{course_name}/{language}.yaml').get('course')
+    if not self.course:
+      raise RuntimeError(f'File should have top-level "course" field: coursedata/course/{course_name}/{language}.yaml')
+
+    self.validate_course()
 
     self.docs = docs.DocCollection(keys=['level', 'slug'], synth={
       'slug': lambda d: docs.slugify(d.front_matter.get('title', None))
     })
     self.docs.load_dir(f'coursedata/course/{course_name}/docs-{language}')
 
-  def max_level(self, trajectory='default'):
-    return len(self.course.get('trajectories', {}).get(trajectory, []))
+  def max_level(self):
+    return len(self.course)
 
-  def get_assignment(self, number, trajectory='default'):
-    """Return the 1-based Assignment from the given trajectory of this course."""
-    assignments = self.course.get('trajectories', {}).get(trajectory, [])
-    index = int(number) - 1
-    if index >= len(assignments): return None
+  def max_step(self, level):
+    level_ix = int(level) - 1
+    if level_ix >= len(self.course): return 0
+    return len(self.course[level_ix].get('assignments', []))
 
-    level = assignments[index]['level']
-    assignment = attr.asdict(self.defaults.get_defaults(int(level)))
-    assignment.update(**assignments[index])
+  def get_assignment(self, level, number):
+    """Return the 1-based Assignment from this course."""
+    level_ix = int(level) - 1
+    if level_ix >= len(self.course): return None
+    assignments = self.course[level_ix].get('assignments')
 
-    assignment['commands'] = [Command(**c) for c in assignment.get('commands', [])]
+    assignment_values = {
+      "level": str(level),
+    }
+    assignment_values.update(**self.defaults.get_defaults(int(level)))
 
-    assignment['docs'] = [Doc(slug=slug, title=doc.front_matter.get('title'))
-                          for slug, doc in self.docs.get(number).items()]
+    # If we don't have any "assignments", return a default Assignment object
+    # based off the level and the level defaults. This is used in the Hedy main
+    # course.
+    #
+    # Otherwise, if this is a course that DOES have assignments, validate and
+    # load the data into the accumulator object.
+    if assignments:
+      step_ix = int(number) - 1
+      if step_ix >= len(assignments): return None
+      assignment_values.update(**assignments[step_ix])
 
-    return Assignment(**assignment)
+    assignment_values['commands'] = [Command(**c) for c in assignment_values.get('commands', [])]
+
+    assignment_values['docs'] = [Doc(slug=slug, title=doc.front_matter.get('title'))
+                          for slug, doc in self.docs.get(level).items()]
+
+    return Assignment(**assignment_values)
+
+  def validate_course(self):
+    """Check that the 'level' and 'step' fields have the right number in the entire course.
+
+    This information is redundant, but it helps the human locate their way in the YAML file.
+    """
+    for level_i, level in enumerate(self.course):
+      expected_value = str(level_i + 1)
+      actual_value = level.get('level')
+      if expected_value != actual_value:
+        raise RuntimeError(f'Expected \'level: "{expected_value}"\' but found "{actual_value}" in {self.course_name}-{self.language}')
+
+      assignments = level.get('assignments')
+      if assignments:
+        for ass_i, ass in enumerate(assignments):
+          expected_ass = str(ass_i + 1)
+          actual_ass = ass.get('step')
+          if expected_value != actual_value:
+            raise RuntimeError(f'Expected \'step: "{expected_ass}"\' but got "{actual_ass}" in {self.course_name}-{self.language}')
 
 
 class NoSuchCourse:
-  def get_assignment(self, number, trajectory='default'):
+  def get_assignment(self, level, number):
     return None
-
-
-@attr.s(slots=True, frozen=True)
-class Defaults:
-  intro_text = attr.ib(default='')
-  start_code = attr.ib(default='')
-  commands = attr.ib(default=list)
 
 
 @attr.s(slots=True, frozen=True)
@@ -79,6 +112,11 @@ class Command:
 
 @attr.s(slots=True, frozen=True)
 class Assignment:
+  """A single assignment.
+
+  Either a concrete assignment written in the YAML, or a default-generated
+  Assignment.
+  """
   level = attr.ib()
   prompt = attr.ib(default='')
   intro_text = attr.ib(default=None)
