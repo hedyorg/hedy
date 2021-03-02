0afe8bcce39dc0dcb999ef1d02871f3f76153219
Felienne <felienne@gmail.com>
2021-03-02 09:54:45 +0100

    only translates chars in a parse error

--- app.py
+++ app.py
@@ -122,11 +122,15 @@ def parse():
                 error_template = hedy_errors[E.error_code]
                 response["Code"] = "# coding=utf8\n" + E.arguments['fixed_code']
                 response["Warning"] = error_template.format(**E.arguments)
-            else:
+            elif E.args[0] == "Parse":
                 error_template = hedy_errors[E.error_code]
+                # Localize the names of characters
                 # Localize the names of characters
                 if 'character_found' in E.arguments:
                     E.arguments['character_found'] = hedy_errors[E.arguments['character_found']]
+                response["Error"] = error_template.format(**E.arguments)
+            else:
+                error_template = hedy_errors[E.error_code]
                 response["Error"] = error_template.format(**E.arguments)
         except Exception as E:
             print(f"error transpiling {code}")
