eb07183be7997d1a9ca4e45149436cfa4872a0e8
omedtjeee <omed_nasrat@hotmail.com>
2021-04-28 14:22:55 +0200

    lvl 2

--- static/js/syntaxModesRules.js
+++ static/js/syntaxModesRules.js
@@ -62,3 +62,85 @@ define('ace/mode/level1_highlight_rules', [], function(require, exports, module)
   exports.ExampleHighlightRules = ExampleHighlightRules;
 
 });
+
+ //  MODE LEVEL 2 SETUP
+define('ace/mode/level2', [], function(require, exports, module) {
+
+  var oop = require("ace/lib/oop");
+  var TextMode = require("ace/mode/text").Mode;
+  var Tokenizer = require("ace/tokenizer").Tokenizer;
+  var ExampleHighlightRules = require("ace/mode/level2_highlight_rules").ExampleHighlightRules;
+
+  var Mode = function() {
+    this.HighlightRules = ExampleHighlightRules;
+  };
+  oop.inherits(Mode, TextMode);
+
+  (function() {
+    this.lineCommentStart = "#";
+
+    this.createWorker = function(session) {
+        var worker = new WorkerClient(["ace"], "ace/mode/mynew_worker", "NewWorker");
+        worker.attachToDocument(session.getDocument());
+        worker.on("errors", function(e) {
+            session.setAnnotations(e.data);
+        });
+        return worker;
+    };
+
+  }).call(Mode.prototype);
+
+  exports.Mode = Mode;
+});
+
+
+ //  Syntax rules level 2
+define('ace/mode/level2_highlight_rules', [], function(require, exports, module) {
+  var oop = require("ace/lib/oop");
+  var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+
+  var ExampleHighlightRules = function() {
+
+    this.$rules = {
+
+      "start": [{
+        token: "keyword",
+        regex: "^print ",
+        next: "print rest"
+      },{
+        token: "keyword",
+        regex: " is ask ",
+        next: "rest"
+      },{
+        token: "keyword",
+        regex: " is ",
+        next: "rest"
+      }],
+
+      "print rest": [{
+        token: "keyword",
+        regex: " at random | at random$",
+      },{
+        token: "text",
+        regex: "$",
+        next: "start"
+      },{
+        defaultToken : "text"
+      }],
+
+      "rest": [{
+        token: "text",
+        regex: "$",
+        next: "start"
+      },{
+        defaultToken : "text"
+      }]
+    };
+    this.normalizeRules();
+  };
+
+  oop.inherits(ExampleHighlightRules, TextHighlightRules);
+
+  exports.ExampleHighlightRules = ExampleHighlightRules;
+
+});
