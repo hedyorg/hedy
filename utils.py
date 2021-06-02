cc1535cf0ba23d4b216b745af30ae01d07bbb558
Federico Pereiro <fpereiro@gmail.com>
2021-06-02 15:19:07 +0200

    Refactor db functions.

--- utils.py
+++ utils.py
@@ -125,24 +125,23 @@ db = boto3.client ('dynamodb', region_name = config ['dynamodb'] ['region'], aws
 db_prefix = os.getenv ('AWS_DYNAMODB_TABLE_PREFIX')
 
 # Encode a dict so that it has the format expected by DynamoDB
-def db_encode (data, *args):
-    # args may contain a boolean flag which we use to detect whether we should format the payload for update_item
-    # when len (args) is truthy, we know we're dealing with update_item
+def db_encode (data, update=False):
+    # update is a boolean flag which we use to detect whether we should format the payload for update_item
     processed_data = {}
     for key in data:
         if type_check (data [key], 'str'):
-            if len (args):
+            if update:
                 processed_data [key] = {'Value': {'S': data [key]}}
             else:
                 processed_data [key] = {'S': data [key]}
         elif type_check (data [key], 'int'):
             # Note we convert the value into a string
-            if len (args):
+            if update:
                 processed_data [key] = {'Value': {'N': str (data [key])}}
             else:
                 processed_data [key] = {'N': str (data [key])}
         elif data [key] == None:
-            if len (args):
+            if update:
                 processed_data [key] = {'Action': 'DELETE'}
             else:
                 processed_data [key] = {'NULL': True}
@@ -164,30 +163,30 @@ def db_decode (data):
             raise ValueError ('Unsupported type passed to db_put')
     return processed_data
 
+db_main_indexes = {
+   'users': 'username',
+   'tokens': 'id',
+   'programs': 'id'
+}
+
 # This function takes a dict `data` and returns a new dict with only the key/value for the index key for the table.
-# If *remove is truthy, then the index key is removed instead, leaving the rest of the keys intact.
-def db_key (table, data, *remove):
+# If remove is truthy, then the index key is removed instead, leaving the rest of the keys intact.
+def db_key (table, data, remove=False):
     processed_data = {}
-    if len (remove):
+    if remove:
         for key in data:
-            if table == 'users' and key == 'username':
-                continue
-            if (table == 'tokens' or table == 'programs') and key == 'id':
-                continue
-            processed_data [key] = data [key]
+            if key != db_main_indexes [table]:
+                processed_data [key] = data [key]
     else:
-        if table == 'users':
-            processed_data ['username'] = data ['username']
-        if table == 'tokens' or table == 'programs':
-            processed_data ['id'] = data ['id']
+        processed_data [db_main_indexes [table]] = data [db_main_indexes [table]]
     return processed_data
 
 # Gets an item by index from the database. If not_primary is truthy, the search is done by a field that should be set as a secondary index.
 @querylog.timed
-def db_get (table, data, *not_primary):
+def db_get (table, data, not_primary=False):
     querylog.log_counter('db_get:' + table)
     # If we're querying by something else than the primary key of the table, we assume that data contains only one field, that on which we want to search. We also require that field to have an index.
-    if len (not_primary):
+    if not_primary:
         field = list (data.keys ()) [0]
         result = db.query (TableName = db_prefix + '-' + table, IndexName = field + '-index', KeyConditionExpression = field + ' = :value', ExpressionAttributeValues = {':value': {'S': data [field]}})
         if len (result ['Items']):
@@ -202,9 +201,9 @@ def db_get (table, data, *not_primary):
 
 # Gets an item by index from the database. If not_primary is truthy, the search is done by a field that should be set as a secondary index.
 @querylog.timed
-def db_get_many (table, data, *not_primary):
+def db_get_many (table, data, not_primary=False):
     querylog.log_counter('db_get_many:' + table)
-    if len (not_primary):
+    if not_primary:
         field = list (data.keys ()) [0]
         # We use ScanIndexForward = False to get the latest items from the Programs table
         result = db.query (TableName = db_prefix + '-' + table, IndexName = field + '-index', KeyConditionExpression = field + ' = :value', ExpressionAttributeValues = {':value': {'S': data [field]}}, ScanIndexForward = False)
@@ -236,11 +235,11 @@ def db_del (table, data):
 
 # Deletes multiple items.
 @querylog.timed
-def db_del_many (table, data, *not_primary):
+def db_del_many (table, data, not_primary=False):
     querylog.log_counter('db_del_many:' + table)
     # We define a recursive function in case the number of results is very large and cannot be returned with a single call to db_get_many.
     def batch ():
-        to_delete = db_get_many (table, data, *not_primary)
+        to_delete = db_get_many (table, data, not_primary)
         if len (to_delete) == 0:
             return
         for item in to_delete:
