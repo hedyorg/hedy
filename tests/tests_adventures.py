4600f7a8ce6a60aa5e951bec345909198f4d7e21
Federico Pereiro <fpereiro@gmail.com>
2021-09-07 15:58:11 +0200

    Proper markdown -> HTML parsing; report all errors for all files; print errors as they are found; use default directory for running tests.

--- tests/tests_adventures.py
+++ tests/tests_adventures.py
@@ -4,47 +4,52 @@ import utils
 import hedy
 import unittest
 
+# Set the current directory to the root Hedy folder
+os.chdir(os.path.join (os.getcwd (), __file__.replace (os.path.basename (__file__), '')))
+
 path = '../coursedata/adventures'
 files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith ('.yaml')]
 
+def check_code(f, level, field_name, code):
+    # We ignore empty code snippets
+    if len(code) == 0:
+        return True
+    try:
+        hedy.transpile(code, int(level))
+    except Exception as E:
+        if E.args[0] != 'Has Blanks':  # code with blanks is ok!
+            error = 'Invalid ' + field_name + ' in file ' + f + ', level #' + str(level) + ' - Error: ' + E.args [0] + ' - code: ' + code
+            # We print the error for readability, since otherwise they get accumulated on a long list
+            print (error)
+            return error
+    return True
 
-class TestsLevel1(unittest.TestCase):
-  level = 1
+class TestsAdventurePrograms(unittest.TestCase):
 
-  def check_code(self, f, level, field_name, code):
-      # We ignore empty code snippets
-      if len(code) == 0:
-          return True
-      try:
-          hedy.transpile(code, int(level))
-      except Exception as E:
-          if E.args[0] != 'Has Blanks':  # code with blanks is ok!
-              return 'Invalid ' + field_name + ' in file ' + f + ', level ' + str(level) + code
+    def test_adventure_snippets(self):
+        adventure_fails = []
 
-      return True
+        for f in files:
+            f = os.path.join (path, f)
+            yaml = utils.load_yaml_uncached (f)
 
-  def test_adventure_snippets(self):
-    for f in files:
-      f = os.path.join (path, f)
-      yaml = utils.load_yaml_uncached (f)
+            for adventure in yaml ['adventures'].values ():
+                for level_number in adventure ['levels']:
+                    level = adventure ['levels'] [level_number]
 
-      adventure_fails = []
+                    # start_code
+                    result = check_code (f, level_number, 'start_code', level ['start_code'])
+                    if result != True:
+                        adventure_fails.append(result)
 
-      for adventure in yaml ['adventures']:
-        for level in yaml ['adventures'] [adventure] ['levels']:
+                    code_snippet_counter = 0
+                    # code snippets inside story_text
+                    for tag in utils.markdown_to_html_tags (level ['story_text']):
+                        if tag.name != 'pre' or not tag.contents [0]:
+                            continue
+                        code_snippet_counter += 1
+                        result = check_code (f, level_number, 'story_text code snippet #' + str (code_snippet_counter), tag.contents [0].contents [0])
+                        if result != True:
+                            adventure_fails.append(result)
 
-          result = self.check_code (f, level, 'start_code', yaml ['adventures'] [adventure] ['levels'] [level] ['start_code'])
-          if result != True:
-            adventure_fails.append(result)
-
-          # story_text (between triple backticks)
-          splitted_story_text = re.split (r'\s*```\n', yaml ['adventures'] [adventure] ['levels'] [level] ['story_text'])
-          for k, part in enumerate (splitted_story_text):
-            if k % 2 == 0:
-              continue
-            # Only the odd parts of the text are code snippets
-            result = self.check_code (f, level, 'story_text code snippet #' + str (int ((k - 1) / 2) + 1), part)
-            if result != True:
-              adventure_fails.append(result)
-
-    self.assertEqual([],adventure_fails)
+        self.assertEqual([],adventure_fails)
--- tests/tests_level_defaults.py
+++ tests/tests_level_defaults.py
@@ -2,33 +2,56 @@ import os
 import re
 import utils
 import hedy
+import unittest
+
+# Set the current directory to the root Hedy folder
+os.chdir(os.path.join (os.getcwd (), __file__.replace (os.path.basename (__file__), '')))
 
 path = '../coursedata/level-defaults'
 files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith ('.yaml')]
 
-def test_code (f, level, field_name, code):
+def check_code(f, level, field_name, code):
     # We ignore empty code snippets
-    if len (code) == 0:
-        return
+    if len(code) == 0:
+        return True
     try:
-        hedy.transpile (code, int (level))
-    except:
-        print ('Invalid ' + field_name + ' in file ' + f + ', level ' + str (level), code + '\n\n')
+        hedy.transpile(code, int(level))
+    except Exception as E:
+        if E.args[0] != 'Has Blanks':  # code with blanks is ok!
+            error = 'Invalid ' + field_name + ' in file ' + f + ', level #' + str(level) + ' - Error: ' + E.args [0] + ' - code: ' + code
+            # We print the error for readability, since otherwise they get accumulated on a long list
+            print (error)
+            return error
+    return True
 
-for f in files:
-    f = os.path.join (path, f)
-    yaml = utils.load_yaml_uncached (f)
-    for level in yaml:
-        # start_code
-        test_code (f, level, 'start_code', yaml [level] ['start_code'])
-        # commands.k.demo_code
-        for k, command in enumerate (yaml [level] ['commands']):
-            test_code (f, level, 'command #' + str (k + 1) + ' demo_code', command ['demo_code'])
+class TestsLevelDefaultsPrograms(unittest.TestCase):
 
-        # intro_text (between triple backticks)
-        splitted_intro = re.split (r'\s*```\n', yaml [level] ['intro_text'])
-        for k, part in enumerate (splitted_intro):
-            if k % 2 == 0:
-                continue
-            # Only the odd parts of the text are code snippets
-            test_code (f, level, 'intro_text code snippet #' + str ((k - 1) / 2), part)
+    def test_level_defaults_snippets(self):
+        level_default_fails = []
+
+        for f in files:
+            f = os.path.join (path, f)
+            yaml = utils.load_yaml_uncached (f)
+
+            for level in yaml:
+                # start_code
+                result = check_code (f, level, 'start_code', yaml [level] ['start_code'])
+                if result != True:
+                    level_default_fails.append(result)
+                # commands.k.demo_code
+                for k, command in enumerate (yaml [level] ['commands']):
+                    result = check_code (f, level, 'command #' + str (k + 1) + ' demo_code', command ['demo_code'])
+                    if result != True:
+                        level_default_fails.append(result)
+
+                # code snippets inside intro_text
+                code_snippet_counter = 0
+                for tag in utils.markdown_to_html_tags (yaml [level] ['intro_text']):
+                    if tag.name != 'pre' or not tag.contents [0]:
+                        continue
+                    code_snippet_counter += 1
+                    result = check_code (f, level, 'intro_text snippet #' + str (code_snippet_counter), tag.contents [0].contents [0])
+                    if result != True:
+                        level_default_fails.append(result)
+
+        self.assertEqual([],level_default_fails)
