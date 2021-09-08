6719dcdfd86b86c58304be5a76ccc553508ea80f
Felienne <felienne@gmail.com>
2021-09-08 14:45:46 +0200

    Fixes to default texts

--- tests/tests_defaults.py
+++ tests/tests_defaults.py
@@ -10,7 +10,7 @@ os.chdir(os.path.join (os.getcwd (), __file__.replace (os.path.basename (__file_
 path = '../coursedata/level-defaults'
 files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith ('.yaml')]
 
-def check_code(f, level, field_name, code):
+def check_code(filename, level, field_name, code):
     # We ignore empty code snippets
     if len(code) == 0:
         return True
@@ -18,7 +18,9 @@ def check_code(f, level, field_name, code):
         hedy.transpile(code, int(level))
     except Exception as E:
         if E.args[0] != 'Has Blanks':  # code with blanks is ok!
-            error = 'Invalid ' + field_name + ' in file ' + f + ', level #' + str(level) + ' - Error: ' + E.args [0] + ' - code: ' + code
+            filename_shorter = filename.split("/")[3]
+            language = filename_shorter.split(".")[0]
+            error = f'{language}: level #{level} - {field_name}. Error: {E.args[0]}'
             # We print the error for readability, since otherwise they get accumulated on a long list
             print (error)
             return error
@@ -29,18 +31,19 @@ class TestsLevelDefaultsPrograms(unittest.TestCase):
     def test_level_defaults_snippets(self):
         level_default_fails = []
 
-        for f in files:
-            f = os.path.join (path, f)
-            yaml = utils.load_yaml_uncached (f)
+        for file in files:
+            file = os.path.join (path, file)
+            yaml = utils.load_yaml_uncached (file)
 
             for level in yaml:
                 # start_code
-                result = check_code (f, level, 'start_code', yaml [level] ['start_code'])
+                result = check_code(file, level, 'start_code', yaml [level] ['start_code'])
                 if result != True:
                     level_default_fails.append(result)
                 # commands.k.demo_code
-                for k, command in enumerate (yaml [level] ['commands']):
-                    result = check_code (f, level, 'command #' + str (k + 1) + ' demo_code', command ['demo_code'])
+                for k, command in enumerate(yaml[level]['commands']):
+                    command_text_short = command['explanation'][0:10]
+                    result = check_code(file, level, 'command ' + command_text_short + ' demo_code', command['demo_code'])
                     if result != True:
                         level_default_fails.append(result)
 
@@ -50,8 +53,8 @@ class TestsLevelDefaultsPrograms(unittest.TestCase):
                     if tag.name != 'pre' or not tag.contents [0]:
                         continue
                     code_snippet_counter += 1
-                    result = check_code (f, level, 'intro_text snippet #' + str (code_snippet_counter), tag.contents [0].contents [0])
+                    result = check_code(file, level, 'intro_text snippet #' + str (code_snippet_counter), tag.contents [0].contents [0])
                     if result != True:
                         level_default_fails.append(result)
 
-        self.assertEqual([],level_default_fails)
+        self.assertEqual(0, len(level_default_fails))
