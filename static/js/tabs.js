e28a45fc3317e9095e8a8f53586fe34bd266952a
Federico Pereiro <fpereiro@gmail.com>
2021-09-26 11:38:13 +0200

    Do not hide blue top bar when showing quiz tab, only the name input & buttons.

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -39,12 +39,12 @@ $(function() {
     });
 
     if (tabName === 'end') {
+      $ ('#level-header input').hide ();
       $ ('#editor-area').hide ();
-      $ ('#level-header').hide ();
       return;
     }
+    $ ('#level-header input').show ();
     $ ('#editor-area').show ();
-    $ ('#level-header').show ();
     // If the loaded program (directly requested by link with id) matches the currently selected tab, use that, overriding the loaded program that came in the adventure or level.
     if (window.State.loaded_program && (window.State.adventure_name_onload || 'level') === tabName) {
       $ ('#program_name').val (window.State.loaded_program.name);
