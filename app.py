3ee401f614b48c3c7a0db51fe1d9aec1d65c5291
fpereiro <fpereiro@gmail.com>
2021-09-27 10:49:45 +0200

    Quiz tab (#810)
    * First approximation to quizzes in tabs.
    
    * Maintain iframes but clean up X-Frame header setting and templates to be used inside the iframe.
    
    * Add required field to yaml
    
    * Do not hide blue top bar when showing quiz tab, only the name input & buttons.
    
    * Unorthodox CSS for quiz iframe.
    
    * updates nl.yaml for the quiz

--- app.py
+++ app.py
@@ -102,6 +102,10 @@ def load_adventures_per_level(lang, level):
     for short_name, adventure in adventures.items ():
         if not level in adventure['levels']:
             continue
+        # end adventure is the quiz
+        # if quizzes are not enabled, do not load it
+        if short_name == 'end' and not config['quiz-enabled']:
+            continue
         all_adventures.append({
             'short_name': short_name,
             'name': adventure['name'],
@@ -217,7 +221,7 @@ def after_request_log_status(response):
 def set_security_headers(response):
     security_headers = {
         'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
-        'X-Frame-Options': 'DENY',
+        'X-Frame-Options': None if re.match ('.*/quiz', request.url) else 'DENY',
         'X-XSS-Protection': '1; mode=block',
     }
     response.headers.update(security_headers)
@@ -495,7 +499,7 @@ def get_quiz(level_source, question_nr, attempt):
                                    is_teacher=is_teacher(request),
                                    auth=TRANSLATIONS.get_translations (requested_lang(), 'Auth'))
 
-@app.route('/submit_answer/<level_source>/<question_nr>/<attempt>', methods=["POST"])
+@app.route('/quiz/submit_answer/<level_source>/<question_nr>/<attempt>', methods=["POST"])
 def submit_answer(level_source, question_nr, attempt):
     if not config.get('quiz-enabled') and g.lang != 'nl':
         return 'Hedy quiz disabled!', 404
