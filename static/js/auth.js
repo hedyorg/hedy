e28ad49577c49be800cc33f9840349f7e54fe72e
TiBiBa <timonbakker@hotmail.com>
2021-10-12 18:37:25 +0200

    Improved option selection and fixed country default value to always be in english

--- static/js/auth.js
+++ static/js/auth.js
@@ -261,7 +261,7 @@ window.auth = {
 // *** LOADERS ***
 
 if ($ ('#country')) {
-  var html = '<option value="" selected disabled hidden>Select</option>';
+  var html = $ ('#country').html();
   Object.keys (countries).map (function (code) {
     html += '<option value="' + code + '">' + countries [code] + '</option>';
   });
