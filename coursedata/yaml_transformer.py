0446bf528892f9da54621ea48f88e0b9038c8483
Felienne <felienne@gmail.com>
2021-11-15 09:56:03 +0100

    updates content to make space for new level 12

--- coursedata/yaml_transformer.py
+++ coursedata/yaml_transformer.py
@@ -64,12 +64,12 @@ def transform_adventures(old_level, new_level=None, function=nop):
 def transform_levels_in_all_YAMLs(old_level, new_level=None, function=nop):
   # Set the current directory to the root Hedy folder
   os.chdir(os.path.join(os.getcwd(), __file__.replace(os.path.basename(__file__), '')))
-  transform_level_defaults(old_level, new_level, function)
+  # transform_level_defaults(old_level, new_level, function)
   # WARNING: adventure do not properly use the transformer function yet!
-  transform_adventures(old_level, new_level=None, function=nop)
+  transform_adventures(old_level, new_level, function=nop)
 
 
 def remove_brackets(s):
   return s.replace('(', ' ').replace(')', '')
 
-transform_levels_in_all_YAMLs(14, 15)
+transform_levels_in_all_YAMLs(12, 13)
