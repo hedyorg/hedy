e77a7402d1a9cc696292744831a2890a48ff4837
TiBiBa <timonbakker@hotmail.com>
2021-11-18 12:01:47 +0100

    Level restriction is implemented

--- app.py
+++ app.py
@@ -780,7 +780,10 @@ def index(level, step):
     adventures, restrictions = get_restrictions(load_adventures_per_level(g.lang, level), current_user()['username'], level)
     level_defaults_for_lang = LEVEL_DEFAULTS[g.lang]
 
-    if level not in level_defaults_for_lang.levels:
+    print(adventures)
+    print(restrictions)
+
+    if level not in level_defaults_for_lang.levels or restrictions['hide_level']:
         return utils.page_404 (TRANSLATIONS, render_main_menu('hedy'), current_user()['username'], g.lang, TRANSLATIONS.get_translations (g.lang, 'ui').get ('no_such_level'))
     defaults = level_defaults_for_lang.get_defaults_for_level(level)
     max_level = level_defaults_for_lang.max_level()
@@ -812,23 +815,26 @@ def get_restrictions(adventures, user, level):
                         display_adventures.append(adventure)
                 restrictions['amount_next_level'] = level_preferences['progress']
                 restrictions['example_programs'] = level_preferences['example_programs']
+                restrictions['hide_level'] = level_preferences['hide']
                 prev_level_preferences = DATABASE.get_level_preferences_class(student_classes[0]['id'], level-1)
                 next_level_preferences = DATABASE.get_level_preferences_class(student_classes[0]['id'], level+1)
+                print(next_level_preferences)
                 if prev_level_preferences:
-                    restrictions['prev_level'] = prev_level_preferences['hide_level']
+                    restrictions['hide_prev_level'] = prev_level_preferences['hide']
                 else:
-                    restrictions['prev_level'] = True
+                    restrictions['hide_prev_level'] = False
                 if next_level_preferences:
-                    restrictions['next_level'] = next_level_preferences['hide_level']
+                    restrictions['hide_next_level'] = next_level_preferences['hide']
                 else:
-                    restrictions['next_level'] = True
+                    restrictions['hide_next_level'] = False
 
     if not found_restrictions:
         display_adventures = adventures
         restrictions['amount_next_level'] = 0
         restrictions['example_programs'] = True
-        restrictions['prev_level'] = True
-        restrictions['next_level'] = True
+        restrictions['hide_level'] = False
+        restrictions['hide_prev_level'] = False
+        restrictions['hide_next_level'] = False
 
     return display_adventures, restrictions
 
