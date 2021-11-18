d7122017dc4f45b36eec9a9166e3f2efbf563f99
eylu4090 <eylu@umich.edu>
2021-11-17 19:42:21 -0500

    update 403 page for techer.py routes

--- website/teacher.py
+++ website/teacher.py
@@ -20,8 +20,7 @@ def routes (app, database):
     @requires_login
     def get_classes (user):
         if not is_teacher(user):
-            # return 'Only teachers can retrieve classes', 403
-            return utils.page_403 (TRANSLATIONS, render_main_menu('hedy'), current_user()['username'], g.lang, TRANSLATIONS.get_translations (g.lang, 'ui').get ('not_teacher'))
+            return utils.page_403 (TRANSLATIONS, render_main_menu('hedy'), current_user()['username'], g.lang, TRANSLATIONS.get_translations (g.lang, 'ui').get ('retrieve_class'))
         return jsonify (DATABASE.get_teacher_classes (user ['username'], True))
 
     @app.route('/class/<class_id>', methods=['GET'])
@@ -29,8 +28,7 @@ def routes (app, database):
     def get_class (user, class_id):
         app.logger.info('This is info output')
         if not is_teacher(user):
-            # return 'Only teachers can retrieve classes', 403
-            return utils.page_403 (TRANSLATIONS, render_main_menu('hedy'), current_user()['username'], g.lang, TRANSLATIONS.get_translations (g.lang, 'ui').get ('not_teacher'))
+            return utils.page_403 (TRANSLATIONS, render_main_menu('hedy'), current_user()['username'], g.lang, TRANSLATIONS.get_translations (g.lang, 'ui').get ('retrieve_class'))
         Class = DATABASE.get_class (class_id)
         if not Class or Class ['teacher'] != user ['username']:
             return utils.page_404 (TRANSLATIONS, render_main_menu('my-profile'), current_user()['username'], g.lang, TRANSLATIONS.get_translations(g.lang, 'ui').get('no_such_class'))
