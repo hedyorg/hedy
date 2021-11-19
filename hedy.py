1c5eee5b9b8c9e83a20bba3aa4c904d7f5952ca0
EmilM <meyknechtemil@gmail.com>
2021-11-19 20:59:09 +0100

    Translation structure level 1 setup

--- hedy.py
+++ hedy.py
@@ -20,7 +20,7 @@ MAX_LINES = 100
 LEVEL_STARTING_INDENTATION = 8
 
 # Boolean variables to allow code which is under construction to not be executed
-local_keywords_enabled = False # If this is True, only the keywords in the specified language can be used for now
+local_keywords_enabled = True # If this is True, only the keywords in the specified language can be used for now
 
 #dictionary to store transpilers
 TRANSPILER_LOOKUP = {}
@@ -1889,6 +1889,97 @@ def execute(input_string, level):
     if python.has_turtle:
         raise exceptions.HedyException("hedy.execute doesn't support turtle")
     exec(python.code)
+
+
+def keywords_to_dict(to_lang="nl"):
+    """"Return a dictionary of keywords from language of choice. Key is english value is lang of choice"""
+    keywords = {}
+    keywords_from = get_keywords_for_language("en").replace("\n\n", "\n").splitlines()
+
+    keywords_to = get_keywords_for_language(to_lang).replace("\n\n", "\n").splitlines()
+    keywords_from_withoutlvl = []
+    for line in keywords_from:
+        if line[0] != '/':
+            keywords_from_withoutlvl.append(line)
+
+    for line in range(len(keywords_from_withoutlvl)):
+        if keywords_from[line][0] != '/':
+            keywords[(keywords_from[line].split('"'))[1]] = keywords_to[line].split('"')[1]
+
+    return keywords
+
+
+def translate_keywords(input_string, from_lang="nl", to_lang="nl", level=1):
+    """"Return code with keywords translated to language of choice in level of choice"""
+    parser = get_parser(level, from_lang)
+
+    punctuation_symbols = ['!', '?', '.']
+
+    keywordDict = keywords_to_dict(to_lang)
+    program_root = parser.parse(input_string + '\n').children[0]
+    abstract_syntaxtree = ExtractAST().transform(program_root)
+    translator = TRANSPILER_LOOKUP[level]
+    abstract_syntaxtree = translator(keywordDict, punctuation_symbols).transform(program_root)
+
+    return abstract_syntaxtree
+
+
+def hedy_translator(level):
+    def decorator(c):
+        TRANSPILER_LOOKUP[level] = c
+        c.level = level
+        return c
+
+    return decorator
+
+
+@hedy_translator(level=1)
+class ConvertToLang1(Transformer):
+
+    def __init__(self, keywords, punctuation_symbols):
+        self.keywords = keywords
+        self.punctuation_symbols = punctuation_symbols
+        __class__.level = 1
+
+    def command(self, args):
+        return args[0]
+
+    def program(self, args):
+        return '\n'.join([str(c) for c in args])
+
+    def text(self, args):
+        return ''.join([str(c) for c in args])
+
+    def invalid_space(self, args):
+        return " " + ''.join([str(c) for c in args])
+
+    def print(self, args):
+        return self.keywords["print"] + " " + "".join([str(c) for c in args])
+
+    def echo(self, args):
+        all_args = self.keywords["echo"]
+        if args:
+            all_args += " "
+        return all_args + "".join([str(c) for c in args])
+
+    def ask(self, args):
+        return self.keywords["ask"] + " " + "".join([str(c) for c in args])
+
+    def turn(self, args):
+        return self.keywords["turn"] + " " + "".join([str(c) for c in args])
+
+    def forward(self, args):
+        return self.keywords["forward"] + " " + "".join([str(c) for c in args])
+
+    def random(self, args):
+        return self.keywords["random"] + "".join([str(c) for c in args])
+
+    def invalid(self, args):
+        return ' '.join([str(c) for c in args])
+
+    def __default__(self, data, children, meta):
+        return Tree(data, children, meta)
+
 
 # f = open('output.py', 'w+')
 # f.write(python)
---
+++ tests/test_translation_level_01.py
@@ -0,0 +1,122 @@
+import hedy
+import textwrap
+from test_level_01 import HedyTester
+
+
+def check_local_lang_bool(func):
+    def inner(self):
+        if not hedy.local_keywords_enabled:
+            return
+
+        return func(self)
+    return inner
+
+class TestsTranslationLevel1(HedyTester):
+    level = 1
+    keywords_from = hedy.keywords_to_dict('en')
+    keywords_to = hedy.keywords_to_dict('nl')
+
+    @check_local_lang_bool
+    def test_print(self):
+        result = hedy.translate_keywords(f"{self.keywords_from['print']} Hallo welkom bij Hedy!", "en", "nl",
+                                         self.level)
+        expected = f"{self.keywords_to['print']} Hallo welkom bij Hedy!"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_print_multiple_lines(self):
+        result = hedy.translate_keywords(
+            f"{self.keywords_from['print']} Hallo welkom bij Hedy!\n{self.keywords_from['print']} veel plezier", "en",
+            "nl", self.level)
+        expected = f"{self.keywords_to['print']} Hallo welkom bij Hedy!\n{self.keywords_to['print']} veel plezier"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_print_kewords(self):
+        result = hedy.translate_keywords(f"{self.keywords_from['print']} print ask echo", "en", "nl", self.level)
+        expected = f"{self.keywords_to['print']} print ask echo"
+
+    @check_local_lang_bool
+    def test_ask(self):
+        result = hedy.translate_keywords(f"{self.keywords_from['ask']} Hallo welkom bij Hedy!", "en", "nl", self.level)
+        expected = f"{self.keywords_to['ask']} Hallo welkom bij Hedy!"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_ask_multiple_lines(self):
+        result = hedy.translate_keywords(
+            f"{self.keywords_from['ask']} Hallo welkom bij Hedy!\n{self.keywords_from['ask']} veel plezier", "en", "nl",
+            self.level)
+        expected = f"{self.keywords_to['ask']} Hallo welkom bij Hedy!\n{self.keywords_to['ask']} veel plezier"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_ask_kewords(self):
+        result = hedy.translate_keywords(f"{self.keywords_from['ask']} print ask echo", "en", "nl", self.level)
+        expected = f"{self.keywords_to['ask']} print ask echo"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_echo(self):
+        result = hedy.translate_keywords(
+            f"{self.keywords_from['ask']} Hallo welkom bij Hedy!\n{self.keywords_from['echo']}", "en", "nl", self.level)
+        expected = f"{self.keywords_to['ask']} Hallo welkom bij Hedy!\n{self.keywords_to['echo']}"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_echo_text(self):
+        result = hedy.translate_keywords(
+            f"{self.keywords_from['ask']} stel je vraag\n{self.keywords_from['echo']} tekst", "en", "nl", self.level)
+        expected = f"{self.keywords_to['ask']} stel je vraag\n{self.keywords_to['echo']} tekst"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_forward(self):
+        result = hedy.translate_keywords(f"{self.keywords_from['forward']} 50", "en", "nl", self.level)
+        expected = f"{self.keywords_to['forward']} 50"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_turn(self):
+        result = hedy.translate_keywords(f"{self.keywords_from['turn']} left", "en", "nl", self.level)
+        expected = f"{self.keywords_to['turn']} left"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_turn_value(self):
+        result = hedy.translate_keywords(f"{self.keywords_from['turn']} 50", "en", "nl", self.level)
+        expected = f"{self.keywords_to['turn']} 50"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def test_invalid(self):
+        result = hedy.translate_keywords(f"hallo", "en", "nl", self.level)
+        expected = f"hallo"
+
+        self.assertEqual(result, expected)
+
+    # No translation because of the invalid space error
+    @check_local_lang_bool
+    def test_invalid_space(self):
+        result = hedy.translate_keywords(f" {self.keywords_from['print']} Hedy", "en", "nl", self.level)
+        expected = f" {self.keywords_from['print']} Hedy"
+
+        self.assertEqual(result, expected)
+
+    @check_local_lang_bool
+    def no_argument_ask(self):
+        result = hedy.translate_keywords(f"{self.keywords_to['ask']}", "en", "nl", self.level)
+        expected = f"{self.keywords_to['ask']}"
+
+        self.assertEqual(result, expected)
+
