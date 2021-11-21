61696f7689f8c9107d66174250559467d389ee5a
Felienne <felienne@gmail.com>
2021-11-21 18:42:01 +0100

    fix nl translation, highlighting and distance algo (final not your fault B)

--- hedy.py
+++ hedy.py
@@ -1899,7 +1899,7 @@ def is_program_valid(program_root, input_string, level, lang):
             raise exceptions.UnsupportedFloatException(value=''.join(invalid_info.arguments))
         else:
             invalid_command = invalid_info.command
-            closest = closest_command(invalid_command, get_suggestions_for_language(lang)[level])
+            closest = closest_command(invalid_command, get_suggestions_for_language('en')[level])
             fixed_code = None
             result = None
             if closest:
