a811aad40d561be97f25c26758ef08b650068fe7
Rico Huijbers <rix0rrr@gmail.com>
2021-12-05 20:45:56 +0100

    fix: new page loading mechanism crashes on requesting unknown page (#1473)

--- app.py
+++ app.py
@@ -974,7 +974,11 @@ def main_page(page):
             return utils.page_403(TRANSLATIONS, current_user()['username'], g.lang,
                                   TRANSLATIONS.get_translations(g.lang, 'ui').get('not_teacher'))
 
-    main_page_translations = hedyweb.PageTranslations(page).get_page_translations(g.lang)
+    requested_page = hedyweb.PageTranslations(page)
+    if not requested_page.exists():
+        abort(404)
+
+    main_page_translations = requested_page.get_page_translations(g.lang)
     return render_template('main-page.html', auth=TRANSLATIONS.get_translations(g.lang, 'Auth'),
                            content=main_page_translations)
 
