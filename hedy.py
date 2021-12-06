978f56deee675cace1a90e45ac13c59ae5b8fd69
EmilM <meyknechtemil@gmail.com>
2021-12-06 22:44:36 +0100

    Added check for github environment

--- hedy.py
+++ hedy.py
@@ -22,9 +22,7 @@ MAX_LINES = 100
 LEVEL_STARTING_INDENTATION = 8
 
 # Boolean variables to allow code which is under construction to not be executed
-local_keywords_enabled = False or 'pytest' in sys.argv[0] or environ['GITHUB_ACTIONS'] # If this is True, only the keywords in the specified language can be used for now
-
-raise Exception(environ)
+local_keywords_enabled = False or 'pytest' in sys.argv[0] or environ['GITHUB_ACTIONS'] == 'true' # If this is True, only the keywords in the specified language can be used for now
 
 #dictionary to store transpilers
 TRANSPILER_LOOKUP = {}
