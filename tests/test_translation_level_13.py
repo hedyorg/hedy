35941b7b57d4c88c5cde545e94f7030a06a3232a
EmilM <meyknechtemil@gmail.com>
2021-12-08 14:40:10 +0100

    Added translation for levels 12 to 17 + tests (#1499)
    Co-authored-by: Felienne <felienne@gmail.com>

---
+++ tests/test_translation_level_13.py
@@ -0,0 +1,75 @@
+import hedy
+from test_level_01 import HedyTester
+import hedy_translation
+import textwrap
+
+
+# tests should be ordered as follows:
+# * Translation from English to Dutch
+# * Translation from Dutch to English
+# * Translation to several languages
+# * Error handling
+
+
+class TestsTranslationLevel11(HedyTester):
+    level = 13
+
+    def test_and_condition_english_dutch(self):
+        code = textwrap.dedent("""\
+        naam is 'hedy'
+        leeftijd is 2
+        if naam is 'hedy' and leeftijd is 2
+            print 'hallo'""")
+
+        result = hedy_translation.translate_keywords(code, from_lang="en", to_lang="nl", level=self.level)
+        expected = textwrap.dedent("""\
+        naam is 'hedy'
+        leeftijd is 2
+        als naam is 'hedy' en leeftijd is 2
+            print 'hallo'""")
+
+        self.assertEqual(result, expected)
+
+    def test_or_condition_english_dutch(self):
+        code = textwrap.dedent("""\
+        naam is 'hedy'
+        leeftijd is 2
+        if naam is 'niet hedy' or leeftijd is 2
+            print 'hallo'""")
+
+        result = hedy_translation.translate_keywords(code, from_lang="en", to_lang="nl", level=self.level)
+        expected = textwrap.dedent("""\
+        naam is 'hedy'
+        leeftijd is 2
+        als naam is 'niet hedy' of leeftijd is 2
+            print 'hallo'""")
+
+        self.assertEqual(result, expected)
+
+    def test_and_condition_acces_list_english_dutch(self):
+        code = textwrap.dedent("""\
+        naam is 'hedy', 'niet hedy'
+        if 'hedy' in naam and 'niet hedy' in naam
+            print 'hallo'""")
+
+        result = hedy_translation.translate_keywords(code, from_lang="en", to_lang="nl", level=self.level)
+        expected = textwrap.dedent("""\
+        naam is 'hedy', 'niet hedy'
+        als 'hedy' in naam en 'niet hedy' in naam
+            print 'hallo'""")
+
+        self.assertEqual(result, expected)
+
+    def test_or_condition_acces_list_english_dutch(self):
+        code = textwrap.dedent("""\
+        naam is 'hedy', 'niet hedy'
+        if 'hedy' in naam or 'niet hedy' in naam
+            print 'hallo'""")
+
+        result = hedy_translation.translate_keywords(code, from_lang="en", to_lang="nl", level=self.level)
+        expected = textwrap.dedent("""\
+        naam is 'hedy', 'niet hedy'
+        als 'hedy' in naam of 'niet hedy' in naam
+            print 'hallo'""")
+
+        self.assertEqual(result, expected)
