b7ee1d6d8b29361c375921f3c3fb50c815a23083
Timon Bakker <timonbakker@hotmail.com>
2021-12-22 13:17:43 +0100

    Revert "Fix achievement key error"
    This reverts commit 048d0d701d40ef7e8f2d67c8c235b2fc9797f56b.

--- tests_e2e.py
+++ tests_e2e.py
@@ -658,9 +658,7 @@ class TestProgram(AuthHelper):
         self.assertEqual(len(saved_programs), 1)
         saved_program = saved_programs[0]
         for key in program:
-            # WHEN creating a first program, an achievement is achieved and stored in the response
-            if key != "achievements":
-                self.assertEqual(program[key], saved_program[key])
+            self.assertEqual(program[key], saved_program[key])
 
     def test_invalid_make_program_public(self):
         # GIVEN a logged in user
