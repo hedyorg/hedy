4e911c1c2700f8c9aec340c65728aaf64e320ef4
Felienne <felienne@gmail.com>
2022-01-04 09:25:37 +0100

    fixes until level 12 (after which it is no longer needed)

--- tests/test_level_06.py
+++ tests/test_level_06.py
@@ -354,6 +354,37 @@ class TestsLevel6(HedyTester):
       expected=expected
     )
 
+
+  def test_one_space_in_rhs_if(self):
+    code = textwrap.dedent("""\
+    naam is James
+    if naam is James Bond print 'shaken'""")
+
+    expected = textwrap.dedent("""\
+    naam = 'James'
+    if str(naam) == str('James Bond'):
+      print(f'shaken')""")
+
+    self.multi_level_tester(
+      code=code,
+      expected=expected,
+      max_level=7)
+
+  def test_multiple_spaces_in_rhs_if(self):
+    code = textwrap.dedent("""\
+    naam is James
+    if naam is Bond James Bond print 'shaken'""")
+
+    expected = textwrap.dedent("""\
+    naam = 'James'
+    if str(naam) == str('Bond James Bond'):
+      print(f'shaken')""")
+
+    self.multi_level_tester(
+      code=code,
+      expected=expected,
+      max_level=7)
+
   def test_calc_chained_vars(self):
     code = textwrap.dedent("""\
       a is 5
