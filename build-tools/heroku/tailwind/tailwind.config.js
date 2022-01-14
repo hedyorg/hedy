cd29a3d6b7745f5405e3c4b7c991b22dd5718cb8
Timon Bakker <timonbakker@hotmail.com>
2022-01-14 10:39:17 +0100

    Fixed styles.css conform v2

--- build-tools/heroku/tailwind/tailwind.config.js
+++ build-tools/heroku/tailwind/tailwind.config.js
@@ -20,6 +20,18 @@ module.exports = {
       lg: '1024px',
       xl: '1280px',
     },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
