4bff38c8ba473a04fde71a36923a9912cfbdef38
Timon Bakker <timonbakker@hotmail.com>
2022-01-19 15:23:59 +0100

    Fixed padding-y

--- static/js/app.ts
+++ static/js/app.ts
@@ -177,7 +177,7 @@ function create_language_selector(index: number, current_lang: string, other_lan
   if (hidden) {
     dropdownContainer.addClass('hidden');
   }
-  const button = $('<button>').addClass("inline-flex items-center text-xl px-2 bg-blue-600 rounded-lg").text(current_lang.toUpperCase());
+  const button = $('<button>').addClass("inline-flex items-center text-xl px-2 py-2 bg-blue-600 rounded-lg").text(current_lang.toUpperCase());
   button.append("<svg class=\"w-6 h-6\" fill=\"none\" stroke=\"currentColor\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\"  d=\"M19 9l-7 7-7-7\"></path></svg>");
   const menu = $('<div>').addClass("dropdown-menu absolute hidden right-0");
   const list = $('<ul>').addClass("dropdown-menu list-none text-xl z-10 text-white px-4 mr-1 bg-blue-600 rounded-lg mt-2");
