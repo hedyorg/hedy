aac77491053023cfec26d748fbe2f95e0e186ef7
Timon Bakker <timonbakker@hotmail.com>
2022-01-20 11:57:03 +0100

    Final fix

--- static/js/app.ts
+++ static/js/app.ts
@@ -246,10 +246,9 @@ export function runit(level: string, lang: string, cb: () => void) {
         return;
       }
         runPythonProgram(response.Code, response.has_turtle, response.has_sleep, response.Warning, cb).catch(function(err) {
-        console.log(err)
-        console.log(err.message)
+        console.log(err);
         // If it is an error we throw due to program execution while another is running -> don't show and log it
-        if (!(err.message === "program_interrupt")) {
+        if (!(err.message == "\"program_interrupt\"")) {
           error.show(ErrorMessages['Execute_error'], err.message);
           reportClientError(level, code, err.message);
         }
