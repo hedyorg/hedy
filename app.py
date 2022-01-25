5a0425defb73007ca8750068593c60d0d2a25fd3
Timon Bakker <timonbakker@hotmail.com>
2022-01-25 11:46:07 +0100

    Fixed implementation

--- app.py
+++ app.py
@@ -1250,7 +1250,9 @@ def get_admin_users_page(user):
 def get_admin_classes_page(user):
     if not is_admin(user):
         return utils.error_page(error=403, ui_message='unauthorized')
-    return render_template('admin-classes.html')
+
+    classes = DATABASE.all_classes()
+    return render_template('admin-classes.html', classes=classes, page_title=hedyweb.get_page_title('admin'))
 
 
 @app.route('/admin/stats', methods=['GET'])
@@ -1258,7 +1260,7 @@ def get_admin_classes_page(user):
 def get_admin_stats_page(user):
     if not is_admin(user):
         return utils.error_page(error=403, ui_message='unauthorized')
-    return render_template('admin-stats.html')
+    return render_template('admin-stats.html', page_title=hedyweb.get_page_title('admin'))
 
 
 @app.route('/change_language', methods=['POST'])
