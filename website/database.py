8413180faf675fa49610b91d43cb4d7a7fd23721
Timon Bakker <timonbakker@hotmail.com>
2022-01-27 17:00:53 +0100

    Fix noneType error

--- website/database.py
+++ website/database.py
@@ -376,7 +376,7 @@ class Database:
         if user:
             student_classes = self.get_student_classes(user)
             if student_classes:
-                all_teacher_adventures = self.student_get_teacher_adventures(student_classes[0]['id'])
+                all_teacher_adventures = self.student_get_teacher_adventures(student_classes[0]['id']) or []
                 level_preferences = self.get_level_customizations_class(student_classes[0]['id'], level)
                 if level_preferences:
                     found_restrictions = True
