604e98dee1d1ddbcaf2585b17569021beb03a247
Yunus Isleyen <y.isleyen@umail.leidenuniv.nl>
2022-02-10 00:55:21 +0100

    rewrite program_repair for mutations

--- program_repair.py
+++ program_repair.py
@@ -1,36 +1,40 @@
 import hedy
 
 
-def insert(input_string, line, column, new_string):
-    """"insert new_string at (line, column)"""
-    rows = input_string.splitlines()
-    rows[line - 1] = rows[line - 1][:column - 1] + new_string + rows[line - 1][column - 1:]
-
-    return '\n'.join(rows)
-
-
-def delete(input_string, line, column, length):
-    """"delete length chars starting at (line, column)"""
-    rows = input_string.splitlines()
-    rows[line - 1] = rows[line - 1][:column - 1] + rows[line - 1][column - 1 + length:]
-
-    return '\n'.join(rows)
-
-
-def replace(input_string, line, column, length, new_string):
-    """"replace at (line, column) length chars with new_string"""
-    result = delete(input_string, line, column, length)
-    result = insert(result, line, column, new_string)
+def insert(line, column, new_string):
+    """"insert new_string at column from line"""
+    result = line[:column - 1] + new_string + line[column - 1:]
 
     return result
 
 
-def make_mutants(input_string, line):
+def delete(line, column, length):
+    """"delete length chars starting at column from line"""
+    result = line[:column - 1] + line[column - 1 + length:]
+
+    return result
+
+
+def replace(line, column, length, new_string):
+    """"replace at column length chars with new_string"""
+    result = delete(line, column, length)
+    result = insert(line, column, new_string)
+
+    return result
+
+
+def make_mutants(input_string, row):
     mutants = []
+    mutated_lines = []
+    lines = input_string.splitlines()
+    line = lines[row - 1]
     # +2 because we start counting from 1, and we are adding a character
-    length = len(input_string.splitlines()[line - 1]) + 2
-    for i in range(1, length):
-        mutants.append(insert(input_string, line, i, '\''))
+    length = len(line) + 2
+    for i in range(1, length):  # make mutant lines
+        mutated_lines.append(insert(line, i, '\''))
+    for mutated_line in mutated_lines:  # replace the line with mutated lines
+        lines[row - 1] = mutated_line
+        mutants.append('\n'.join(lines))
     return mutants
 
 
@@ -57,16 +61,24 @@ def remove_leading_spaces(input_string):
     return '\n'.join([x.lstrip() for x in input_string.split('\n')])
 
 
-def remove_char(input_string, line, column):
-    return delete(input_string, line, column, 1)
+def remove_char(input_string, row, column):
+    lines = input_string.splitlines()
+    line = lines[row - 1]
+    lines[row - 1] = delete(line, column, 1)
+
+    return '\n'.join(lines)
 
 
-def fix_indent(input_string, line, leading_spaces, indent_size):
+def fix_indent(input_string, row, leading_spaces, indent_size):
+    lines = input_string.splitlines()
+    line = lines[row - 1]
     if leading_spaces < indent_size:
         # not enough spaces, add spaces
-        return insert(input_string, line, 1, ' ' * (indent_size - leading_spaces))
+        lines[row - 1] = insert(line, 1, ' ' * (indent_size - leading_spaces))
+        return '\n'.join(lines)
     else:
         # too many spaces, remove spaces
-        return delete(input_string, line, 1, leading_spaces - indent_size)
+        lines[row - 1] = delete(line, 1, leading_spaces - indent_size)
+        return '\n'.join(lines)
 
 
