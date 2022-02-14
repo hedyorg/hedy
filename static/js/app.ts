ecef3533aabffc6c9addc7b1b1031e25852aec8d
Timon Bakker <timonbakker@hotmail.com>
2022-02-14 10:58:11 +0100

    Remove redundant empty line

--- static/js/app.ts
+++ static/js/app.ts
@@ -792,7 +792,6 @@ function runPythonProgram(this: any, code: string, hasTurtle: boolean, hasSleep:
   Sk.pre = "output";
   const turtleConfig = (Sk.TurtleGraphics || (Sk.TurtleGraphics = {}));
   turtleConfig.target = 'turtlecanvas';
-
   // If the adventures are not shown -> increase height of turtleConfig
   if ($('#adventures').is(":hidden")) {
       turtleConfig.height = 600;
