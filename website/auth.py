d477a6a17beb95e55b376c5d7934957a0746e4c9
Timon Bakker <timonbakker@hotmail.com>
2022-02-18 11:31:17 +0100

    Add minlength to password repeat

--- website/auth.py
+++ website/auth.py
@@ -272,6 +272,8 @@ def routes(app, database):
         if not isinstance(body, dict):
             return g.auth_texts.get('ajax_error'), 400
 
+        print(body)
+
         # Validate the essential data using a function -> also used for multiple account creation
         validation = validate_signup_data(body)
         if validation:
