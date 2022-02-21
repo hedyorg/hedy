5830946a8239e201e1e1b5dba4393bb01e062229
Timon Bakker <timonbakker@hotmail.com>
2022-02-21 16:32:25 +0100

    Fix level defaults test

--- tests/test_z_defaults.py
+++ tests/test_z_defaults.py
@@ -51,6 +51,9 @@ def collect_snippets(path):
 
 
 Hedy_snippets = [(s.name, s) for s in collect_snippets(path='../coursedata/level-defaults')]
+keywords = YamlFile.for_file('coursedata/keywords/en.yaml').to_dict()
+for snippet in Hedy_snippets:
+    snippet.code = snippet.code.format(**keywords)
 
 
 class TestsLevelDefaultsPrograms(unittest.TestCase):
