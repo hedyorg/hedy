6c3582263c1bb17cf174bd6e4555d3390c99ef16
Timon Bakker <timonbakker@hotmail.com>
2022-03-03 14:54:18 +0100

    Make sure body_plain is initiliazed

--- website/auth.py
+++ website/auth.py
@@ -819,6 +819,7 @@ def send_email_template(template, email, link='', lang="en", username=''):
         body_html = f.read()
 
     body_html = body_html.format(content=body)
+    body_plain = body_html
     if link:
         body_plain = body.format(link='Please copy and paste this link into a new tab: ' + link)
         body_html = body_html.format(link='<a href="' + link + '">Link</a>')
