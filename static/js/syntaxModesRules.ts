d85bb9db2b7d3d27ada0b019b66d0ce7aa45ce77
Felienne <felienne@gmail.com>
2022-03-11 11:07:59 +0100

    do not highlight left and right in levels 2 and up

--- static/js/syntaxModesRules.ts
+++ static/js/syntaxModesRules.ts
@@ -121,7 +121,7 @@ const LEVELS = [
     name: 'level1',
     rules: pipe(baseRules(),
       rule_printSpace('gobble'),
-      rule_turtle(),
+      rule_turtle_left_right(),
       recognize('start', {
         regex: keywordWithSpace(currentLang._ECHO),
         token: 'keyword',
@@ -484,10 +484,9 @@ function rule_printParen() {
   });
 }
 
-function rule_turtle() {
+function rule_turtle_left_right() {
     return comp(
       recognize('start', {
-        // Note: left and right are not yet keywords
         regex: currentLang._TURN + ' (' + currentLang._LEFT + '|' + currentLang._RIGHT + ')?',
         token: 'keyword',
         next: 'start',
@@ -499,6 +498,22 @@ function rule_turtle() {
       })
     )
 }
+
+function rule_turtle() {
+    return comp(
+      recognize('start', {
+        regex: currentLang._TURN,
+        token: 'keyword',
+        next: 'start',
+      }),
+      recognize('start', {
+        regex: currentLang._FORWARD,
+        token: 'keyword',
+        next: 'start',
+      })
+    )
+}
+
 
 function rule_sleep() {
   return recognize('start', {
