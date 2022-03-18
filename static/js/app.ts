43eb998836e4930017cbcbc5e2ad9a93dd4f8458
Felicia Redelaar <fredelaar@gmail.com>
2022-03-18 17:17:46 +0100

    can set debugger points

--- static/js/app.ts
+++ static/js/app.ts
@@ -1477,3 +1477,27 @@ export function filter_admin() {
     window.open('?filter=' + filter + "&start=" + start_date + "&end=" + end_date, "_self");
   }
 }
+
+var editorFELICIA = ace.edit( "editor" );
+
+editorFELICIA.on("guttermousedown", function(e) {
+    var target = e.domEvent.target;
+    if (target.className.indexOf("ace_gutter-cell") == -1)
+        return;
+    if (!editorFELICIA.isFocused())
+        return;
+
+    if (e.clientX > 25 + target.getBoundingClientRect().left)
+        return;
+
+    var breakpoints = e.editor.session.getBreakpoints(row, 0);
+    console.log(breakpoints);
+var row = e.getDocumentPosition().row;
+if(typeof breakpoints[row] === typeof undefined)
+    e.editor.session.setBreakpoint(row);
+else
+    e.editor.session.clearBreakpoint(row);
+    e.stop();
+})
+
+
