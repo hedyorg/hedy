9434fb441d1b0df12f7ba3a2624ed14c52909ae8
Timon Bakker <timonbakker@hotmail.com>
2022-03-18 11:04:23 +0100

    Updated yamls, fixed cookie

--- website/auth.py
+++ website/auth.py
@@ -21,7 +21,11 @@ from website import querylog, database
 import hashlib
 
 TOKEN_COOKIE_NAME = config['session']['cookie_name']
+# The session_length in the session is set to 60 * 24 * 14 (in minutes) config.py#13
+# We multiply this by 60 to set the "normal" session_length to 14 days
+# We than divide this value again by 84 to set the reset_length to 4 hours
 session_length = config['session']['session_length'] * 60
+reset_length = session_length / 84
 
 env = os.getenv('HEROKU_APP_NAME')
 
@@ -653,7 +657,7 @@ def routes(app, database):
 
         # Create a token
         token = make_salt()
-        DATABASE.store_token({'id': token, 'username': user['username'], 'ttl': times() + session_length})
+        DATABASE.store_token({'id': token, 'username': user['username'], 'ttl': times() + reset_length})
 
         if is_testing_request(request):
             # If this is an e2e test, we return the email verification token directly instead of emailing it.
