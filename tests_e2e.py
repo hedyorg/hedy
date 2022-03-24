5f3ada0408f0c9b78e880fd363195cabff284d24
Timon Bakker <timonbakker@hotmail.com>
2022-03-24 15:25:32 +0100

    Update e2e tests to conform optional mail

--- tests_e2e.py
+++ tests_e2e.py
@@ -93,8 +93,8 @@ class AuthHelper(unittest.TestCase):
 
         if username in USERS:
             return USERS[username]
-        body = {'username': username, 'email': username + '@hedy.com', 'mail_repeat': username + '@hedy.com',
-                'language': 'nl', 'keyword_language': 'en', 'agree_terms': True, 'password': 'foobar', 'password_repeat': 'foobar'}
+        body = {'username': username, 'email': username + '@hedy.com', 'language': 'nl', 'keyword_language': 'en',
+                'agree_terms': True, 'password': 'foobar', 'password_repeat': 'foobar'}
         response = request('post', 'auth/signup', {}, body, cookies=self.user_cookies[username])
 
         # It might sometimes happen that by the time we attempted to create the user, another test did it already.
@@ -332,8 +332,8 @@ class TestAuth(AuthHelper):
     def test_signup(self):
         # GIVEN a valid username and signup body
         username = self.make_username()
-        user = {'username': username, 'email': username + '@hedy.com', 'mail_repeat': username + '@hedy.com',
-                'password': 'foobar', 'password_repeat': 'foobar', 'language': 'nl', 'keyword_language': 'en', 'agree_terms': True}
+        user = {'username': username, 'email': username + '@hedy.com', 'password': 'foobar',
+                'password_repeat': 'foobar', 'language': 'nl', 'keyword_language': 'en', 'agree_terms': True}
 
         # WHEN signing up a new user
         # THEN receive an OK response code from the server
@@ -567,7 +567,6 @@ class TestAuth(AuthHelper):
         }
 
         body = {
-            'email': self.user['email'],
             'language': self.user['language'],
             'keyword_language': self.user['keyword_language']
         }
