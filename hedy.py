da1d3521c4e716b90f3853d992a5374fd502d621
Timon Bakker <timonbakker@hotmail.com>
2022-03-28 11:44:29 +0200

    Changed to correct capital use

--- hedy.py
+++ hedy.py
@@ -39,7 +39,7 @@ ALL_LANGUAGES = {
     'nl': 'Nederlands',
     'es': 'Español',
     'fr': 'Français',
-    'nb_no': 'Norsk Bokmål',
+    'nb_NO': 'Norsk Bokmål',
     'pt_pt': 'Português(pt)',
     'pt_br': 'Português(br)',
     'de': 'Deutsch',
