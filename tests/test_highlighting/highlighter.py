fdb9a884afebc2ca46f99b272c892e18fb0a44e3
thjazi39 <duvillard.tiemen@gmail.com>
2022-04-08 15:24:13 +0200

    fusion of two file

--- tests/test_highlighting/highlighter.py
+++ tests/test_highlighting/highlighter.py
@@ -1,5 +1,4 @@
 import json
-from automaton import simulateRulesWithoutToken
 
 # open data for regex
 file = open('data.json') 
@@ -11,6 +10,46 @@ file = open('tests.json')
 dataTests = json.load(file)
 file.close()
 
+#######################################################################################
+#######################################################################################
+#######################################################################################
+#######################################################################################
+
+import re
+
+
+TokenCode = {
+    "text" :               'T',
+    "keyword" :            'K',
+    "comment" :            'C',
+    "variable" :           'N',
+    "constant.character" : 'S',
+    "invalid" :            'I',
+}
+
+
+def simulateRulesWithoutToken(Rules,Code):
+    Output = ""
+    for c in Code:
+        if c == "\n": Output += "\n"
+        else: Output += " "
+    regRule = Rules["start"][0]
+    for reg in regRule:
+        regComp = re.compile(reg["regex"], re.MULTILINE)
+        for match in regComp.finditer(Code):
+            pos = match.start()
+            for i,submatch in enumerate(match.groups()):
+                tok = reg['token'][i%len(reg['token'])]
+                Output = Output[:pos] + TokenCode[tok] * len(submatch) + Output[pos + len(submatch):]
+                pos += len(submatch)
+
+    return Output
+
+
+#######################################################################################
+#######################################################################################
+#######################################################################################
+#######################################################################################
 
 
 def compare(Result,Expected):
