0e5b6a8b3ee51d756b39ccce9987450cf3e7bd2f
Timon Bakker <timonbakker@hotmail.com>
2022-04-11 20:36:03 +0200

    [PERFORMANCE] Implements user-based saved program level filtering (#2500)

--- app.py
+++ app.py
@@ -101,11 +101,10 @@ def load_adventures_per_level(lang, level):
     loaded_programs = {}
     # If user is logged in, we iterate their programs that belong to the current level. Out of these, we keep the latest created program for both the level mode(no adventure) and for each of the adventures.
     if current_user()['username']:
-        user_programs = DATABASE.programs_for_user(current_user()['username'])
+        user_programs = DATABASE.level_programs_for_user(current_user()['username'], level)
+        print(user_programs)
         for program in user_programs:
-            if program['level'] != level:
-                continue
-            program_key = 'level' if not program.get('adventure_name') else program['adventure_name']
+            program_key = 'default' if not program.get('adventure_name') else program['adventure_name']
             if not program_key in loaded_programs:
                 loaded_programs[program_key] = program
             elif loaded_programs[program_key]['date'] < program['date']:
@@ -129,6 +128,7 @@ def load_adventures_per_level(lang, level):
             continue
         # end adventure is the quiz
         # if quizzes are not enabled, do not load it
+        # Todo TB -> Is this still relevant? Teachers can simply "disable" adventures in customizations!
         if short_name == 'end' and not config['quiz-enabled']:
             continue
         current_adventure = {
