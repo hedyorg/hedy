31ed5b06eb3fffac8bd8bfbae0663a934a08930b
Timon Bakker <timonbakker@hotmail.com>
2022-04-13 10:30:34 +0200

    Add reverse attribute to database call

--- website/database.py
+++ website/database.py
@@ -215,10 +215,10 @@ class Database:
         return USERS.scan(limit=500)
 
     def get_all_explore_programs(self):
-        return PROGRAMS.get_many({'public': 1}, limit=48)
+        return PROGRAMS.get_many({'public': 1}, limit=48, reverse=True)
 
     def get_filtered_explore_programs(self, level=None, adventure=None):
-        programs = PROGRAMS.get_many({'public': 1})
+        programs = PROGRAMS.get_many({'public': 1}, reverse=True)
         if level:
             programs = [x for x in programs if x.get('level') == int(level)]
         if adventure:
