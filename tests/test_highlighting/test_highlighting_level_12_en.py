b24ebe1f2745c705966cb09426b5a0ccdc232528
thjazi39 <duvillard.tiemen@gmail.com>
2022-04-18 16:23:27 +0200

    more test

---
+++ tests/test_highlighting/test_highlighting_level_12_en.py
@@ -0,0 +1,249 @@
+from tests.Highlighter import HighlightTester
+
+class HighlighterTestLeveL12(HighlightTester):
+
+    
+    def test_1(self):
+        self.assertHighlightedMultiLine(
+            "print 'decimal numbers now need to use a dot'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 2.5 + 2.5",
+            "KKKKK NNN K NNN",
+            level="level12",lang='en')
+
+
+    def test_2(self):
+        self.assertHighlightedMultiLine(
+            "print 'Two and a half plus two and a half is...'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 2.5 + 2.5",
+            "KKKKK NNN K NNN",
+            level="level12",lang='en')
+
+
+    def test_3(self):
+        self.assertHighlightedMultiLine(
+            "name = 'Hedy the Robot'",
+            "TTTT K SSSSSSSSSSSSSSSS",
+            "print 'Hello ' name",
+            "KKKKK SSSSSSSS TTTT",
+            level="level12",lang='en')
+
+
+    def test_4(self):
+        self.assertHighlightedMultiLine(
+            "superheroes = 'Spiderman', 'Batman', 'Iron Man'",
+            "TTTTTTTTTTT K SSSSSSSSSSSK SSSSSSSSK SSSSSSSSSS",
+            "print superheroes at random",
+            "KKKKK TTTTTTTTTTT KK KKKKKK",
+            level="level12",lang='en')
+
+
+    def test_5(self):
+        self.assertHighlightedMultiLine(
+            "name = ask 'What is your name?'",
+            "TTTT K KKK SSSSSSSSSSSSSSSSSSSS",
+            "if name = 'Hedy the Robot'",
+            "KK TTTT K SSSSSSSSSSSSSSSS",
+            "print 'Hi there!'",
+            "KKKKK SSSSSSSSSSS",
+            level="level12",lang='en')
+
+
+    def test_6(self):
+        self.assertHighlightedMultiLine(
+            "score = 25",
+            "TTTTT K NN",
+            "print 'You got ' score",
+            "KKKKK SSSSSSSSSS TTTTT",
+            level="level12",lang='en')
+
+
+    def test_7(self):
+        self.assertHighlightedMultiLine(
+            "a = 'Hello '",
+            "T K SSSSSSSS",
+            "b = 'world!'",
+            "T K SSSSSSSS",
+            "print a + b",
+            "KKKKK T K T",
+            level="level12",lang='en')
+
+
+    def test_8(self):
+        self.assertHighlightedMultiLine(
+            "name = 'The Queen of England'",
+            "TTTT K SSSSSSSSSSSSSSSSSSSSSS",
+            "print name ' was eating a piece of cake, when suddenly...'",
+            "KKKKK TTTT SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            level="level12",lang='en')
+
+
+    def test_9(self):
+        self.assertHighlighted(
+            "# place your code here",
+            "CCCCCCCCCCCCCCCCCCCCCC",
+            level="level12",lang='en')
+
+
+    def test_10(self):
+        self.assertHighlightedMultiLine(
+            "actions = 'clap your hands', 'stomp your feet', 'shout Hurray!'",
+            "TTTTTTT K SSSSSSSSSSSSSSSSSK SSSSSSSSSSSSSSSSSK SSSSSSSSSSSSSSS",
+            "for action in actions",
+            "KKK TTTTTT KK TTTTTTT",
+            "for i in range 1 to 2",
+            "KKK T KK KKKKK N KK N",
+            "print 'if youre happy and you know it'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print action",
+            "KKKKK TTTTTT",
+            "print 'if youre happy and you know it and you really want to show it'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'if youre happy and you know it'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print action",
+            "KKKKK TTTTTT",
+            level="level12",lang='en')
+
+
+    def test_11(self):
+        self.assertHighlightedMultiLine(
+            "number1 = ask 'What is the first number?'",
+            "TTTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "number2 = ask 'What is the second number?'",
+            "TTTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "answer = number1 + number2",
+            "TTTTTT K TTTTTTT K TTTTTTT",
+            "print number1 ' plus ' number2 ' is ' answer",
+            "KKKKK TTTTTTT SSSSSSSS TTTTTTT SSSSSS TTTTTT",
+            level="level12",lang='en')
+
+
+    def test_12(self):
+        self.assertHighlightedMultiLine(
+            "price = 0",
+            "TTTTT K N",
+            "food = ask 'What would you like to order?'",
+            "TTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "drinks = ask 'What would you like to drink?'",
+            "TTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "if food is 'hamburger'",
+            "KK TTTT KK SSSSSSSSSSS",
+            "price = price + 6.50",
+            "TTTTT K TTTTT K NNNN",
+            "if food is 'pizza'",
+            "KK TTTT KK SSSSSSS",
+            "price = price + 5.75",
+            "TTTTT K TTTTT K NNNN",
+            "if drinks is 'water'",
+            "KK TTTTTT KK SSSSSSS",
+            "price = price + 1.20",
+            "TTTTT K TTTTT K NNNN",
+            "if drink is 'soda'",
+            "KK TTTTT KK SSSSSS",
+            "price = price + 2.35",
+            "TTTTT K TTTTT K NNNN",
+            "print 'That will be ' price ' dollar, please'",
+            "KKKKK SSSSSSSSSSSSSSS TTTTT SSSSSSSSSSSSSSSSS",
+            level="level12",lang='en')
+
+
+    def test_13(self):
+        self.assertHighlightedMultiLine(
+            "fortunes = 'you will be rich', 'you will fall in love', 'you will slip on a banana peel'",
+            "TTTTTTTT K SSSSSSSSSSSSSSSSSSK SSSSSSSSSSSSSSSSSSSSSSSK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'I will take a look in my crystall ball for your future.'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'I see... I see...'",
+            "KKKKK SSSSSSSSSSSSSSSSSSS",
+            "sleep",
+            "KKKKK",
+            "print fortunes at random",
+            "KKKKK TTTTTTTT KK KKKKKK",
+            level="level12",lang='en')
+
+
+    def test_14(self):
+        self.assertHighlightedMultiLine(
+            "print 'The digital piggy bank'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSS",
+            "wish = ask 'What would you like to buy?'",
+            "TTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "price = ask 'How much does that cost?'",
+            "TTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "saved = ask 'How much money have you saved already?'",
+            "TTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "allowance = ask 'How much pocket money do you get per week?'",
+            "TTTTTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "to_save = price - saved",
+            "TTTTTTT K TTTTT K TTTTT",
+            "weeks = to_save / allowance",
+            "TTTTT K TTTTTTT K TTTTTTTTT",
+            "print 'You can buy a ' wish ' in ' weeks ' weeks.'",
+            "KKKKK SSSSSSSSSSSSSSSS TTTT SSSSSS TTTTT SSSSSSSSS",
+            level="level12",lang='en')
+
+
+    def test_15(self):
+        self.assertHighlightedMultiLine(
+            "name is ask 'What is your name?'",
+            "TTTT KK KKK SSSSSSSSSSSSSSSSSSSS",
+            "if name is 'Agent007'",
+            "KK TTTT KK SSSSSSSSSS",
+            "a is 'Go to the airport '",
+            "T KK SSSSSSSSSSSSSSSSSSSS",
+            "else",
+            "KKKK",
+            "a is 'Go to the trainstation '",
+            "T KK SSSSSSSSSSSSSSSSSSSSSSSSS",
+            "password is ask 'What is the password?'",
+            "TTTTTTTT KK KKK SSSSSSSSSSSSSSSSSSSSSSS",
+            "if password is 'TOPSECRET'",
+            "KK TTTTTTTT KK SSSSSSSSSSS",
+            "b is 'tomorrow at 02.00'",
+            "T KK SSSSSSSSSSSSSSSSSSS",
+            "else",
+            "KKKK",
+            "b is 'today at 10.00'",
+            "T KK SSSSSSSSSSSSSSSS",
+            "print a + b",
+            "KKKKK T K T",
+            level="level12",lang='en')
+
+
+    def test_16(self):
+        self.assertHighlighted(
+            "## place your code here",
+            "CCCCCCCCCCCCCCCCCCCCCCC",
+            level="level12",lang='en')
+
+
+    def test_17(self):
+        self.assertHighlightedMultiLine(
+            "username is ask 'What is your username?'",
+            "TTTTTTTT KK KKK SSSSSSSSSSSSSSSSSSSSSSSS",
+            "password is ask 'What is your password?'",
+            "TTTTTTTT KK KKK SSSSSSSSSSSSSSSSSSSSSSSS",
+            "if username is 'Hedy'",
+            "KK TTTTTTTT KK SSSSSS",
+            "if password is 'secret'",
+            "KK TTTTTTTT KK SSSSSSSS",
+            "print 'Welcome Hedy!'",
+            "KKKKK SSSSSSSSSSSSSSS",
+            "else",
+            "KKKK",
+            "print 'Access denied'",
+            "KKKKK SSSSSSSSSSSSSSS",
+            "else",
+            "KKKK",
+            "print 'Access denied!'",
+            "KKKKK SSSSSSSSSSSSSSSS",
+            level="level12",lang='en')
+
+
+    def test_18(self):
+        self.assertHighlighted(
+            "print 'Lets go to the next level!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            level="level12",lang='en')
---
+++ tests/test_highlighting/test_highlighting_level_13_en.py
@@ -0,0 +1,183 @@
+from tests.Highlighter import HighlightTester
+
+class HighlighterTestLeveL13(HighlightTester):
+
+
+    def test_1(self):
+        self.assertHighlightedMultiLine(
+            "name = ask 'what is your name?'",
+            "TTTT K KKK SSSSSSSSSSSSSSSSSSSS",
+            "age = ask 'what is your age?'",
+            "TTT K KKK SSSSSSSSSSSSSSSSSSS",
+            "if name is 'Hedy' and age is 2",
+            "KK TTTT KK SSSSSS KKK TTT KK N",
+            "print 'You are the real Hedy!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSS",
+            level="level13",lang='en')
+
+
+    def test_2(self):
+        self.assertHighlightedMultiLine(
+            "sword = 'lost'",
+            "TTTTT K SSSSSS",
+            "game = 'on'",
+            "TTTT K SSSS",
+            "print 'Our hero is walking through the forest'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'The path splits two ways'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "for i in range 0 to 2",
+            "KKK T KK KKKKK N KK N",
+            "if game is 'on'",
+            "KK TTTT KK SSSS",
+            "path = ask 'Which path should she choose?'",
+            "TTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "if path is 'left' and sword is 'found'",
+            "KK TTTT KK SSSSSS KKK TTTTT KK SSSSSSS",
+            "print 'Our hero comes across a dragon!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'Luckily our hero has a sword to defeat the beast!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "game = 'over'",
+            "TTTT K SSSSSS",
+            "if path is 'left' and sword is 'lost'",
+            "KK TTTT KK SSSSSS KKK TTTTT KK SSSSSS",
+            "print 'Our hero finds a dragon, but she doesnt have any weapons!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'Our hero is beaten by the dragon...'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'Try again'",
+            "KKKKK SSSSSSSSSSS",
+            "game = 'over'",
+            "TTTT K SSSSSS",
+            "if path is 'right' and sword is 'found'",
+            "KK TTTT KK SSSSSSS KKK TTTTT KK SSSSSSS",
+            "print 'You have already found the sword. There is nothing left here.'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'She walks back'",
+            "KKKKK SSSSSSSSSSSSSSSS",
+            "if path is 'right' and sword is 'lost'",
+            "KK TTTT KK SSSSSSS KKK TTTTT KK SSSSSS",
+            "print 'Our hero finds a sword'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSS",
+            "print 'This could come in very handy'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "sword = 'found'",
+            "TTTTT K SSSSSSS",
+            level="level13",lang='en')
+
+
+    def test_3(self):
+        self.assertHighlighted(
+            "# place your code here",
+            "CCCCCCCCCCCCCCCCCCCCCC",
+            level="level13",lang='en')
+
+
+    def test_4(self):
+        self.assertHighlightedMultiLine(
+            "options = 'rock', 'paper', 'scissors'",
+            "TTTTTTT K SSSSSSK SSSSSSSK SSSSSSSSSS",
+            "your_choice = ask 'What do you choose?'",
+            "TTTTTTTTTTT K KKK SSSSSSSSSSSSSSSSSSSSS",
+            "computer_choice = options at random",
+            "TTTTTTTTTTTTTTT K TTTTTTT KK KKKKKK",
+            "print 'You choose ' your_choice",
+            "KKKKK SSSSSSSSSSSSS TTTTTTTTTTT",
+            "print 'The computer chooses ' computer_choice",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSS TTTTTTTTTTTTTTT",
+            "if computer_choice is your_choice",
+            "KK TTTTTTTTTTTTTTT KK TTTTTTTTTTT",
+            "print 'Tie'",
+            "KKKKK SSSSS",
+            "if computer_choice is 'rock' and your_choice is 'paper'",
+            "KK TTTTTTTTTTTTTTT KK SSSSSS KKK TTTTTTTTTTT KK SSSSSSS",
+            "print 'You win!'",
+            "KKKKK SSSSSSSSSS",
+            "if computer_choice is 'rock' and your_choice is 'scissors'",
+            "KK TTTTTTTTTTTTTTT KK SSSSSS KKK TTTTTTTTTTT KK SSSSSSSSSS",
+            "print 'The computer wins!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSS",
+            level="level13",lang='en')
+
+
+    def test_5(self):
+        self.assertHighlightedMultiLine(
+            "price = 10",
+            "TTTTT K NN",
+            "food = ask 'What would you like to eat?'",
+            "TTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "drinks = ask 'What would you like to drink?'",
+            "TTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "if food is 'sandwich' and drinks is 'juice'",
+            "KK TTTT KK SSSSSSSSSS KKK TTTTTT KK SSSSSSS",
+            "print 'Thats our discount menu'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSS",
+            "price = price - 3",
+            "TTTTT K TTTTT K N",
+            "print 'That will be ' price ' dollars'",
+            "KKKKK SSSSSSSSSSSSSSS TTTTT SSSSSSSSSS",
+            level="level13",lang='en')
+
+
+    def test_6(self):
+        self.assertHighlightedMultiLine(
+            "drinks = ask 'What would you like to drink?'",
+            "TTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "if drinks is 'water' or drinks is 'juice'",
+            "KK TTTTTT KK SSSSSSS KK TTTTTT KK SSSSSSS",
+            "print 'Thats a healthy choice'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSS",
+            level="level13",lang='en')
+
+
+    def test_7(self):
+        self.assertHighlightedMultiLine(
+            "name is ask 'What is your name?'",
+            "TTTT KK KKK SSSSSSSSSSSSSSSSSSSS",
+            "password is ask 'What is your password?'",
+            "TTTTTTTT KK KKK SSSSSSSSSSSSSSSSSSSSSSSS",
+            "if name is 'Agent007' and password is 'TOPSECRET'",
+            "KK TTTT KK SSSSSSSSSS KKK TTTTTTTT KK SSSSSSSSSSS",
+            "print 'Go to the airport at 02.00'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "else",
+            "KKKK",
+            "print 'Go to the trainstation at 10.00'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            level="level13",lang='en')
+
+
+    def test_8(self):
+        self.assertHighlighted(
+            "## place your code here",
+            "CCCCCCCCCCCCCCCCCCCCCCC",
+            level="level13",lang='en')
+
+
+    def test_9(self):
+        self.assertHighlightedMultiLine(
+            "first_grade = ask 'What score did you get on your first test?'",
+            "TTTTTTTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "second_grade = ask 'What score did you get on your second test?'",
+            "TTTTTTTTTTTT K KKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "added is first_grade + second_grade",
+            "TTTTT KK TTTTTTTTTTT K TTTTTTTTTTTT",
+            "mean_grade is added / 2",
+            "TTTTTTTTTT KK TTTTT K N",
+            "if mean_grade = 1 or mean_grade = 2 or mean_grade = 3 or mean_grade = 4 or mean_grade = 5",
+            "KK TTTTTTTTTT K N KK TTTTTTTTTT K N KK TTTTTTTTTT K N KK TTTTTTTTTT K N KK TTTTTTTTTT K N",
+            "print 'Oh no! You have failed the subject...'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            "else",
+            "KKKK",
+            "print 'Great! You have passed the subject!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            level="level13",lang='en')
+
+
+    def test_10(self):
+        self.assertHighlighted(
+            "print 'Lets go to the next level!'",
+            "KKKKK SSSSSSSSSSSSSSSSSSSSSSSSSSSS",
+            level="level13",lang='en')
