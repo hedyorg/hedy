c9d26408ec068e93b9b3399f0d9b77b90c0d5dd1
Timon Bakker <timonbakker@hotmail.com>
2022-04-25 13:10:47 +0200

    Correctly reset level inputs

--- static/js/teachers.ts
+++ static/js/teachers.ts
@@ -379,6 +379,7 @@ export function remove_customizations(class_id: string, prompt: string) {
         }).done(function (response) {
             modal.alert(response.success, 3000, false);
             $('#remove_customizations_button').addClass('hidden');
+            $('.adventure_level_input').show();
             $('.adventure_level_input').prop('checked', true);
             $('.teacher_adventures_checkbox').prop('checked', false);
             $('.other_settings_checkbox').prop('checked', false);
