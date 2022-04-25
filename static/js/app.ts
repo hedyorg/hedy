b4df3382108018f013c723ac7daf5797d771da74
thjazi39 <duvillard.tiemen@gmail.com>
2022-04-25 10:14:12 +0200

    remove unused import

--- static/js/app.ts
+++ static/js/app.ts
@@ -1,7 +1,4 @@
 // It's important that this file gets loaded first
-/*import './syntaxLang-en';*/
-/*import './syntaxLang-es';*/
-/*import './syntaxLang-nl';*/
 import './syntaxModesRules';
 
 import { modal, error, success } from './modal';
