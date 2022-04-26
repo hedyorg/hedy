cf5d94e33c7470b403af62b3831d8e98832e1dfc
Felienne <felienne@gmail.com>
2022-04-26 15:21:42 +0200

    rename and clean keyword transformation

---
+++ content/transform_keywords_to_grammar.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+import os
+import copy
+import utils
+import yaml
+
+def nop(s):
+  return s
+
+
+def extract_Lark_grammar_from_yaml(only_new_lang=True):
+  """Creates a lark file in ../grammars/ for  all yaml files located in ../content/keywords/.
+  If a keyword is not yet translated, it will use the English translation of the keyword
+
+  Args:
+      only_new_lang (bool, optional): Specifies if only a lark file should be created for a new keyword language or for all languages. Defaults to True.
+  """
+  input_path = '../content/keywords/'
+  current_grammar_path = '../grammars/'
+  output_path = '../grammars-transformed/'
+  Path(output_path).mkdir(parents=True, exist_ok=True)
+
+  yaml_languages = [f.replace('.yaml', '') for f in os.listdir(input_path) if
+                    os.path.isfile(os.path.join(input_path, f)) and f.endswith('.yaml')]
+
+  lark_languages = [f.replace('keywords-', '').replace('.lark', '') for f in os.listdir(current_grammar_path) if
+                    os.path.isfile(os.path.join(current_grammar_path, f)) and f.startswith('keywords')]
+
+  for yaml_lang in yaml_languages:
+    if yaml_lang in lark_languages:
+      if only_new_lang:
+        continue
+    yaml_filesname_with_path = os.path.join(input_path, yaml_lang + '.yaml')
+    default_yaml_with_path = os.path.join(input_path, 'en' + '.yaml')
+
+    with open(default_yaml_with_path, 'r', encoding='utf-8') as stream:
+      en_command_combinations = yaml.safe_load(stream)
+
+    with open(yaml_filesname_with_path, 'r', encoding='utf-8') as stream:
+      command_combinations = yaml.safe_load(stream)
+
+    lark_filesname_with_path = os.path.join(output_path, 'keywords-' + yaml_lang + '.lark')
+
+    with open(lark_filesname_with_path, 'w+', encoding='utf-8') as f:
+      list_of_translations = []
+      
+      for command, translation in command_combinations.items():   
+        en_translation = en_command_combinations[command]
+        
+        if translation == '':
+          translation = en_translation
+          
+        if yaml_lang != 'en':
+          if translation in list_of_translations:
+            print(f'Warning! {translation} is a duplicate translation. This is not desired when creating lark files')
+          else:
+            list_of_translations.append(translation)
+
+        # random, left and right are tokens and need to be printed lowercase without _
+        if command != 'random' and  command != 'left' and  command != 'right' and type(command) != int:
+          command_upper = command.upper()
+          command = '_' + command_upper
+
+        if translation != en_translation:
+          f.write(f'{command}: "{translation}" | "{en_translation}"\n')
+        else:
+          f.write(f'{command}: "{translation}"\n')
+
+
+extract_Lark_grammar_from_yaml(True)
