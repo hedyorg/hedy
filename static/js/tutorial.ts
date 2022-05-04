9f1c0a602e89858c85cfa5fe38e79788c57f9e59
Timon Bakker <timonbakker@hotmail.com>
2022-05-04 16:06:16 +0200

    Reverse button disabling

--- static/js/tutorial.ts
+++ static/js/tutorial.ts
@@ -47,8 +47,6 @@ function runButtonStep() {
   $('#speak_container').hide();
 
   $('#runButtonContainer').addClass("z-40");
-  // Disable the button as we only want to show where it is -> not use it yet
-  $('#runit').prop("disabled",true);
 
   relocatePopup("middle");
   tutorialPopup("De uitvoer knop", "Met deze knop kun je een programma uitvoeren, zullen we het proberen?");
@@ -60,7 +58,6 @@ function tryRunButtonStep() {
 
   theGlobalEditor?.setValue("print Hallo wereld!\nprint Ik volg de Hedy tutorial");
   theGlobalEditor?.setOptions({readOnly: true});
-  $('#runit').prop("disabled",false);
 
   relocatePopup("bottom");
   tutorialPopup("Probeer het uit!", "Voer het programma uit, klik op 'volgende stap' als je klaar bent.");
