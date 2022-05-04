7fd19faae06b697768c8da187834cf78efa3e336
Timon Bakker <timonbakker@hotmail.com>
2022-05-04 12:20:54 +0200

    Hide on clicking next

--- static/js/tutorial.ts
+++ static/js/tutorial.ts
@@ -5,10 +5,19 @@ function hideFunctionality() {
   $('#developers_toggle_container').hide();
 }
 
+function callNextStep() {
+  return 0;
+  // We have to implement this
+}
+
 function tutorialPopup(title: string, message: string) {
   $('#tutorial_title').text(title);
   $('#tutorial_text').text(message);
   $('#tutorial-pop-up').fadeIn(1500);
+  $('#tutorial_next_button').off('click').on('click', () => {
+    $('#tutorial-pop-up').hide();
+    callNextStep();
+  });
 }
 
 export function startTutorial() {
