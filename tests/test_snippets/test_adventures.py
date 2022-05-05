f7939eae863c26ed3ff9e6af440da70b1c2a170a
Felienne <felienne@gmail.com>
2022-05-05 14:10:49 +0200

    fix issue found through testing local keywords

--- tests/test_snippets/test_adventures.py
+++ tests/test_snippets/test_adventures.py
@@ -75,6 +75,11 @@ def translate_keywords_in_snippets(snippets):
 
 Hedy_snippets = [(s.name, s) for s in collect_snippets(path='../../content/adventures')]
 
+# lang = 'hi' #useful if you want to test just 1 language
+# if lang:
+#     Hedy_snippets = [(name, snippet) for (name, snippet) in Hedy_snippets if snippet.language[:2] == lang]
+
+
 Hedy_snippets = translate_keywords_in_snippets(Hedy_snippets)
 
 class TestsAdventurePrograms(unittest.TestCase):
