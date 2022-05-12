785dca97db1f1cf43049f3e71617358bae0661e8
Timon Bakker <timonbakker@hotmail.com>
2022-05-12 12:16:49 +0200

    Only create .lark files for translated keywords

--- content/transform_keywords_to_grammar.py
+++ content/transform_keywords_to_grammar.py
@@ -28,6 +28,10 @@ def extract_Lark_grammar_from_yaml():
         with open(yaml_filesname_with_path, 'r', encoding='utf-8') as stream:
             command_combinations = yaml.safe_load(stream)
 
+        # We don't want to create the grammar if there are no translations
+        if en_command_combinations == command_combinations:
+            continue
+
         # Create an empty dictionary -> fill with english keywords and then overwrite all translated keywords
         translations = collections.defaultdict(lambda: 'Unknown Exception')
         translations.update(en_command_combinations)
