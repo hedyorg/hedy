ec0c2fcf3985ed3464acd60d179dd43a469d4b49
Timon Bakker <timonbakker@hotmail.com>
2022-05-16 10:51:12 +0200

    Add template and routing

--- website/admin.py
+++ website/admin.py
@@ -17,7 +17,6 @@ def routes(app, database):
             return utils.error_page(error=403, ui_message=gettext('unauthorized'))
         return render_template('admin/admin.html', page_title=gettext('title_admin'))
 
-
     @app.route('/admin/users', methods=['GET'])
     @requires_login
     def get_admin_users_page(user):
@@ -87,7 +86,6 @@ def routes(app, database):
                                language_filter=language, keyword_language_filter=keyword_language,
                                program_count=DATABASE.all_programs_count(), user_count=DATABASE.all_users_count())
 
-
     @app.route('/admin/classes', methods=['GET'])
     @requires_login
     def get_admin_classes_page(user):
@@ -129,4 +127,11 @@ def routes(app, database):
         if not is_admin(user):
             return utils.error_page(error=403, ui_message=gettext('unauthorized'))
         return render_template('admin/admin-stats.html', page_title=gettext('title_admin'))
+
+    @app.roud('/admin/achievements', methods=['GET'])
+    @requires_login
+    def get_admin_achievements_page(user):
+        if not is_admin(user):
+            return utils.error_page(error=403, ui_message=gettext('unauthorized'))
+        return render_template('admin/admin-achievements.html', page_title=gettext('title_admin'))
 
