45c2159505b3e20be9ba08401b52db2b3067ff0d
Timon Bakker <timonbakker@hotmail.com>
2022-05-17 10:57:31 +0200

    Fix dynamic loading of code values

--- static/js/quiz.ts
+++ static/js/quiz.ts
@@ -13,17 +13,32 @@ export function startQuiz(level: number) {
 export function loadQuestQuestion(level: number, question: number) {
     $.ajax({
       type: 'GET',
-      url: '/quiz/get-question/' + level + '/' + question,
+      url: '/quiz/get-question/' + level + '/' + 2,
       dataType: 'json'
     }).done(function(response: any) {
-        console.log(response);
         $('#quiz_container').show();
-        showQuestion();
+        console.log(response);
+        showQuestion(response.question.question_text);
+        if (response.question.code) {
+            console.log("Er is een code!");
+            showQuestionCode(response.question.code);
+        }
+        showAnswers(response.question.mp_choice_options);
     }).fail(function(err) {
        modal.alert(err.responseText, 3000, true);
     });
 }
 
-function showQuestion() {
-    console.log("todo...");
+function showQuestion(question: string) {
+    $('#quiz_question_title').text(question);
+}
+
+function showQuestionCode(code: string) {
+    $('#quiz_question_container').show();
+    let editor = ace.edit("quiz_question_code");
+    editor.setValue(code);
+}
+
+function showAnswers(options: any) {
+    console.log(options);
 }
