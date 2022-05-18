6f83769f8d16f0681893e06b7b765a9513ecb52b
Timon Bakker <timonbakker@hotmail.com>
2022-05-18 10:45:30 +0200

    Remove draggable functionality

--- static/js/app.ts
+++ static/js/app.ts
@@ -7,52 +7,6 @@ import { auth } from './auth';
 export let theGlobalEditor: AceAjax.Editor;
 export let theModalEditor: AceAjax.Editor;
 
-$(document).ready(function () {
-    var box = $(".parsons-box");
-
-    box.draggable({
-        helper: "clone",
-
-        start: function () {
-            $(this).css({
-                opacity: 0
-            });
-
-            $(".box").css("z-index", "0");
-        },
-
-        stop: function () {
-            $(this).css({
-                opacity: 1
-            });
-        }
-    });
-
-    box.droppable({
-        accept: box,
-
-        //@ts-ignore
-        drop: function (event, ui) {
-            var draggable = ui.draggable;
-            var droppable = $(this);
-            var dragPos = draggable.position();
-            var dropPos = droppable.position();
-
-            draggable.css({
-                left: dropPos.left + "px",
-                top: dropPos.top + "px",
-                "z-index": 20
-            });
-
-            droppable.css("z-index", 10).animate({
-                left: dragPos.left,
-                top: dragPos.top
-            });
-        }
-    });
-
-});
-
 (function() {
   // A bunch of code expects a global "State" object. Set it here if not
   // set yet.
