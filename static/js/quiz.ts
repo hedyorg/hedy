ac2a61b4d640f8503d4a7e7e49a6b3b4979afb64
Timon Bakker <timonbakker@hotmail.com>
2022-05-31 14:30:58 +0200

    Reverse whitespace whoopsie

--- static/js/quiz.ts
+++ static/js/quiz.ts
@@ -74,7 +74,7 @@ function showAnswers(options: any, level: number, question: number) {
             $('#answer_text_' + i).hide();
             let editor = ace.edit('answer_code_' + i);
             // This does look like magic: It removes all backticks and the resting newlines, tabs and whitespaces
-            editor.setValue(options[i-1].option.replace(new RegExp('`', 'g'),"").replace(/\s+/g, ""));
+            editor.setValue(options[i-1].option.replace(new RegExp('`', 'g'),"").replace(/\s+/g, " "));
             editor.clearSelection(); // Make sure the ace editor is not selected
             editor.renderer.$cursorLayer.element.style.display = "none"; // Also remove the cursor
             $('#answer_code_' + i).show();
@@ -174,7 +174,7 @@ function showFeedback(response: any, question: string, correct: boolean) {
     $('#feedback_feedback_text').text(response.feedback);
     if (response.correct_answer_text.includes("```")) {
         let editor = ace.edit("feedback_answer_code");
-        editor.setValue(response.correct_answer_text.replace(new RegExp('`', 'g'),"").replace(/\s+/g, ""));
+        editor.setValue(response.correct_answer_text.replace(new RegExp('`', 'g'),"").replace(/\s+/g, " "));
         editor.clearSelection(); // Make sure the ace editor is not selected
         editor.renderer.$cursorLayer.element.style.display = "none"; // Also remove the cursor
         $('#feedback_correct_answer_container').hide();
