94eb7823e178427b1db16c58a31b1dee6eee8b06
Timon Bakker <timonbakker@hotmail.com>
2022-06-07 13:27:35 +0200

    Removes quiz-css.css by placing all styling under Tailwind

--- static/js/quiz.ts
+++ static/js/quiz.ts
@@ -3,8 +3,13 @@ import {showAchievements} from "./app";
 
 (function() {
     $('.option-block').on("click", function () {
+        // Remove active attribute and hide possible answer button
         $('.option-block').removeClass('border-double border-8 active');
+        $('.submit-button').hide();
+
+        // Add active attribute and show correct answer button
         $(this).addClass('border-double border-8 active');
+        $(this).find(".submit-button").show();
     });
 })();
 
