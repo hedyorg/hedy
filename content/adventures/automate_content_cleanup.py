f87794f6be811d0e9e4504cd558d17ad1c232096
Timon Bakker <timonbakker@hotmail.com>
2022-06-13 10:35:33 +0200

    Reverse white line removal

--- content/adventures/automate_content_cleanup.py
+++ content/adventures/automate_content_cleanup.py
@@ -3,14 +3,10 @@ fw = open("en-cleaned.yaml", "w")
 
 lines = fr.readlines()
 counter = 0
-empty_counter = 0
 for line in lines:
     # Update this line for each language (manually...)
     if "## Example code" in line or "## Example Hedy code" in line or "## Example Hedy Code" in line:
         counter += 1
-    elif not line.strip():
-        empty_counter += 1
     else:
         fw.write(line)
 print(f"We have deleted {counter} lines")
-print(f"We have deleted {empty_counter} empty lines")
