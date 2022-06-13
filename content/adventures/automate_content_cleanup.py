e05060f9ed7a56759b6be489d339ab60d5ca4c5e
Timon Bakker <timonbakker@hotmail.com>
2022-06-13 10:38:03 +0200

    Fix en add nl clean-up

--- content/adventures/automate_content_cleanup.py
+++ content/adventures/automate_content_cleanup.py
@@ -1,11 +1,11 @@
-fr = open("en.yaml", "r")
-fw = open("en-cleaned.yaml", "w")
+fr = open("nl.yaml", "r")
+fw = open("nl-cleaned.yaml", "w")
 
 lines = fr.readlines()
 counter = 0
 for line in lines:
     # Update this line for each language (manually...)
-    if "## Example code" in line or "## Example Hedy code" in line or "## Example Hedy Code" in line:
+    if "## Voorbeeldcode" in line or "## Voorbeeld code" in line or "## Voorbeeld Hedy Code" in line or "## Voorbeeld Hedy code" in line:
         counter += 1
     else:
         fw.write(line)
