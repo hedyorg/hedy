4f89a8de584271a0f456102d75e87f95645403c1
Timon Bakker <timonbakker@hotmail.com>
2022-06-13 10:13:59 +0200

    Fixed en yaml

---
+++ content/adventures/automate_content_cleanup.py
@@ -0,0 +1,11 @@
+fr = open("en.yaml", "r")
+fw = open("en-cleaned.yaml", "w")
+
+lines = fr.readlines()
+counter = 0
+for line in lines:
+    if "## Example code" in line or "## Example Hedy code" in line or "## Example Hedy Code" in line:
+        counter += 1
+    else:
+        fw.write(line)
+print(f"We have deleted {counter} lines")
