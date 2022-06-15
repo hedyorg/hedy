3cb6fc8f34c21b34b02167c995e09ce8249c80dd
Timon Bakker <timonbakker@hotmail.com>
2022-06-15 08:26:14 +0200

    Fix a file oopsie, compile first version

--- static/js/index.ts
+++ static/js/index.ts
@@ -1,5 +1,3 @@
-import {modal} from "./modal";
-
 export * from './modal';
 export * from './app';
 export * from './auth';
@@ -10,39 +8,4 @@ export * from './parsons';
 import './syntaxModesRules';
 import './tabs';
 import './translating';
-import {loadQuestQuestion} from "./quiz";
 export * from './teachers';
-
-export function startQuiz(level: number) {
-    $('#start_parsons_container').hide();
-    $.ajax({
-      type: 'POST',
-      url: '/quiz/initialize_user',
-      data: JSON.stringify({
-        level: level
-      }),
-      contentType: 'application/json',
-      dataType: 'json'
-    }).done(function() {
-        loadParsonsExercise(level, 1);
-    }).fail(function(err) {
-       modal.alert(err.responseText, 3000, true);
-    });
-}
-
-export function loadParsonsExercise(level: number, question: number) {
-    $.ajax({
-      type: 'GET',
-      url: '/parsons/get-question/' + level + '/' + question,
-      dataType: 'json'
-    }).done(function(response: any) {
-        $('#parsons_container').show();
-        showExercise(response);
-    }).fail(function(err) {
-       modal.alert(err.responseText, 3000, true);
-    });
-}
-
-function showExercise(response: any) {
-    console.log("Let's show the exercise...");
-}
--- static/js/parsons.ts
+++ static/js/parsons.ts
@@ -1,11 +1,29 @@
-(function() {
-    $('.option-block').on("click", function () {
-        // Remove active attribute and hide possible answer button
-        $('.option-block').removeClass('border-double border-8 active');
-        $('.submit-button').hide();
+import {modal} from "./modal";
 
-        // Add active attribute and show correct answer button
-        $(this).addClass('border-double border-8 active');
-        $(this).find(".submit-button").show();
+(function() {
+        // We might not need this -> is there something we want to load on page load?
+    }
+)();
+
+export function startParsons(level: number) {
+    $('#start_parsons_container').hide();
+    loadParsonsExercise(level, 1);
+}
+
+export function loadParsonsExercise(level: number, question: number) {
+    $.ajax({
+      type: 'GET',
+      url: '/parsons/get-question/' + level + '/' + question,
+      dataType: 'json'
+    }).done(function(response: any) {
+        $('#parsons_container').show();
+        showExercise(response);
+    }).fail(function(err) {
+       modal.alert(err.responseText, 3000, true);
     });
-})();
+}
+
+function showExercise(response: any) {
+    console.log("Let's show the exercise...");
+    console.log(response);
+}
