34d41f347e302af170f40b591b5270839ec2fa59
Timon Bakker <timonbakker@hotmail.com>
2022-06-27 09:28:07 +0200

    Improve dict call

--- app.py
+++ app.py
@@ -1228,7 +1228,7 @@ def get_highscores_page(user, filter):
     # We have to make the data a bit nicer if possible
     highscores = copy.deepcopy(highscores)
     for highscore in highscores:
-        highscore['country'] = "-" if not highscore.get('country') else highscore.get('country')
+        highscore['country'] = highscore.get('country', '-')
         highscore['last_achievement'] = utils.delta_timestamp(highscore.get('last_achievement'))
     return render_template('highscores.html', highscores=highscores, has_country=True if country else False,
                            filter=filter, in_class=True if classes else False)
