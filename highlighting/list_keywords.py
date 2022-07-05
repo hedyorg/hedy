87dc63ac3801f6574c0f5cc2c97a7906c95512a3
thjazi39 <duvillard.tiemen@gmail.com>
2022-07-05 10:33:13 +0200

    fusion between keyword and number in list

--- highlighting/list_keywords.py
+++ highlighting/list_keywords.py
@@ -16,114 +16,155 @@
 # - K_SP
 #   This category of keywords allows you to have keywords that can be preceded immediately
 #   by another word, but that are not followed by another word.
-KEYWORDS = {
+LEVELS = {
     4 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from"],
         "K"       : [","],
         "SP_K"    : ["print","ask","sleep","forward","turn","random"],
         "K_SP"    : [],
+        "number"  : False,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     5 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","else"],
         "K"       : [","],
         "SP_K"    : ["print","ask","sleep","forward","turn","random"],
         "K_SP"    : [],
+        "number"  : False,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     6 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","else"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","forward","turn","random"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     7 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","else","repeat","times"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","forward","turn","random"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     8 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","repeat"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","forward","turn","random","else","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     9 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","repeat"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","forward","turn","random","else","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     10 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","repeat","for"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","forward","turn","random","else","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     11 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","for","range","to","repeat"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","random","else","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": False,
+        "ExtraRules" : [
+        ]
     },
     12 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","for","range","to","repeat"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","random","else","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": True ,
+        "ExtraRules" : [
+        ]
     },
     13 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","for","range","to","and","or","repeat"],
         "K"       : [",","-","=","/","\\*","\\+"],
         "SP_K"    : ["print","ask","sleep","random","else","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": True ,
+        "ExtraRules" : [
+        ]
     },
     14 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","for","range","to","and","or","else","repeat"],
         "K"       : [",","-","=","/","\\*","\\+","<",">","!"],
         "SP_K"    : ["print","ask","sleep","random","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": True ,
+        "ExtraRules" : [
+        ]
     },
     15 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","for","range","to","and","or","while","repeat"],
         "K"       : [",","-","=","/","\\*","\\+","<",">","!"],
         "SP_K"    : ["print","ask","sleep","random","else","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": True ,
+        "ExtraRules" : [
+        ]
     },
     16 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","else","for","range","to","and","or","while","repeat"],
         "K"       : [",","-","=","/","\\*","\\+","<",">","!","\\[","\\]"],
         "SP_K"    : ["print","ask","sleep","random","times"],
         "K_SP"    : [],
+        "number"  : True,
+        "number_with_decimal": True ,
+        "ExtraRules" : [
+        ]
     },
     17 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","else","for","range","to","and","or","while","repeat"],
         "K"       : [",","-","=","/","\\*","\\+","<",">","!","\\[","\\]",":"],
         "SP_K"    : ["print","ask","sleep","random","times"],
         "K_SP"    : ["elif"],
+        "number"  : True,
+        "number_with_decimal": True ,
+        "ExtraRules" : [
+        ]
     },
     18 :{
         "SP_K_SP" : ["is","at","add","to_list","remove","from","in","if","else","for","range","to","and","or","while","input","repeat"],
         "K"       : [",","-","=","/","\\*","\\+","<",">","!","\\[","\\]",":","\\(","\\)"],
         "SP_K"    : ["print","sleep","random","times"],
         "K_SP"    : ["elif"],
+        "number"  : True,
+        "number_with_decimal": True,
+        "ExtraRules" : [
+        ]
     },
-}
-
-# Rules concerning numbers (recognized or not, with or without decimals)
-NUMBERS = {
-    4  : {"number" : False , "number_with_decimal": False },
-    5  : {"number" : False , "number_with_decimal": False },
-    6  : {"number" : True  , "number_with_decimal": False },
-    7  : {"number" : True  , "number_with_decimal": False },
-    8  : {"number" : True  , "number_with_decimal": False },
-    9  : {"number" : True  , "number_with_decimal": False },
-    10 : {"number" : True  , "number_with_decimal": False },
-    11 : {"number" : True  , "number_with_decimal": False },
-    12 : {"number" : True  , "number_with_decimal": True  },
-    13 : {"number" : True  , "number_with_decimal": True  },
-    14 : {"number" : True  , "number_with_decimal": True  },
-    15 : {"number" : True  , "number_with_decimal": True  },
-    16 : {"number" : True  , "number_with_decimal": True  },
-    17 : {"number" : True  , "number_with_decimal": True  },
-    18 : {"number" : True  , "number_with_decimal": True  }
 }
