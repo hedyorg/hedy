dc67a01f11c3fd20d84f10e6caf39349671d516d
thjazi39 <duvillard.tiemen@gmail.com>
2022-07-25 14:04:05 +0200

    add new tests

---
+++ tests/test_highlighting/test_over_coloration.py
@@ -0,0 +1,68 @@
+from tests.Highlighter import HighlightTester
+from parameterized import parameterized
+
+class HighlighterTestOverCol(HighlightTester):
+
+    @parameterized.expand([
+        ("level2"),
+        ("level3"),
+        ("level4"),
+        ("level5"),
+        ("level6"),
+        ("level7"),
+        ("level8"),
+        ("level9"),
+        ("level10"),
+        ("level11"),
+    ])
+    def test_is1(self, level):
+        self.assert_highlighted_chr(
+            "ask is lost",
+            "TTT KK TTTT",
+            level=level, lang="en", intermediate_tests=False)
+
+
+
+    @parameterized.expand([
+        ("level12"),
+        ("level13"),
+        ("level14"),
+        ("level15"),
+        ("level16"),
+        ("level17"),
+    ])
+    def test_is2(self, level):
+        self.assert_highlighted_chr(
+            "ask is 'lost'",
+            "TTT KK SSSSSS",
+            level=level, lang="en", intermediate_tests=False)
+
+
+    @parameterized.expand([
+        ("level6"),
+        ("level7"),
+        ("level8"),
+        ("level9"),
+        ("level10"),
+        ("level11"),
+    ])
+    def test_Eq1(self, level):
+        self.assert_highlighted_chr(
+            "ask = lost",
+            "TTT K TTTT",
+            level=level, lang="en", intermediate_tests=False)
+
+    @parameterized.expand([
+        ("level12"),
+        ("level13"),
+        ("level14"),
+        ("level15"),
+        ("level16"),
+        ("level17"),
+    ])
+    def test_Eq2(self, level):
+        self.assert_highlighted_chr(
+            "ask = 'lost'",
+            "TTT K SSSSSS",
+            level=level, lang="en", intermediate_tests=False)
+
