9acbf50208100011cc3634cc0be75e3ea20b876c
Felienne <felienne@gmail.com>
2022-07-27 11:29:13 +0200

    Update highlighting/definition.py

--- highlighting/definition.py
+++ highlighting/definition.py
@@ -70,7 +70,7 @@ TOKEN_CONSTANT = "text"
 
 def get_translated_keyword(word, withoutGroup = False):
 	""" Function that allows to add double underscores around the keywords to be translated.
-		The "__" tags are added before and after only if the keyword belongs to the list.
+		The "__" are added before and after only if the keyword belongs to the list.
 
 		- withoutGroup : bool, Add parentheses for make a group or not
 	"""
