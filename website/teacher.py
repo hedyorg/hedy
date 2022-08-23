a1b68fe37c6e88a057c23663ab550a994f4c5df7
Felienne <felienne@gmail.com>
2022-08-23 13:22:10 +0200

    allow admins to view classes

--- website/teacher.py
+++ website/teacher.py
@@ -64,7 +64,7 @@ def routes(app, database, achievements):
     @requires_login
     def get_class(user, class_id):
         app.logger.info('This is info output')
-        if not is_teacher(user):
+        if not (is_teacher(user) or is_admin(user)):
             return utils.error_page_403(error=403, ui_message=gettext('retrieve_class_error'))
         Class = DATABASE.get_class(class_id)
         if not Class or Class['teacher'] != user['username']:
