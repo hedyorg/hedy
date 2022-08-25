b5f171d9a3396f5a59d892c6929a4a4c5274440f
Timon Bakker <timonbakker@hotmail.com>
2022-08-25 10:50:23 +0200

    Fix front-end filtering

--- website/admin.py
+++ website/admin.py
@@ -85,7 +85,7 @@ def routes(app, database):
             userdata.append(data)
 
         return render_template('admin/admin-users.html', users=userdata, page_title=gettext('title_admin'),
-                               filter=category, start_date=start_date, end_date=end_date, email_filter=substring,
+                               filter=category, start_date=start_date, end_date=end_date, text_filter=substring,
                                language_filter=language, keyword_language_filter=keyword_language)
 
     @app.route('/admin/classes', methods=['GET'])
