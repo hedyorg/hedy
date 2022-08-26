d43fec28d6a49d57fbcbb47ed1789fbde3d0d58c
Timon Bakker <timonbakker@hotmail.com>
2022-08-26 12:06:07 +0200

    Add updating op db code

--- update_classes.py
+++ update_classes.py
@@ -10,5 +10,17 @@ if not storage:
 USERS = dynamo.Table(storage, 'users', 'username', indexed_fields=[dynamo.IndexKey('email')])
 CLASSES = dynamo.Table(storage, 'classes', 'id', indexed_fields=[dynamo.IndexKey(v) for v in ['teacher', 'link']])
 
+# Get all classes
 classes = CLASSES.scan()
-print(classes)
+teacher_classes = {}
+
+# For each class -> change "teacher string" into "teachers set"
+for Class in classes:
+    CLASSES.update({'id': Class.get('id')}, {'teacher': None, 'teachers': {Class.get('teacher')}})
+    if not teacher_classes.get(Class.get('teacher')):
+        teacher_classes[Class.get('teacher')] = set()
+    teacher_classes[Class.get('teacher')].add(Class.get('id'))
+
+# Add "teacher_classes" to each user
+for teacher, classes in teacher_classes.items():
+    USERS.update({'username': teacher}, {'teacher_classes': classes})
