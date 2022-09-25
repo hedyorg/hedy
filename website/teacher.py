9eaff14915418c66daf92b23c494cceca35df9f5
Rico Huijbers <rix0rrr@gmail.com>
2022-09-25 23:09:00 +0200

    [FIX] Make pylint lint everything (#3316)
    Because the `validate-python` script explicitly lists files and
    directories to lint, parts of the code base were not being linted
    (leading to possibility of typo mistakes).
    
    Add the missing directories.
    
    Co-authored-by: Felienne Hermans <felienne@gmail.com>

--- website/teacher.py
+++ website/teacher.py
@@ -57,7 +57,7 @@ def routes(app, database, achievements):
     @requires_login
     def get_classes(user):
         if not is_teacher(user):
-            return utils.error_page_403(error=403, ui_message=gettext('retrieve_class_error'))
+            return utils.error_page(error=403, ui_message=gettext('retrieve_class_error'))
         return jsonify (DATABASE.get_teacher_classes(user['username'], True))
 
     @app.route('/for-teachers/class/<class_id>', methods=['GET'])
@@ -65,7 +65,7 @@ def routes(app, database, achievements):
     def get_class(user, class_id):
         app.logger.info('This is info output')
         if not is_teacher(user) and not is_admin(user):
-            return utils.error_page_403(error=403, ui_message=gettext('retrieve_class_error'))
+            return utils.error_page(error=403, ui_message=gettext('retrieve_class_error'))
         Class = DATABASE.get_class(class_id)
         if not Class or (Class['teacher'] != user['username'] and not is_admin(user)):
             return utils.error_page(error=404, ui_message=gettext('no_such_class'))
