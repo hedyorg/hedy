7c5061847041f80b34a8d48937c8e23cf5267fa4
Felienne Hermans <felienne@gmail.com>
2022-09-28 19:54:37 +0200

    Update website/website_module.py

--- website/website_module.py
+++ website/website_module.py
@@ -7,7 +7,7 @@ class WebsiteModule(flask.Blueprint):
 
     It's based on Flask Blueprints[1], but instead of defining an object and
     then registering routes on it, we put everything together in a class so that
-    the routes can also share private variables that are injected from 'app.py'.
+    the routes can also share private variables that are injected from 'app.py', for example the database. The alternative (using a global g.db) would be untyped and then the IDE lacks information about the type causing less autocomplete support.
 
     Heavily inspired by [2].
 
