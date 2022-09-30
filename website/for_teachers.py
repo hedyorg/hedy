8b1094d57826dcd17de5649d6d78cc7ae9a710c2
Timon Bakker <timonbakker@hotmail.com>
2022-09-30 13:39:47 +0200

    Fix storing of thresholds

--- website/for_teachers.py
+++ website/for_teachers.py
@@ -145,8 +145,6 @@ class ForTeachersModule(WebsiteModule):
             return utils.error_page(error=404, ui_message=gettext('no_such_class'))
 
         body = request.json
-        print(body)
-        return '', 400
         # Validations
         if not isinstance(body, dict):
             return gettext('ajax_error'), 400
@@ -160,6 +158,8 @@ class ForTeachersModule(WebsiteModule):
             return 'Other settings must be a list', 400
         if not isinstance(body.get('opening_dates'), dict):
             return 'Opening dates must be a dict', 400
+        if not isinstance(body.get('level_thresholds'), dict):
+            return 'Level thresholds must be a dict', 400
 
         # Values are always strings from the front-end -> convert to numbers
         levels = [int(i) for i in body['levels']]
@@ -178,13 +178,26 @@ class ForTeachersModule(WebsiteModule):
         for name, adventure_levels in body['adventures'].items():
             adventures[name] = [int(i) for i in adventure_levels]
 
+        level_thresholds = {}
+        for name, value in body.get('level_thresholds').items():
+            # We only manually check for the quiz threshold, if we add more -> generalize this code
+            if name == 'quiz_threshold':
+                try:
+                    value = int(value)
+                except:
+                    return 'Quiz threshold value is invalid', 400
+                if value < 0 or value > 100:
+                    return 'Quiz threshold value is invalid', 400
+            level_thresholds[name] = value
+
         customizations = {
             'id': class_id,
             'levels': levels,
             'opening_dates': opening_dates,
             'adventures': adventures,
             'teacher_adventures': body['teacher_adventures'],
-            'other_settings': body['other_settings']
+            'other_settings': body['other_settings'],
+            'level_thresholds': level_thresholds
         }
 
         self.db.update_class_customizations(customizations)
