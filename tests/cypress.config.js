6697b1e57d343b051184ee5e4ea031bf1517f680
Martin Janssen <martinjanssen01@gmail.com>
2022-10-14 15:54:12 +0200

    Disable watch for file changes

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '6y9x8a',
+  watchForFileChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
