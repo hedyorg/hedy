3297afc4e5a1f9b078fc9829d989b0b892f5c41e
Martin Janssen <martinjanssen01@gmail.com>
2022-10-14 16:01:35 +0200

    Fix cypress config

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -4,13 +4,14 @@ module.exports = defineConfig({
   projectId: '6y9x8a',
   watchForFileChanges: false,
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   env: {
-    login_page: '/login'
+    base_url: 'http://localhost:8080',
+    login_page: '/login',
     hedy_page: '/hedy'
   }
 });
