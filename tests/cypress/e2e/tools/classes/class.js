5a60ca15ab2fad88979329ce9615f9c02f51e88e
Martin Janssen <martinjanssen01@gmail.com>
2022-10-17 16:02:03 +0200

    Add create class

---
+++ tests/cypress/e2e/tools/classes/class.js
@@ -0,0 +1,20 @@
+import { loginForTeacher } from "../login/login";
+import { goToTeachersPage } from "../navigation/nav";
+
+export function createClass()
+{
+    goToTeachersPage();
+
+    // Click 'Create new class' button
+    cy.get('#teacher_classes > .green-btn').click();
+
+    // Type 'test class'
+    cy.get('#modal-prompt-input').type("test class");
+
+    // Click 'ok'
+    cy.get('#modal-ok-button').click();
+
+    goToTeachersPage();
+}
+
+export default {createClass};
