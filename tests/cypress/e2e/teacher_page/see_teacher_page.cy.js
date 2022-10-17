aa8545ca1ebcb3a887b1dfc7cd5ea7e2de49d4ce
Martin Janssen <martinjanssen01@gmail.com>
2022-10-17 15:05:04 +0200

    Add wait to check of location

--- tests/cypress/e2e/teacher_page/see_teacher_page.cy.js
+++ tests/cypress/e2e/teacher_page/see_teacher_page.cy.js
@@ -3,9 +3,12 @@ import {loginForAdmin, loginForTeacher} from '../tools/login/login.js'
 describe('Is able to see teacher page', () => {
   it('Passes', () => {
     loginForTeacher();
+    cy.wait(500);
     cy.location().then((loc) => {
       expect(loc.pathname).to.equal('/for-teachers');
-    })
+    });
+
+     
     
   })
 })
