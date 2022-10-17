9603d439e321aee90160935ce63dc7388e2943cd
Martin Janssen <martinjanssen01@gmail.com>
2022-10-17 20:30:31 +0200

    Add new tests

---
+++ tests/cypress/e2e/teacher_page/general/duplicate_class.cy.js
@@ -0,0 +1,23 @@
+import { createClass } from '../../tools/classes/class.js';
+import {loginForTeacher} from '../../tools/login/login.js'
+import { goToTeachersPage } from '../../tools/navigation/nav.js';
+
+describe('Is able to click on duplicate class', () => {
+  it('Passes', () => {
+    loginForTeacher();
+    createClass();
+    goToTeachersPage();
+
+    // Click on duplicate icon
+    cy.get('.no-underline > .fas').click();
+
+    // Checks for input field
+    cy.get('#modal-prompt-input').type('test class 2');
+    cy.get('#modal-ok-button').click();
+
+    goToTeachersPage();
+
+
+    
+  })
+})
