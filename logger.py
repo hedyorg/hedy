eb01f838c14861d8a8f455b76d4686b4995326e4
babaMar <stefano.messina@bigdatarepublic.nl>
2022-10-28 11:51:25 +0200

    Log level to info if NO_DEBUG_MODE env var is set

--- logger.py
+++ logger.py
@@ -1,11 +1,14 @@
+import os
 import logging
 
+LOG_LEVEL = logging.DEBUG if not os.getenv('NO_DEBUG_MODE') else logging.INFO
 LOG_FORMAT = '[%(asctime)s] %(filename)s %(funcName)s %(lineno)d <%(levelname)s>: %(message)s'
 logging.basicConfig(format=LOG_FORMAT)
 
 app_logger = logging.getLogger('app')
 log_queue_logger = logging.getLogger('querylog')
 jsonbin_logger = logging.getLogger('jsonbin')
+hedy_content_logger = logging.getLogger('hedy_content')
 
 for _logger in (app_logger, log_queue_logger, jsonbin_logger):
-    _logger.setLevel(logging.DEBUG)
+    _logger.setLevel(LOG_LEVEL)
