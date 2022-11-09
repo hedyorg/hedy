7eb75792c4a955cc85f81ee0c7c5312208d55a6c
Menno Liefstingh <menno.liefstingh@vantage-ai.com>
2022-11-09 16:07:37 +0100

    Updates website_module.py to be pep8-compliant

--- website/website_module.py
+++ website/website_module.py
@@ -1,19 +1,23 @@
 import collections
-import functools
+
 import flask
+
 
 class WebsiteModule(flask.Blueprint):
     """A website module is a class with its own routes that can be mounted into an app.
 
     It's based on Flask Blueprints[1], but instead of defining an object and
     then registering routes on it, we put everything together in a class so that
-    the routes can also share private variables that are injected from 'app.py', for example the database. The alternative (using a global g.db) would be untyped and then the IDE lacks information about the type causing less autocomplete support.
+    the routes can also share private variables that are injected from 'app.py', for example the database.
+    The alternative (using a global g.db) would be untyped and then the IDE lacks information about the type,
+    causing less autocomplete support.
 
     Heavily inspired by [2].
 
     [1] https://flask.palletsprojects.com/en/2.2.x/blueprints/
     [2] https://gist.github.com/dplepage/2024199
     """
+
     def __init__(self, name: str, import_name: str, url_prefix=None):
         """Initialize the Blueprint.
 
@@ -30,12 +34,14 @@ class WebsiteModule(flask.Blueprint):
             for rd in getattr(fn, '_routing_data', []):
                 self.route(*rd.args, **rd.kwargs)(fn)
 
+
 RoutingData = collections.namedtuple('RoutingData', ['args', 'kwargs'])
+
 
 def route(*args, **kwargs):
     def wrap(fn):
-        l = getattr(fn, '_routing_data', [])
-        l.append(RoutingData(args, kwargs))
-        setattr(fn, '_routing_data', l)
+        routing_data = getattr(fn, '_routing_data', [])
+        routing_data.append(RoutingData(args, kwargs))
+        setattr(fn, '_routing_data', routing_data)
         return fn
     return wrap
