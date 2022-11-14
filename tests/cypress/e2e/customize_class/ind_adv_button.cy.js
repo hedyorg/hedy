ffebfa3742aaac7f861e8c962c0bd94731b08002
Sharanda <s2630575@vuw.leidenuniv.nl>
2022-11-14 14:12:50 +0100

    final tests

--- tests/cypress/e2e/customize_class/ind_adv_button.cy.js
+++ tests/cypress/e2e/customize_class/ind_adv_button.cy.js
@@ -1,5 +1,5 @@
-import {loginForAdmin, loginForTeacher} from '../../tools/login/login.js'
-import { createClass } from "../../tools/classes/class";
+import {loginForAdmin, loginForTeacher} from '../tools/login/login.js'
+import { createClass } from "../tools/classes/class";
 //import { goToPage } from "../navigation/nav";
 
 describe('Testing individual adventure buttons', () => {
@@ -9,7 +9,14 @@ describe('Testing individual adventure buttons', () => {
     
     createClass();
     cy.get(':nth-child(3) > .no-underline').click()
-    cy.get('.green-btn').contains("Customize class").click()
+    //cy.get('.green-btn').contains("Customize class").click()
+
+    var currentUrl = '';
+    cy.url().then(url => {
+      currentUrl = url;
+      let classUrl = `[onclick="window.location.href = '/for-teachers/customize-class/` + currentUrl.substring(currentUrl.indexOf('class/')+6) + `'"]`; // get statistics class url
+      cy.get(classUrl).click(); // Press class statistics button
+    })
 
     cy.get('#level_button_1').click()
     cy.get('.adventure_level_1').should('be.not.checked')
--- tests/cypress/e2e/customize_class/opening_dates.cy.js
+++ tests/cypress/e2e/customize_class/opening_dates.cy.js
@@ -1,5 +1,5 @@
-import {loginForAdmin, loginForTeacher} from '../../tools/login/login.js'
-import { createClass } from "../../tools/classes/class";
+import {loginForAdmin, loginForTeacher} from '../tools/login/login.js'
+import { createClass } from "../tools/classes/class";
 //import { goToPage } from "../navigation/nav";
 
 // Test is incomplete, because it only checks if it is not empty instead of the value
@@ -12,10 +12,37 @@ describe('Testing if opening dates is not empty', () => {
     
     createClass();
     cy.get(':nth-child(3) > .no-underline').click()
-    cy.get('.green-btn').contains("Customize class").click()
+    //cy.get('.green-btn').contains("Customize class").click()
 
-    //cy.get('#opening_date_level_1 > .ltr\:pl-2 > .opening_date_input').click()
-    cy.get("#opening_date_level_1").type("2022-12-01").should("not.be.empty")
+    var currentUrl = '';
+    cy.url().then(url => {
+      currentUrl = url;
+      let classUrl = `[onclick="window.location.href = '/for-teachers/customize-class/` + currentUrl.substring(currentUrl.indexOf('class/')+6) + `'"]`; // get statistics class url
+      cy.get(classUrl).click(); // Press class statistics button
+    })
+
+    // The following line has a bug in cypress:
+    // cy.get("#opening_date_level_1").type("2022-12-01").should("have.value", "2023-01-01")
+    // The following tests only check if the field is not empty:
+    cy.get("#opening_date_level_1").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_2").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_3").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_4").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_5").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_6").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_7").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_8").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_9").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_10").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_11").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_12").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_13").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_14").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_15").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_16").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_17").type("2023-01-01").should("not.be.empty")
+    cy.get("#opening_date_level_18").type("2023-01-01").should("not.be.empty")
+
 
   })
 })
