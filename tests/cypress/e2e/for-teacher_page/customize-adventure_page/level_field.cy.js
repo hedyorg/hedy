6ddc2b2230d6bb465380eb9bd9fda5a47b63e70f
Jimmy Oei <jimmy.oei10@gmail.com>
2022-11-21 16:33:55 +0100

    level field test works

---
+++ tests/cypress/e2e/for-teacher_page/customize-adventure_page/level_field.cy.js
@@ -0,0 +1,15 @@
+import {loginForTeacher} from '../../tools/login/login.js'
+import {goToEditAdventure} from '../../tools/navigation/nav.js'
+
+describe('Level Field test', () => {
+  it('passes', () => {
+    loginForTeacher();
+    goToEditAdventure();
+
+    // Tests level field interaction
+    cy.get('#custom_adventure_level')
+      .should('be.visible')
+      .select('1')
+      .should('have.value', '1')
+  })
+})
