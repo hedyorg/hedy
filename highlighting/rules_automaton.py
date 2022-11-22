c1b5f861eb549808466fca558be760d659e2d847
Jesús Pelay <45865185+jpelay@users.noreply.github.com>
2022-11-22 08:41:13 -0400

    [FIX] Fix highlighting to use unicode categories (#3690)
    **Description**
    
    Changes the regex used to detect a character and the end of a word in the highliter, preventing problems with it not detecting end of word correctly in languages that don't use the Latin alphabet.
    
    **Fixes #3476**
    
    **How to test**
    
    Change language to hebrew and try this code in level 5:
    ```
    לכל מספר בתוך טווח 1 עד 5
        הדפס מספר
    ```
    
    **Checklist**
    Done? Check if you have it all in place using this list:*
      
    - [ ] Contains one of the PR categories in the name
    - [ ] Describes changes in the format above
    - [ ] Links to an existing issue or discussion 
    - [ ] Has a "How to test" section
    
    If you're unsure about any of these, don't hesitate to ask. We're here to help!

--- highlighting/rules_automaton.py
+++ highlighting/rules_automaton.py
@@ -9,26 +9,32 @@ def rule_level1():
             'regex': START_LINE + get_translated_keyword("ask"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("print"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("echo"),
             'token': ["text",'keyword'], 
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("forward"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("turn"),
             'token': ["text",'keyword'],
             'next': 'direction',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("color"),
             'token': ["text",'keyword'],
             'next': 'color',
+            'unicode': True
         }],
     "value" : [],
     "color" : [{
@@ -46,6 +52,7 @@ def rule_level1():
                     get_translated_keyword("yellow",True) + \
                 ")",
             'token': [TOKEN_CONSTANT],
+            'unicode': True
         }],
     "direction" : [{
             'regex': "(" +\
@@ -53,6 +60,7 @@ def rule_level1():
                     get_translated_keyword("left",True) +\
                 ")",
             'token': [TOKEN_CONSTANT],
+            'unicode': True
         }]
     })
 
@@ -62,30 +70,37 @@ def rule_level2() :
             'regex': START_LINE + get_translated_keyword("print"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + WORD + SPACE + get_translated_keyword("is") + SPACE + get_translated_keyword("ask"),
             'token': ["text","text","text",'keyword',"text","keyword"],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + WORD + SPACE + get_translated_keyword("is"),
             'token': ["text","text","text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("sleep"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("forward"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("turn"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         },{
             'regex': START_LINE + get_translated_keyword("color"),
             'token': ["text",'keyword'],
             'next': 'value',
+            'unicode': True
         }],
     "value" : [{
             'regex': "(" +\
@@ -102,6 +117,7 @@ def rule_level2() :
                     get_translated_keyword("yellow",True) +\
                 ")",
             'token': [TOKEN_CONSTANT],
+            'unicode': True
         }]
     })
 
@@ -110,48 +126,60 @@ def rule_level3():
         'regex': START_LINE + WORD + SPACE + get_translated_keyword("is") + "( *)" + get_translated_keyword("ask"),
         'token': ["text",'text','text','keyword','text','keyword'],
         'next': 'valueExpr',
+        'unicode': True
     },{
         'regex': START_LINE + WORD + SPACE + get_translated_keyword("is"),
         'token': ["text",'text','text','keyword'],
         'next': 'value',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("print") ,
         'token': ['text','keyword'],
         'next': 'valueExpr',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("turn") ,
         'token': ['text','keyword'],
         'next': 'valueExpr',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("sleep") ,
         'token': ['text','keyword'],
         'next': 'valueExpr',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("forward") ,
         'token': ['text','keyword'],
         'next': 'valueExpr',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("add"),
         'token': ["text",'keyword'],
         'next': 'valAdd',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("remove"),
         'token': ["text",'keyword'],
         'next': 'valRemove',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("color"),
         'token': ["text",'keyword'],
         'next': 'value',
+        'unicode': True
     }],
     "value" : [{
         'regex': START_WORD + get_translated_keyword("at") + SPACE + get_translated_keyword("random") ,
         'token': ['text','keyword','keyword','keyword'],
+        'unicode': True
     },{
         'regex': START_WORD + get_translated_keyword("at") + END_WORD,
         'token': ['text','keyword'],
+        'unicode': True
     },{
         'regex': get_translated_keyword("comma") ,
         'token': ['keyword'],
+        'unicode': True
     },{
         'regex': "(" +\
                 get_translated_keyword("black",True) + "|" +\
@@ -167,18 +195,22 @@ def rule_level3():
                 get_translated_keyword("yellow",True) +\
             ")",
         'token': [TOKEN_CONSTANT],
+        'unicode': True
     }],
     "valueExpr" : [{
         'regex': START_WORD + get_translated_keyword("at") + SPACE + get_translated_keyword("random") ,
         'token': ['text','keyword','keyword','keyword'],
+        'unicode': True
     },{
         'regex': START_WORD + get_translated_keyword("at") + END_WORD,
         'token': ['text','keyword'],
+        'unicode': True
     }],
     "valAdd"    : [{
         'regex': START_WORD + get_translated_keyword("to_list") + END_WORD ,
         'token': ['text','keyword'],
         'next': 'valueTo',
+        'unicode': True
     },{
         'regex': "(" +\
                 get_translated_keyword("black",True) + "|" +\
@@ -194,12 +226,14 @@ def rule_level3():
                 get_translated_keyword("yellow",True) +\
             ")",
         'token': [TOKEN_CONSTANT],
+        'unicode': True
     }],
     "valueTo" : [],
     "valRemove" : [{
         'regex': START_WORD + get_translated_keyword("from") + END_WORD ,
         'token': ['text','keyword'],
         'next': 'valueFrom',
+        'unicode': True
     },{
         'regex': "(" +\
                 get_translated_keyword("black",True) + "|" +\
@@ -215,6 +249,7 @@ def rule_level3():
                 get_translated_keyword("yellow",True) +\
             ")",
         'token': [TOKEN_CONSTANT],
+        'unicode': True
     }],
     "valueFrom" : [],
     })
@@ -225,48 +260,60 @@ def rule_level4():
         'regex': START_LINE + WORD + SPACE + get_translated_keyword("is") + "( *)" + get_translated_keyword("ask"),
         'token': ["text",'text','text','keyword','text','keyword'],
         'next': 'valueExpr',
+        'unicode': True
     },{
         'regex': START_LINE + WORD + SPACE + get_translated_keyword("is"),
         'token': ["text",'text','text','keyword'],
         'next': 'value',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("print") ,
         'token': ['text','keyword'],
         'next': 'valueExpr',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("turn") ,
         'token': ['text','keyword'],
         'next': 'valueSimple',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("sleep") ,
         'token': ['text','keyword'],
         'next': 'valueSimple',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("forward") ,
         'token': ['text','keyword'],
         'next': 'valueSimple',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("color"),
         'token': ["text",'keyword'],
         'next': 'valueSimple',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("add"),
         'token': ["text",'keyword'],
         'next': 'valAdd',
+        'unicode': True
     },{
         'regex': START_LINE + get_translated_keyword("remove"),
         'token': ["text",'keyword'],
         'next': 'valRemove',
+        'unicode': True
     }],
     "value" : [{
         'regex': START_WORD + get_translated_keyword("at") + SPACE + get_translated_keyword("random") ,
         'token': ['text','keyword','keyword','keyword'],
+        'unicode': True
     },{
         'regex': START_WORD + get_translated_keyword("at") + END_WORD,
         'token': ['text','keyword'],
+        'unicode': True
     },{
         'regex': get_translated_keyword("comma") ,
         'token': ['keyword'],
+        'unicode': True
     },{
         'regex': "(" +\
                 get_translated_keyword("black",True) + "|" +\
@@ -282,34 +329,43 @@ def rule_level4():
                 get_translated_keyword("yellow",True) +\
             ")",
         'token': [TOKEN_CONSTANT],
+        'unicode': True
     }],
     "valueExpr" : [{
         'regex': START_WORD + get_translated_keyword("at") + SPACE + get_translated_keyword("random") ,
         'token': ['text','keyword','keyword','keyword'],
+        'unicode': True
     },{
         'regex': START_WORD + get_translated_keyword("at") + END_WORD,
         'token': ['text','keyword'],
+        'unicode': True
     },{
         'regex': '\"[^\"]*\"',
         'token': 'constant.character',
+        'unicode': True
     },{
         'regex': "\'[^\']*\'",
         'token': 'constant.character',
+        'unicode': True
     },{
         'regex': '\"[^\"]*$',
         'token': 'constant.character',
-        'next' : 'start'
+        'next' : 'start',
+        'unicode': True
     },{
         'regex': "\'[^\']*$",
         'token': 'constant.character',
-        'next' : 'start'
+        'next' : 'start',
+        'unicode': True
     }],
     "valueSimple":[{
         'regex': START_WORD + get_translated_keyword("at") + SPACE + get_translated_keyword("random") ,
         'token': ['text','keyword','keyword','keyword'],
+        'unicode': True
     },{
         'regex': START_WORD + get_translated_keyword("at") + END_WORD,
         'token': ['text','keyword'],
+        'unicode': True
     },{
         'regex': "(" +\
                 get_translated_keyword("black",True) + "|" +\
@@ -325,11 +381,13 @@ def rule_level4():
                 get_translated_keyword("yellow",True) +\
             ")",
         'token': [TOKEN_CONSTANT],
+        'unicode': True
     }],
     "valAdd"    : [{
         'regex': START_WORD + get_translated_keyword("to_list") + END_WORD ,
         'token': ['text','keyword'],
         'next': 'valueTo',
+        'unicode': True
     },{
         'regex': "(" +\
                 get_translated_keyword("black",True) + "|" +\
@@ -345,12 +403,14 @@ def rule_level4():
                 get_translated_keyword("yellow",True) +\
             ")",
         'token': [TOKEN_CONSTANT],
+        'unicode': True
     }],
     "valueTo" : [],
     "valRemove" : [{
         'regex': START_WORD + get_translated_keyword("from") + END_WORD ,
         'token': ['text','keyword'],
         'next': 'valueFrom',
+        'unicode': True
     },{
         'regex': "(" +\
                 get_translated_keyword("black",True) + "|" +\
@@ -366,6 +426,7 @@ def rule_level4():
                 get_translated_keyword("yellow",True) +\
             ")",
         'token': [TOKEN_CONSTANT],
+        'unicode': True
     }],
     "valueFrom" : [],
     })
