e97e301f6b85cde830b8bc27ca7173cb98796352
Jimmy Oei <jimmy.oei10@gmail.com>
2022-12-08 15:43:02 +0100

    period toggles tests

---
+++ tests/cypress/e2e/admin_page/stats-page/past_year_toggle.cy.js
@@ -0,0 +1,13 @@
+import { loginForAdmin } from '../../tools/login/login.js'
+import { goToAdminStatsPage } from '../../tools/navigation/nav.js';
+
+describe('Past year toggle', () => {
+  it('passes', () => {
+    loginForAdmin();
+    goToAdminStatsPage()
+
+    cy.get('#period_toggle_past_year')
+      .should('be.visible')
+      .should('be.not.disabled')
+  })
+})
