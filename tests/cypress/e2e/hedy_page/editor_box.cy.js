5b8531a104d320c5d1207c8628e840a5d61ead69
Timon Bakker <timonbakker@hotmail.com>
2022-12-09 12:42:48 +0100

    Simplify test

--- tests/cypress/e2e/hedy_page/editor_box.cy.js
+++ tests/cypress/e2e/hedy_page/editor_box.cy.js
@@ -4,16 +4,8 @@ describe('Is able to type in the editor box', () => {
     it('Passes', () => {
       goToHedyPage();
 
-      // This doesn't work!
-      cy.get('#editor > textarea').clear({force: true});
       cy.get('#editor').type('ask What is your name?\necho hello');
-      cy.get('#editor > .ace_scroller > .ace_content .ace_line').each((element, index) => {
-        if(index == 0) {
-          cy.get(element).should('have.text', 'ask What is your name?');
-        }
-        if(index == 1) {
-          cy.get(element).should('have.text', 'echo hello');
-        }
-      })
+      cy.get('#editor > .ace_scroller > .ace_content .ace_line').should('have.text', 'ask What is your name?');
+      cy.get('#editor > .ace_scroller > .ace_content .ace_line').should('have.text', 'echo hello');
     })
 })
