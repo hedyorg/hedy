5a8bef5f13dcd808ed2d37664ca3d2bd26ec37bf
Timon Bakker <timonbakker@hotmail.com>
2022-12-09 12:26:37 +0100

    Make sure the editor box is empty when testing

--- tests/cypress/e2e/hedy_page/editor_box.cy.js
+++ tests/cypress/e2e/hedy_page/editor_box.cy.js
@@ -4,15 +4,13 @@ describe('Is able to type in the editor box', () => {
     it('Passes', () => {
       goToHedyPage();
 
-      cy.get('#editor > .ace_scroller > .ace_content').type('\nask What is your name\necho hello');
+      cy.get('textarea').clear({force: true})
+      cy.get('#editor').type('ask What is your name\necho hello');
       cy.get('#editor > .ace_scroller > .ace_content .ace_line').each((element, index) => {
         if(index == 0) {
-          cy.get(element).should('have.text', 'print hello world!');
+          cy.get(element).should('have.text', 'ask What is your name?');
         }
         if(index == 1) {
-          cy.get(element).should('have.text', 'ask What is your name');
-        }
-        if(index == 2) {
           cy.get(element).should('have.text', 'echo hello');
         }
       })
