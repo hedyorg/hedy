b1be39e3216aaeb6ad2e67d236f85e0415586cd6
Timon Bakker <timonbakker@hotmail.com>
2022-12-09 12:41:49 +0100

    Make editor check non-language dependent

--- tests/cypress/e2e/hedy_page/editor_box.cy.js
+++ tests/cypress/e2e/hedy_page/editor_box.cy.js
@@ -4,8 +4,9 @@ describe('Is able to type in the editor box', () => {
     it('Passes', () => {
       goToHedyPage();
 
-      cy.get('textarea').clear({force: true})
-      cy.get('#editor').type('ask What is your name\necho hello');
+      // This doesn't work!
+      cy.get('#editor > textarea').clear({force: true});
+      cy.get('#editor').type('ask What is your name?\necho hello');
       cy.get('#editor > .ace_scroller > .ace_content .ace_line').each((element, index) => {
         if(index == 0) {
           cy.get(element).should('have.text', 'ask What is your name?');
