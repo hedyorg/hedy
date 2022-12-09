7272c45d9c6e019efa7c054ebd98aa3301095e4a
reiades <59891012+reiades@users.noreply.github.com>
2022-12-09 11:12:07 -0500

    Add new test cases

--- tests/test_level/test_level_16.py
+++ tests/test_level/test_level_16.py
@@ -567,15 +567,25 @@ class TestsLevel16(HedyTester):
 
         self.single_level_tester(code=code, expected=expected)
 
-    
     def test_at_random_express(self):
         code = textwrap.dedent("""\
             fruit is ['banaan', 'appel', 'kers']
             randomfruit is fruit at random
             print randomfruit""")
+    def test_at_random_express_eq(self):
+        code = textwrap.dedent("""\
+            fruit is ['banaan', 'appel', 'kers']
+            randomfruit = fruit at random
+            print randomfruit""")
         self.single_level_tester(code=code, exception=hedy.exceptions.InvalidRandomCommandException)
     def test_at_random_print(self):
         code = textwrap.dedent("""\
             fruit is ['banaan', 'appel', 'kers']
+            print fruit at random""")
+        self.single_level_tester(code=code, exception=hedy.exceptions.InvalidRandomCommandException)
+    def test_at_random_print_2(self):
+        code = textwrap.dedent("""\
+            fruit is ['banaan', 'appel', 'kers']
+            print fruit[random]
             print fruit at random""")
         self.single_level_tester(code=code, exception=hedy.exceptions.InvalidRandomCommandException)
