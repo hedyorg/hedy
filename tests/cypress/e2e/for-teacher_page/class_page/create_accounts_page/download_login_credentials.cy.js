9711fc50930eea448b7a096b86b9afa873ddeeed
Elyanne <s2626500@vuw.leidenuniv.nl>
2022-12-12 00:37:06 +0100

    made a small change to download test

--- tests/cypress/e2e/for-teacher_page/class_page/create_accounts_page/download_login_credentials.cy.js
+++ tests/cypress/e2e/for-teacher_page/class_page/create_accounts_page/download_login_credentials.cy.js
@@ -10,13 +10,13 @@ describe('Is able to go to logs page', () => {
     cy.get(".view_class").first().click(); // Press view class button
     cy.get('#add-student').click();
     cy.get('#create-accounts').click(); 
-    cy.get(':nth-child(2) > #username').type("student10");
+    cy.get(':nth-child(2) > #username').type("student14");
     cy.get(':nth-child(2) > #password').type("123456");
-    cy.get(':nth-child(3) > #username').type("student11");
+    cy.get(':nth-child(3) > #username').type("student15");
     cy.get(':nth-child(3) > #password').type("123456");
-    cy.get(':nth-child(4) > #username').type("student12");
+    cy.get(':nth-child(4) > #username').type("student16");
     cy.get(':nth-child(4) > #password').type("123456");
-    cy.get(':nth-child(5) > #username').type("student13");
+    cy.get(':nth-child(5) > #username').type("student17");
     cy.get(':nth-child(5) > #password').type("123456");
     cy.wait(1000);
 
