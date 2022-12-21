72739ff7e874a8ec0d83c1649c700a51b7ec266b
Timon Bakker <timonbakker@hotmail.com>
2022-12-21 16:39:50 +0100

    Fix forced keywords for parsons and quiz

--- website/parsons.py
+++ website/parsons.py
@@ -10,10 +10,13 @@ class ParsonsModule(WebsiteModule):
 
         self.parsons = parsons
 
-    @route("/get-exercise/<int:level>/<int:exercise>", methods=["GET"])
-    def get_parsons_exercise(self, level, exercise):
+    @route("/get-exercise/<int:level>/<int:exercise>", methods=["GET"], defaults={'keyword_lang': None})
+    @route("/get-exercise/<int:level>/<int:exercise>/<keyword_lang>", methods=["GET"])
+    def get_parsons_exercise(self, level, exercise, keyword_lang):
         if exercise > self.parsons[g.lang].get_highest_exercise_level(level) or exercise < 1:
             return gettext("exercise_doesnt_exist"), 400
-
-        exercise = self.parsons[g.lang].get_parsons_data_for_level_exercise(level, exercise, g.keyword_lang)
+        if keyword_lang:
+            exercise = self.parsons[g.lang].get_parsons_data_for_level_exercise(level, exercise, keyword_lang)
+        else:
+            exercise = self.parsons[g.lang].get_parsons_data_for_level_exercise(level, exercise, g.keyword_lang)
         return jsonify(exercise), 200
