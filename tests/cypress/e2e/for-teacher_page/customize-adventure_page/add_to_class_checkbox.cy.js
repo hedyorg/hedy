cb919ed7c5dbb3231e9991d1bc305c1cd2542d4e
Jesús Pelay <45865185+jpelay@users.noreply.github.com>
2023-01-22 09:29:25 -0400

    [FIX] Fixing broken customization of adventures (#3962)
    **Now fill out the remainder of this template by replacing all _italic_ content**
    
    **Description**
    
    There was a bug where class without customizations, adventures wouldn't load correctly. Also there were other edge cases fixed:
    
    * Need to update the adventure creation page.
    
    
    **Checklist**
    Done? Check if you have it all in place using this list:*
      
    - [ ] Contains one of the PR categories in the name
    - [ ] Describes changes in the format above
    - [ ] Links to an existing issue or discussion 
    - [ ] Has a "How to test" section
    
    If you're unsure about any of these, don't hesitate to ask. We're here to help!

--- tests/cypress/e2e/for-teacher_page/customize-adventure_page/add_to_class_checkbox.cy.js
+++ tests/cypress/e2e/for-teacher_page/customize-adventure_page/add_to_class_checkbox.cy.js
@@ -8,12 +8,14 @@ describe('Add to class checkbox test', () => {
 
     // Tests the first class out of all the classes we can add to
     // It does not matter which one we take (we choose the first one)
-    cy.get(':nth-child(1) > .customize_adventure_class_checkbox')
-      .should('be.visible')
-      .should('not.be.disabled')
-      .check()
-      .should('be.checked')
-      .uncheck()
-      .should('not.be.checked');
+  
+  // Disabling this test as we enable this functionality again
+  //   cy.get(':nth-child(1) > .customize_adventure_class_checkbox')
+  //     .should('be.visible')
+  //     .should('not.be.disabled')
+  //     .check()
+  //     .should('be.checked')
+  //     .uncheck()
+  //     .should('not.be.checked');
   })
 })
