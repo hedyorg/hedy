67ecab397490c34ac5b5e8834e5fdbff2b0ee7a6
Jesús Pelay <pelayjesus@gmail.com>
2023-01-31 11:54:44 -0400

    Revert "removing waits from login helpers"
    This reverts commit 2d76235ee8bfc5eaef5bf810bb93328ed863d90b.

--- tests/cypress/e2e/tools/login/login.js
+++ tests/cypress/e2e/tools/login/login.js
@@ -2,18 +2,22 @@ import { goToHome, goToLogin } from "../navigation/nav";
 
 export function loginForUser() {
     login("user1", "123456");
+    cy.wait(500);
 }
 
 export function loginForTeacher() {
     login("teacher1", "123456");
+    cy.wait(500);
 }
 
 export function loginForStudent() {
     login("student1", "123456");
+    cy.wait(500);
 }
 
 export function loginForAdmin() {
-    login("admin", "123456");    
+    login("admin", "123456");
+    cy.wait(500);
 }
 
 export function login(username, password) {
@@ -21,6 +25,7 @@ export function login(username, password) {
     cy.get('#username').type(username);
     cy.get('#password').type(password);
     cy.get('#login_button').click();
+    cy.wait(500);
 }
 
 export function logout()
@@ -30,7 +35,9 @@ export function logout()
         if ($body.find(".menubar-text:contains('Log in')").length == 0) {
             
             cy.get('.dropdown > .menubar-text').click();
-            cy.get('#logout_button').click();   
+            cy.get('#logout_button').click();
+            cy.wait(500);
+            
         } 
     });
 }
