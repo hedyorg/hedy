5c47c33b70d10a976f4b9ae1640b24349de09800
Martin Janssen <martinjanssen01@gmail.com>
2023-02-02 16:16:39 +0100

    Fix for typing issue

--- tests/cypress/e2e/hedy_page/editor_box.cy.js
+++ tests/cypress/e2e/hedy_page/editor_box.cy.js
@@ -3,9 +3,12 @@ import {goToHedyPage} from "../tools/navigation/nav";
 describe('Is able to type in the editor box', () => {
     it('Passes', () => {
       goToHedyPage();
-
+      // click on textaread to get focus
+      cy.get('#editor > .ace_scroller > .ace_content').click();
+      // empty textarea
+      cy.focused().clear()
       cy.get('#editor').type('ask What is your name?\necho hello');
-      cy.get('#editor > .ace_scroller > .ace_content').should('have.text', 'ask What is your name?');
-      cy.get('#editor > .ace_scroller > .ace_content').should('have.text', 'echo hello');
+      cy.get('#editor > .ace_scroller > .ace_content').should('contain.text', 'ask What is your name?');
+      cy.get('#editor > .ace_scroller > .ace_content').should('contain.text', 'echo hello');
     })
 })
