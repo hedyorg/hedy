3082626642a44c0dd7d2828b5804bd2137fd90cf
Jesse Hoobergs <jesse@codebergs.com>
2023-02-10 14:03:31 +0100

    Fix cypress in codespaces

--- .devcontainer/create.sh
+++ .devcontainer/create.sh
@@ -3,4 +3,9 @@ set -e
 pre-commit install
 pybabel compile -f -d translations
 cp -r /var/tmp/node_modules . 
-echo "export BASE_URL=\"https://${CODESPACE_NAME}-8080.preview.app.github.dev\"" >> ~/.bashrc
+
+if [[ -z "${BASE_URL}" ]]; then
+  echo "export BASE_URL=\"https://${CODESPACE_NAME}-8080.preview.app.github.dev\"" >> ~/.bashrc
+fi
+
+
