dfe488bc896607e37715dafcec842797b06cfced
Rico Huijbers <rix0rrr@gmail.com>
2023-03-18 20:41:34 +0100

    Fix skipping initial saved program

--- website/dynamo.py
+++ website/dynamo.py
@@ -1031,6 +1031,7 @@ class QueryIterator:
     def __init__(self, pagination_token=None):
         drop_initial = self._analyze_pagination_token(pagination_token)
         self.page = None
+        self.have_eof = None
 
         # Eat records according to the amount we needed to drop
         i = 0
@@ -1044,14 +1045,22 @@ class QueryIterator:
         if not isinstance(self.page, ResultPage):
             raise RuntimeError('_do_fetch must return a ResultPage')
 
+        self.have_eof = len(self.page) == 0
+
     def _do_fetch(self):
         raise NotImplementedError('_do_fetch should be implemented')
 
     @property
     def eof(self):
+        if self.have_eof:
+            # This removes the need for fetches just to answer the EOF question
+            return self.have_eof
+
         if self.page is None:
             self._fetch_next_page()
-        return self.i >= len(self.page)
+
+        # If we have a page, we're at eof if we're at the end of the last page
+        return self.i >= len(self.page) and not self.page.next_page_token
 
     def advance(self):
         if self.page is None:
@@ -1059,9 +1068,13 @@ class QueryIterator:
             return
 
         self.i += 1
-        if self.eof and self.page.next_page_token:
+        if self.i >= len(self.page) and self.page.next_page_token:
             self.pagination_token = self.page.next_page_token
-            self._fetch_next_page()
+            if self.pagination_token is None: self.have_eof = True
+
+            # Reset self.page, so we don't retrieve the next page unnecessarily
+            # but the next lookup will fetch it
+            self.page = None
 
     @property
     def current(self):
@@ -1089,14 +1102,14 @@ class QueryIterator:
             return 0
 
         parts = x.split('@')
-        self.pagination_token = parts[0]
+        self.pagination_token = parts[0] or None
         return int(parts[1])
 
     @property
     def next_page_token(self):
         if self.eof:
             return None
-        return f'{self.pagination_token}@{self.i}'
+        return f'{self.pagination_token or ""}@{self.i}'
 
 
 class PythonQueryIterator:
@@ -1108,10 +1121,11 @@ class PythonQueryIterator:
         self.iter = iter
 
     def __next__(self):
-        self.iter.advance()
         if self.iter.eof:
             raise StopIteration()
-        return self.iter.current
+        ret = self.iter.current
+        self.iter.advance()
+        return ret
 
 
 class GetManyIterator(QueryIterator):
