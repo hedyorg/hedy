73bc7679517f3e1ef9a1ca3c458f6de8c6a17d2c
Jesús Pelay <pelayjesus@gmail.com>
2023-03-22 18:42:50 -0400

    adding all languages

--- tests/cypress/e2e/hedy_page/editor_box.cy.js
+++ tests/cypress/e2e/hedy_page/editor_box.cy.js
@@ -1,12 +1,12 @@
 const YAML = require('js-yaml')
 
 describe('Is able to type in the editor box', () => {
-  const LANGUAGES_TO_TEST = ['en', 'nl', 'fr'];
+  const LANGUAGES_TO_TEST = ['ar', 'bg', 'bn', 'ca', 'cs', 'cy', 'da', 'de', 'en','el', 'eo', 'es', 'et', 'fa', 'fi', 'fr', 'fy', 'he', 'hi', 'hu', 'id', 'it', 'ja', 'kmr', 'ko', 'nb_NO', 'nl', 'pa_PK', 'pl', 'pt_BR', 'pt_PT', 'ro', 'ru', 'sq', 'sr', 'sv', 'sw', 'te', 'th']
 
   // Do something for every language
   for (const language of LANGUAGES_TO_TEST) {
 
-    it(`in ${language}`, () => {
+    it(`Language ${language} should run`, () => {
       cy.visit(`${Cypress.env('hedy_page')}?language=${language}`);
 
       // click on textaread to get focus
