9299127d71a3ba4c43a07dbee18149bd62b32f8f
Felienne <felienne@gmail.com>
2023-03-31 14:32:51 +0200

    oops missed an occurrence

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,9 +4,9 @@
 def worker_exit(server, worker):
     # When the worker is being exited (perhaps because of a timeout),
     # give the query_log handler a chance to flush to disk.
-    from website import querylog, jsonbin
+    from website import querylog, s3_logger
     querylog.emergency_shutdown()
-    jsonbin.emergency_shutdown()
+    s3_logger.emergency_shutdown()
 
 
 def post_fork(server, worker):
