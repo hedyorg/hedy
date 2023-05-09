92fa6edbadd51131f2cca1affd9c6a83bce4f8bd
Jesús Pelay <pelayjesus@gmail.com>
2023-05-09 13:28:07 -0400

    adding function to render minimum level

--- app.py
+++ app.py
@@ -1075,7 +1075,6 @@ def teacher_tutorial(user):
 @app.route('/onlinemasters', methods=['GET'], defaults={'level': '1', 'program_id': None})
 @app.route('/onlinemasters/<int:level>', methods=['GET'], defaults={'program_id': None})
 @app.route('/space_eu', methods=['GET'], defaults={'level': '1', 'program_id': None})
-@app.route('/hedy', methods=['GET'], defaults={'level': '1', 'program_id': None})
 @app.route('/hedy/<level>', methods=['GET'], defaults={'program_id': None})
 @app.route('/hedy/<level>/<program_id>', methods=['GET'])
 def index(level, program_id):
@@ -1087,7 +1086,6 @@ def index(level, program_id):
         return utils.error_page(error=404, ui_message=gettext('no_such_level'))
 
     loaded_program = None
-
     if program_id:
         result = DATABASE.program_by_id(program_id)
         if not result or not current_user_allowed_to_see_program(result):
@@ -1240,6 +1238,17 @@ def index(level, program_id):
             initial_tab=initial_tab,
             current_user_name=current_user()['username'],
         ))
+
+
+@app.route('/hedy', methods=['GET'])
+def index_level():
+    if current_user()['username']:
+        highest_quiz = get_highest_quiz_level(current_user()['username'])
+        # This function returns the character '-' in case there are no finished quizes
+        highest_level = 0 if highest_quiz == '-' else highest_quiz
+        return index(highest_level + 1, None)
+    else:
+        return index(1, None)
 
 
 @app.route('/hedy/<id>/view', methods=['GET'])
