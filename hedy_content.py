18e23bcc733c7af95b219748c4714bbf542c65fb
Felienne Hermans <felienne@gmail.com>
2023-06-29 12:13:34 +0200

    add clear to commands

--- hedy_content.py
+++ hedy_content.py
@@ -41,7 +41,8 @@ KEYWORDS_ADVENTURES = set([
     'for_command',
     'and_or_command',
     'while_command',
-    'elif_command'
+    'elif_command',
+    'clear_command'
 ])
 
 ADVENTURE_ORDER_PER_LEVEL = {
