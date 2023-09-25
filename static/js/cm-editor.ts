6c131ab8abcd81bd2b7d960eb824f9279ec8f41d
Jesús Pelay <pelayjesus@gmail.com>
2023-09-25 11:26:25 -0400

    move decorations to their own file

--- static/js/cm-editor.ts
+++ static/js/cm-editor.ts
@@ -1,93 +1,14 @@
 import { HedyEditor, EditorType, Breakpoints, HedyEditorCreator, EditorEvent } from "./editor";
 import { basicSetup } from 'codemirror';
-import { Decoration, DecorationSet, EditorView, ViewUpdate } from '@codemirror/view'
-import { EditorState, Compartment, StateEffect, StateField, Prec } from '@codemirror/state'
+import { EditorView, ViewUpdate } from '@codemirror/view'
+import { EditorState, Compartment, StateEffect, Prec } from '@codemirror/state'
 import { oneDark } from '@codemirror/theme-one-dark';
 import { EventEmitter } from "./event-emitter";
 import { deleteTrailingWhitespace } from '@codemirror/commands'
-
-
-const addErrorLine = StateEffect.define<{row: number}>();
-const addErrorWord = StateEffect.define<{row: number, col: number}>();
-const removeErrorMarkers = StateEffect.define<void>();
-
-const addDebugLine = StateEffect.define<{row: number}>();
-const removeDebugLine = StateEffect.define<void>();
-
-const errorLineField = StateField.define<DecorationSet>({
-    create() {
-      return Decoration.none;
-    },
-    update(errors, tr) {
-      errors = errors.map(tr.changes);
-      for (let e of tr.effects) {
-        if (e.is(addErrorLine)) {
-            // Get line given the row number
-            const line = tr.state.doc.line(e.value.row);
-            errors = errors.update({
-                add: [errorHighlightLine.range(line.from, line.from)]
-            });
-        } else if(e.is(addErrorWord)) {
-            const line = tr.state.doc.line(e.value.row);
-            const length = line.text.slice(e.value.col - 1).split(/(\s+)/)[0].length;
-            if (length > 0) {
-                errors = errors.update({
-                    add: [errorHighlightMark.range(line.from + e.value.col - 1, line.from + e.value.col - 1 + length)]
-                });
-            } else { // If we can't find the word, highlight the whole line
-                errors = errors.update({
-                    add: [errorHighlightLine.range(line.from, line.from)]
-                });
-            }
-        }
-        else if (e.is(removeErrorMarkers)) {
-            // Just return the empty decoration set to remove all of the errors
-            return Decoration.none;
-        }
-      } 
-      return errors;
-    },
-    provide: f => EditorView.decorations.from(f)
-});
-
-const debugLineField = StateField.define<DecorationSet>({
-    create() {
-      return Decoration.none;
-    },
-    update(errors, tr) {
-      errors = errors.map(tr.changes);
-      for (let e of tr.effects) {
-        if (e.is(addDebugLine)) {
-            // Get line given the row number
-            const line = tr.state.doc.line(e.value.row);
-            errors = errors.update({
-                add: [debugLine.range(line.from, line.from)]
-            });
-        } else if (e.is(removeDebugLine)) {
-            // Just return the empty decoration set to remove all of the errors
-            return Decoration.none;
-        }
-      } 
-      return errors;
-    },
-    provide: f => EditorView.decorations.from(f)
-})
-  
-const errorHighlightLine = Decoration.line({class: "cm-error-editor"});
-const errorHighlightMark = Decoration.mark({class: "cm-error-editor"});
-
-const debugLine = Decoration.line({class: "cm-debugger-current-line"});
-
-const decorationsTheme = EditorView.theme({
-    ".cm-error-editor": {
-        outline: "2px solid #F56565",
-        backgroundColor: "rgba(66, 153, 225, 0.7)",
-        color: "white"
-    },
-    ".cm-debugger-current-line": {
-        backgroundColor: "#2D6099"
-    },
-});
+import { 
+    errorLineField, debugLineField, decorationsTheme, addDebugLine, 
+    addErrorLine, addErrorWord, removeDebugLine, removeErrorMarkers
+} from "./cm-decorations";
 
 export class HedyCodeMirrorEditorCreator implements HedyEditorCreator {
     /**
