b6c2ddd533278b2e2681ec2244e95ee289ddb7d6
Toni <t.skulj@hotmail.com>
2023-10-16 17:24:18 +0200

    Strip Hedy code

--- hedy.py
+++ hedy.py
@@ -2823,7 +2823,7 @@ def transpile(input_string, level, lang="en", skip_faulty=True):
         transpile_result = transpile_inner(input_string, level, lang, populate_source_map=True)
 
     except Exception as original_error:
-        hedy_amount_lines = len(input_string.split('\n'))
+        hedy_amount_lines = len(input_string.strip().split('\n'))
 
         if getenv('ENABLE_SKIP_FAULTY', False) and skip_faulty and hedy_amount_lines > 1:
             if isinstance(original_error, source_map.exceptions_not_to_skip):
