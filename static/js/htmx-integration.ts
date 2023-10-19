000bd7f2d45912c7575a0d32ea215bf4bc9ce254
hasan-sh <hasansh343@gmail.com>
2023-10-19 04:00:22 +0000

    fix errors caused by autopep8

--- static/js/htmx-integration.ts
+++ static/js/htmx-integration.ts
@@ -1,50 +1,47 @@
-import {initializeHighlightedCodeBlocks
-        import showAchievements} from './app'
-import {Achievement} from './types'
-import {modal} from './modal'
-import {ClientMessages} from './client-messages'
-import Sortable = require("sortablejs")
 /**
-* Custom integrations we have with HTMX
-*/
+ * Custom integrations we have with HTMX
+ */
+import { initializeHighlightedCodeBlocks, showAchievements } from './app';
+import { ClientMessages } from './client-messages';
+import { modal } from './modal';
+import { Achievement } from './types';
+import Sortable = require("sortablejs");
 
-declare const htmx: typeof import ('./htmx')
+declare const htmx: typeof import('./htmx');
 
 /**
-* Disable elements as they are being used to submit HTMX requests.
-*
-* Prevents impatient kids from double-triggering server-side events.
-*/
+ * Disable elements as they are being used to submit HTMX requests.
+ *
+ * Prevents impatient kids from double-triggering server-side events.
+ */
 htmx.defineExtension('disable-element', {
-    onEvent: function(name, evt) {
-        let elt= evt.detail.elt;
-        if (!elt.getAttribute) {
-            return;
-        }
+  onEvent: function (name, evt) {
+      let elt = evt.detail.elt;
+      if (!elt.getAttribute) {
+        return;
+      }
 
-        let target= elt.getAttribute("hx-disable-element") ?? 'self';
-        let targetElement = (target == "self") ? elt: document.querySelector(target);
+      let target = elt.getAttribute("hx-disable-element") ?? 'self';
+      let targetElement = (target == "self") ? elt : document.querySelector(target);
 
-        if (name === "htmx:beforeRequest" & & targetElement) {
-            targetElement.disabled= true;
-        } else if (name == "htmx:afterRequest" & & targetElement) {
-            targetElement.disabled= false;
-        }
-    }
+      if (name === "htmx:beforeRequest" && targetElement) {
+          targetElement.disabled = true;
+      } else if (name == "htmx:afterRequest" && targetElement) {
+          targetElement.disabled = false;
+      }
+  }
 });
 
 /**
-* We have some custom JavaScript to run on new content that's loaded into the DOM.
-*
-* (Notably: turning < pre > s into Ace editors)
-*/
-htmx.onLoad((content)= > {
-    initializeHighlightedCodeBlocks(content)
-    var sortables=  content.querySelectorAll('.sortable')
-    for (let i=0
-         i < sortables.length
-         i++) {
-        var sortable= sortables[i] as HTMLElement
+ * We have some custom JavaScript to run on new content that's loaded into the DOM.
+ *
+ * (Notably: turning <pre>s into Ace editors)
+ */
+htmx.onLoad((content) => {
+    initializeHighlightedCodeBlocks(content);
+    var sortables =  content.querySelectorAll('.sortable');
+    for (let i = 0; i < sortables.length; i++) {
+        var sortable = sortables[i] as HTMLElement;
         new Sortable(sortable, {
             animation: 150,
             ghostClass: 'drop-adventures-active'
@@ -53,29 +50,29 @@ htmx.onLoad((content)= > {
 });
 
 interface HtmxEvent {
-    readonly xhr: XMLHttpRequest
-    readonly error: string
+    readonly xhr: XMLHttpRequest;
+    readonly error: string;
 }
 
 /**
-* If the server reports an error, we send it into our regular error popup
-*/
-htmx.on('htmx:responseError', (ev)= > {
-    const event = ev as CustomEvent < HtmxEvent >
-    const xhr: XMLHttpRequest= event.detail.xhr
-    const genericError= event.detail.error
-    modal.notifyError(xhr.responseText.length < 1000 ? xhr.responseText: genericError)
+ * If the server reports an error, we send it into our regular error popup
+ */
+htmx.on('htmx:responseError', (ev) => {
+    const event = ev as CustomEvent<HtmxEvent>;
+    const xhr: XMLHttpRequest = event.detail.xhr;
+    const genericError = event.detail.error;
+    modal.notifyError(xhr.responseText.length < 1000 ? xhr.responseText : genericError);
 });
 
-htmx.on('htmx:sendError', ()= > {
-    modal.notifyError(`${ClientMessages.Connection_error} ${ClientMessages.CheckInternet}`)
-})
+htmx.on('htmx:sendError', () => {
+    modal.notifyError(`${ClientMessages.Connection_error} ${ClientMessages.CheckInternet}`);
+});
 
 
 /**
-* The server can trigger achievement events
-*/
-htmx.on('displayAchievements', (ev)= > {
+ * The server can trigger achievement events
+ */
+htmx.on('displayAchievements', (ev) => {
     const payloads = (ev as any).detail.value
     for (const payload of payloads) {
         const achievement= payload["achievement"] as Achievement[]
