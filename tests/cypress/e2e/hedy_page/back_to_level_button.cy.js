3921525ae75f0c55731bbbaaa7041956e3bcd785
hasan-sh <hasansh343@gmail.com>
2023-11-06 12:23:58 +0100

    test going back

--- tests/cypress/e2e/hedy_page/back_to_level_button.cy.js
+++
@@ -1,8 +0,0 @@
-import {goToHedyLevel2Page, goToHedyPage} from "../tools/navigation/nav";
-
-describe('Is able to use "Go back to level x" button', () => {
-    it('Passes', () => {
-      // make sure to text navigating to next levels
-      // after allowing the user to do so.
-    })
-  })
--- tests/cypress/e2e/hedy_page/to_level_button.cy.js
+++ tests/cypress/e2e/hedy_page/to_level_button.cy.js
@@ -1,8 +1,23 @@
-import {goToHedyPage} from "../tools/navigation/nav";
+import { loginForStudent } from "../tools/login/login";
+import {goToHedyLevel2Page} from "../tools/navigation/nav";
 
-describe('Is able to use "Go to level x" button', () => {
-    it('Passes', () => {
-      // make sure to text navigating to next levels
-      // after allowing the user to do so.
-    })
+describe('Go to levels buttons', () => {
+  it('Is able to use "Go back to level x" button', () => {
+    loginForStudent('student5')
+    // Test when code is unchanged
+    goToHedyLevel2Page();
+    cy.get('#prev_level_button').click();
+    cy.url().should('include', Cypress.env('hedy_page'));
+
+    // Test when code is changed
+    goToHedyLevel2Page();
+    cy.get('#editor').type('hello');
+    cy.get('#prev_level_button').click();
+    cy.url().should('include', Cypress.env('hedy_page'));
   })
+
+  it('Is able to use "Go to level x" button', () => {
+    // TODO: add go to next when there's no quiz and parsons.
+    // but if go back passes, this should also pass, so no rush!
+  })
+})
