2bdb0d0ffc1d6db565dd9a2b45fe21bcc8988aa5
Yusuf <71043418+akseron@users.noreply.github.com>
2023-11-06 22:51:00 +0100

    debugging var_access _at var_access

--- hedy.py
+++ hedy.py
@@ -1126,6 +1126,7 @@ class IsValid(Filter):
 
     def error_list_access(self, meta, args):
         # for now copying another function, to test whether anything is being done at all
+
         error = InvalidInfo('misspelled "at" command', arguments=[str(args[0])], line=meta.line, column=meta.column)
         return False, error, meta
 
@@ -1341,9 +1342,15 @@ class ConvertToPython(Transformer):
             return args
         else:
             # TODO: check whether this is really never raised??
+            # It is being raised! In case of the following command: 'print animals at inde'
+            # In this case 'animals' is a correct list and 'inde' is an unset variable
+            # This means the var setting has to be altered a bit here
             # return first name with issue
             first_unquoted_var = unquoted_args[0]
-            raise exceptions.UndefinedVarException(name=first_unquoted_var, line_number=var_access_linenumber)
+            for a in unquoted_args:
+                if a == 'None':
+                    a = first_unquoted_var
+                    raise exceptions.UndefinedVarException(name=first_unquoted_var, line_number=var_access_linenumber)
 
     # static methods
     @staticmethod
