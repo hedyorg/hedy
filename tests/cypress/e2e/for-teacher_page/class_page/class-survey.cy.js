2aaeeda3f3bf82c6094bdfcf1ce2d654d63034d2
annelein <anneleinvanreijen@gmail.com>
2023-11-27 15:49:41 +0100

    class-survey.cy.js

--- tests/cypress/e2e/for-teacher_page/class_page/class-survey.cy.js
+++ tests/cypress/e2e/for-teacher_page/class_page/class-survey.cy.js
@@ -1,82 +1,48 @@
-import {loginForTeacher, logout} from '../../tools/login/login.js'
-import { createClass} from '../../tools/classes/class'
+import {loginForTeacher} from '../../tools/login/login.js'
+import {createClassAndAddStudents} from '../../tools/classes/class.js'
+import { goToTeachersPage } from '../../tools/navigation/nav.js';
 
-const surveyView = body => body.find("#survey")
+let classname;
+
+beforeEach(() => {
+    loginForTeacher("teacher4");
+    ({classname} = createClassAndAddStudents());
+    goToTeachersPage();
+    cy.get(".view_class").contains(new RegExp(`^${classname}$`)).click();
+})
+
 describe('Class Survey View', () => {
-  it('Can respond to 1 questions', () => {
-    
-    loginForTeacher();
-    cy.wait(500);
-
-    cy.get("#total_students").should('not.have.value', '0').then(
-        cy.get(".view_class").first().click(),
-            cy.get("body").then(surveyView).then(survey => {
-                if (survey.length){
-                    survey = cy.get("#survey")
-                    survey.should("exist")
-                    survey.get("#input").type("test")
-                    survey.get("#submit").click()
-                }
-            })
-    )
+  it('Can first respond to 1 question, then to last 3 questions', () => {
+    cy.get("#input").type("test");
+    cy.get("#submit").click();
+    goToTeachersPage();
+    cy.get(".view_class").contains(new RegExp(`^${classname}$`)).click();
+    var surveyInputs = Array.from({length:3},(v, k)=>k+1)
+    cy.wrap(surveyInputs).each((index) => {
+        cy.getBySel("input_" + index)
+            .type("test")
+            .invoke('val').then((text) => {
+            expect('test').to.equal(text);
+            });
+        });
+    cy.get("#submit").click();
+    goToTeachersPage();
+    cy.get(".view_class").contains(new RegExp(`^${classname}$`)).click();
+    cy.get("#survey").should('not.exist');
   })
 
-  it('Should have 3 questions remaning, can respond to those', () => {
-    
-    loginForTeacher();
-    cy.wait(500);
 
-    cy.get("#total_students").should('not.have.value', '0').then(
-        cy.get(".view_class").first().click(),
-        cy.get("body").then(surveyView).then(survey => {
-            if (survey.length){
-                survey = cy.get("#survey")
-                survey.should("exist")
-                var surveyInputs = Array.from({length:3},(v, k)=>k+1)
-                cy.wrap(surveyInputs).each((index) => {
-                    cy.getBySel("input_" + index)
-                      .type("test")
-                      .invoke('val').then((text) => {
-                        expect('test').to.equal(text);
-                      });
-                  });
-                survey.get("#submit").click()
-            }
-        })
-    )
+  it('Can be skipped and survey is not shown after', () => {
+    cy.get("#skip").click();
+    goToTeachersPage();
+    cy.get(".view_class").contains(new RegExp(`^${classname}$`)).click();
+    cy.get("#survey").should('not.exist');
   })
 
-  it('Can be skipped - never show again', () => {
-    
-    loginForTeacher();
-    cy.wait(500);
-
-    cy.get("#total_students").should('not.have.value', '0').then(
-        cy.get(".view_class").eq(2).click(),
-            cy.get("body").then(surveyView).then(survey => {
-                if (survey.length){
-                    survey = cy.get("#survey")
-                    survey.should("exist")
-                    survey.get("#skip").click()
-                }
-            })
-    )
-  })
-
-  it('Can be skipped - remind me later', () => {
-    
-    loginForTeacher();
-    cy.wait(500);
-
-    cy.get("#total_students").should('not.have.value', '0').then(
-        cy.get(".view_class").eq(3).click(),
-            cy.get("body").then(surveyView).then(survey => {
-                if (survey.length){
-                    survey = cy.get("#survey")
-                    survey.should("exist")
-                    survey.get("#remind_later").click()
-                }
-            })
-    )
+  it('Can be skipped and survey is not shown after', () => {
+    cy.get("#remind_later").click();
+    goToTeachersPage();
+    cy.get(".view_class").contains(new RegExp(`^${classname}$`)).click();
+    cy.get("#survey").should('not.exist');
   })
 })
