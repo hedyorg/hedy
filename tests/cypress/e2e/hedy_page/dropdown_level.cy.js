5749220ad4bd3d3ce8199168c5bb9653383989a1
annelein <anneleinvanreijen@gmail.com>
2023-11-28 16:25:48 +0100

    create new class for test

--- tests/cypress/e2e/hedy_page/dropdown_level.cy.js
+++ tests/cypress/e2e/hedy_page/dropdown_level.cy.js
@@ -1,5 +1,6 @@
 import { loginForStudent, loginForTeacher } from "../tools/login/login";
-import {goToHedyLevel2Page} from "../tools/navigation/nav";
+import {goToHedyLevel2Page, goToTeachersPage} from "../tools/navigation/nav";
+import {createClassAndAddStudents} from '../tools/classes/class.js'
 
 describe('Go to level dropdown', () => {
   it('Is able to use the level dropdown to go to level 1', () => {
@@ -11,22 +12,24 @@ describe('Go to level dropdown', () => {
     cy.url().should('include', Cypress.env('hedy_page'));
   })
 
-  it('Is not able to go to disabled level 5', () => {
+  it('Is not able to go to disabled level 5', () => {  
+    let classname;
+    let students;
+
     loginForTeacher();
     cy.wait(500);
 
-    cy.getBySel('view_class_link').first().click();
+    ({classname, students} = createClassAndAddStudents());
+    goToTeachersPage();
+
+    cy.get(".view_class").contains(new RegExp(`^${classname}$`)).click();
     cy.get('body').then($b => $b.find("#survey")).then($s => $s.length && $s.hide())
     cy.getBySel('customize_class_button').click();
     cy.get('#enable_level_5').parent('.switch').click();
     cy.getBySel('save_customizations').click();
 
-
-    loginForStudent('student5')
+    loginForStudent(students[0])
     goToHedyLevel2Page();
-    cy.get('#dropdown_level_button').click();
-    cy.get('#level_button_1').click();
-    cy.url().should('include', Cypress.env('hedy_page'));
 
     cy.get('#dropdown_level_button').click();
     cy.get('#level_button_5').should('be.disabled');
