6247a8469ee83e688f27918ce07547ff108c6c76
hasan-sh <hasansh343@gmail.com>
2023-11-29 17:39:57 +0100

    small changes and tests added

--- tests/cypress/e2e/for-teacher_page/customize-adventure_page/level_field.cy.js
+++ tests/cypress/e2e/for-teacher_page/customize-adventure_page/level_field.cy.js
@@ -1,18 +1,32 @@
 import {loginForTeacher} from '../../tools/login/login.js'
 import {goToEditAdventure} from '../../tools/navigation/nav.js'
 
-describe('Level Field test', () => {
-  for (const teacher of ["teacher1", "teacher4"]) { 
-    it(`passes: ${teacher}`, () => {
-      loginForTeacher(teacher);
-      goToEditAdventure();
+describe('Levels Dropdown Select test', () => {
+  const levels = ["3", "5"];
+  for (const level of levels) {
+    for (const teacher of ["teacher1", "teacher4"]) { 
+      it(`${teacher} can select level ${level}`, () => {
+        loginForTeacher(teacher);
+        goToEditAdventure();
 
-      // Tests level field interaction
-      cy.get('#custom_adventure_level')
-        .should('be.visible')
-        .should('not.be.disabled')
-        .select('1')
-        .should('have.value', '1');
-    })
+        // Tests level field interaction
+        cy.get('#custom_adventure_level')
+          .should('be.visible')
+          .should('not.be.disabled')
+          .click()
+        
+        cy.get("div[data-te-select-dropdown-ref]")
+          .should('be.visible')
+        
+        cy.get("div[role='option']")
+          .contains(level)
+          .click()
+        
+        cy.get("div[role='option']")
+          .contains(level)
+          .parent()
+          .should("have.attr", "data-te-select-selected")
+      })
+    }
   }
 })
