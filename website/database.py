f7e3be196d7c9cf14483e33856383eff3ea0551c
Hasan_sh <hasansh343@gmail.com>
2023-12-05 13:49:48 +0100

    [BUG] enable inviting second teachers to multiple classes at the same time (#4812)
    Fixes #4804 
    **Problem**
    1. The main issue spawns from having the _username_ as the partion_key for the invitations table, thereby preventing adding multiple invitations for the same user. 
    2. Also, even if this worked, users currently only see one invitation at the time; due to restricting messages to be 0 or 1. _This is now changed as well._
    3. There are a couple of things in the code that are inconsistent; e.g., https://github.com/hedyorg/hedy/blob/main/website/database.py#L737-L738 would remove _an invitation_ of the username, while the function name implies deleting an invitation to some class!
    
    **Solution**
    - create a unique, partition_key `username#class_id` in the INVITATIONS table.
    - with two independent indexes: `username` and `class_id`
    - allow second teachers (perhaps students later on as well!) to accept more than one invitation at the same time.
      - The reason we have messages in the session is mainly to display a number indicator in the profile dropdown menu. Logically, this should happen whenever teachers invite other users and these invitees do not have to reload the page to see the indicator, but now they do!
      - this should be fixed when we have realtime updates #4646!
    
    **How to test?**
    Follow the steps that Rico listed in #4804.

--- website/database.py
+++ website/database.py
@@ -45,7 +45,8 @@ CLASSES = dynamo.Table(storage, "classes", "id", indexes=[
 # - tags_id (str): id of tags that describe this adventure.
 ADVENTURES = dynamo.Table(storage, "adventures", "id", indexes=[dynamo.Index("creator"), dynamo.Index("public")])
 INVITATIONS = dynamo.Table(
-    storage, "class_invitations", partition_key="username", indexes=[dynamo.Index("class_id")]
+    storage, "invitations", partition_key="username#class_id",
+    indexes=[dynamo.Index("username"), dynamo.Index("class_id")],
 )
 
 """
@@ -372,14 +373,17 @@ class Database:
         """Forget the given user."""
         classes = USERS.get({"username": username}).get("classes") or []
         USERS.delete({"username": username})
-        INVITATIONS.delete({"username": username})
         # The recover password token may exist, so we delete it
         TOKENS.delete({"id": username})
         PROGRAMS.del_many({"username": username})
-
         # Remove user from classes of which they are a student
         for class_id in classes:
             self.remove_student_from_class(class_id, username)
+
+        # Remove existing invitations.
+        invitations = self.get_user_invitations(username)
+        for invite in invitations:
+            self.remove_user_class_invite(username, invite["class_id"])
 
         # Delete classes owned by the user
         for Class in self.get_teacher_classes(username, False):
@@ -740,16 +744,20 @@ class Database:
     def resolve_class_link(self, link_id):
         return CLASSES.get({"link": link_id})
 
-    def get_username_invite(self, username):
-        return INVITATIONS.get({"username": username}) or None
+    def get_user_class_invite(self, username, class_id):
+        return INVITATIONS.get({"username#class_id": f"{username}#{class_id}"}) or None
 
     def add_class_invite(self, data):
+        data['username#class_id'] = data['username'] + '#' + data['class_id']
         INVITATIONS.put(data)
 
-    def remove_class_invite(self, username):
-        INVITATIONS.delete({"username": username})
+    def remove_user_class_invite(self, username, class_id):
+        return INVITATIONS.delete({"username#class_id": f"{username}#{class_id}"})
 
-    def get_class_invites(self, class_id):
+    def get_user_invitations(self, username):
+        return INVITATIONS.get_many({"username": username}) or []
+
+    def get_class_invitations(self, class_id):
         return INVITATIONS.get_many({"class_id": class_id}) or []
 
     def all_classes(self):
