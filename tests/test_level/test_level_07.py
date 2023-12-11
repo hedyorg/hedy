b6d8d15d1c560716ad81eda8c374e3535c0c95aa
Toni Skulj <48225550+ToniSkulj@users.noreply.github.com>
2023-12-11 06:59:24 +0100

    [BUG] Fix broken tests (#4882)
    Fixes #4863.
    
    After #4838, the processing of the string was done after setting the input string on the source map.
    This led to 2 tests failing.
    
    This PR first uses process_input_string before setting the input on the source map. Fixing the failing tests.

--- tests/test_level/test_level_07.py
+++ tests/test_level/test_level_07.py
@@ -170,25 +170,24 @@ class TestsLevel7(HedyTester):
             exception=hedy.exceptions.IncompleteRepeatException
         )
 
-    # Disabled in 4838
-    # def test_repeat_with_missing_times_gives_error_skip(self):
-    #     code = textwrap.dedent("""\
-    #     x is 3
-    #     repeat 3 print 'x'""")
-    #
-    #     expected = textwrap.dedent("""\
-    #     x = '3'
-    #     pass""")
-    #
-    #     skipped_mappings = [
-    #         SkippedMapping(SourceRange(2, 1, 2, 17), hedy.exceptions.IncompleteRepeatException),
-    #     ]
-    #
-    #     self.single_level_tester(
-    #         code=code,
-    #         expected=expected,
-    #         skipped_mappings=skipped_mappings,
-    #     )
+    def test_repeat_with_missing_times_gives_error_skip(self):
+        code = textwrap.dedent("""\
+        x is 3
+        repeat 3 print 'x'""")
+
+        expected = textwrap.dedent("""\
+        x = '3'
+        pass""")
+
+        skipped_mappings = [
+            SkippedMapping(SourceRange(2, 1, 2, 19), hedy.exceptions.IncompleteRepeatException),
+        ]
+
+        self.single_level_tester(
+            code=code,
+            expected=expected,
+            skipped_mappings=skipped_mappings,
+        )
 
     def test_repeat_with_missing_print_gives_lonely_text_exc(self):
         code = textwrap.dedent("""\
