01c052d473ef5251de2a1df78b0be1ada0e7e47c
annelein <anneleinvanreijen@gmail.com>
2023-12-12 14:01:31 +0100

    cypress test remove button

--- tests/cypress/e2e/tools/adventures/adventure.js
+++ tests/cypress/e2e/tools/adventures/adventure.js
@@ -7,9 +7,6 @@ export function createAdventure()
     // Click 'Create new class' button
     cy.get('#create_adventure_button').click();
 
-    // Click 'Go back to teachers page' button
-    cy.get('#go_back_button').click();
-
     cy.wait(500);
 }
 
