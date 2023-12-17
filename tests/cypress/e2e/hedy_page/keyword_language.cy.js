a9372ef56bf6a7910b1b2f876719bc5e7d06fc71
Felienne Hermans <felienne@gmail.com>
2023-12-17 21:11:21 +0100

    switcher must go back and forth

--- tests/cypress/e2e/hedy_page/keyword_language.cy.js
+++ tests/cypress/e2e/hedy_page/keyword_language.cy.js
@@ -6,6 +6,12 @@ describe('when the user changes their language to Arabic', () => {
     cy.get('#editor').type("print Hallo!'\n");
     cy.getBySel('language-dropdown').click();
     cy.getBySel('switch-lang-ar').click();
+
+    // switch back and forth
+    cy.getBySel('kwlang-switch-btn').click();
+    cy.getBySel('kwlang-switch-toggle').click();
+    cy.getBySel('kwlang-switch-btn').click();
+    cy.getBySel('kwlang-switch-toggle').click();
   });
 
   it('initially has keywords in Arabic', () => {
