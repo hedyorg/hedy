4d1c527945c600e34b5729f5a15d03efc8b0372b
hasan-sh <hasansh343@gmail.com>
2023-12-19 12:16:26 +0100

    never return promise in before/afterEach hook

--- tests/cypress/e2e/for-teacher_page/customize-adventure_page/level_field.cy.js
+++ tests/cypress/e2e/for-teacher_page/customize-adventure_page/level_field.cy.js
@@ -1,34 +1,34 @@
 import {loginForTeacher} from '../../tools/login/login.js'
 import {goToEditAdventure} from '../../tools/navigation/nav.js'
 
-//describe('Levels Dropdown Select test', () => {
-//  const levels = ["3", "5"];
-//  for (const level of levels) {
-//    for (const teacher of ["teacher1", "teacher4"]) {
-//      it(`${teacher} can select level ${level}`, () => {
-//        loginForTeacher(teacher);
-//        goToEditAdventure();
-//
-//        // Tests level field interaction
-//        cy.get('#custom_adventure_levels_container')
-//          .should('be.visible')
-//          .should('not.be.disabled')
-//          .click()
-//
-//        cy.wait(400)
-//
-//        cy.get("div[data-te-select-dropdown-ref]")
-//          .should('be.visible')
-//
-//        cy.get("div[role='option']")
-//          .contains(level)
-//          .click()
-//
-//        cy.get("div[role='option']")
-//          .contains(level)
-//          .parent()
-//          .should("have.attr", "data-te-select-selected")
-//      })
-//    }
-//  }
-//})
+describe('Levels Dropdown Select test', () => {
+ const levels = ["3", "5"];
+ for (const level of levels) {
+   for (const teacher of ["teacher1", "teacher4"]) {
+     it(`${teacher} can select level ${level}`, () => {
+       loginForTeacher(teacher);
+       goToEditAdventure();
+
+       // Tests level field interaction
+       cy.get('#custom_adventure_levels_container')
+         .should('be.visible')
+         .should('not.be.disabled')
+         .click()
+
+       cy.wait(400)
+
+       cy.get("div[data-te-select-dropdown-ref]")
+         .should('be.visible')
+
+       cy.get("div[role='option']")
+         .contains(level)
+         .click()
+
+       cy.get("div[role='option']")
+         .contains(level)
+         .parent()
+         .should("have.attr", "data-te-select-selected")
+     })
+   }
+ }
+})
