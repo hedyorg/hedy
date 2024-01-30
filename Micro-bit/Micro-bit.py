cce79395df5a59cdf44db2fad6f272d397c9e089
Teodor Georgiev <t.georgiev@student.vu.nl>
2024-01-30 21:29:07 +0100

    Micro-bit code
    Created a sample MicroPython code
    and a script to flash it to Micro:Bit.

---
+++ Micro-bit/Micro-bit.py
@@ -0,0 +1,6 @@
+from microbit import *
+while True:
+    display.scroll('Hedy is the best! :)')
+    sleep(200)
+    display.show(Image.HEART)
+    sleep(2000)
---
+++ Micro-bit/hex-converter.py
@@ -0,0 +1,16 @@
+import uflash
+
+## The uflash module takes a Micro:bit script and converts it to a hex file(its not saved) and then flashes it to the Micro:bit automatically.
+## If there is no Micro:bit connected, it will give an error. This library is open source so i gues we can just use it.
+## it thakes the path to the python script as an argument and then converts it to a hex file and flashes it to the Micro:bit.
+## I managed to save the hex file to a specific location only if i run the code from the terminal(uflash Micro-bit/Micro-bit.py C:\Users\Teodor\Desktop\hex).
+## Saving the hex file to a specific location is not nessesary at the moment but if we want to emulate the Micro:bit we need it.
+
+def create_hex(input_file):
+    try:
+        uflash.flash(input_file)
+        print(f"Hex file created successfully:")
+    except Exception as e:
+        print(f"Error creating hex file: {e}")
+input_py_script = 'Micro-bit/Micro-bit.py'
+create_hex(input_py_script)
