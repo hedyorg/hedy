7d344695ad7589b0059de0f0b2f78e116381b421
hasan-sh <hasansh343@gmail.com>
2024-01-31 12:09:33 +0100

    added CKEditor instead of plain textarea (no highlighting in pre yet!)

--- static/js/adventure.ts
+++ static/js/adventure.ts
@@ -1,28 +1,92 @@
+// @ts-ignore
+// import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
+
 import ClassicEditor from "./ckeditor";
+import { CustomWindow } from './custom-window';
 
-const editor = document.querySelector('#adventure-editor') as HTMLElement;
+declare let window: CustomWindow;
 
-export function initialize_adv_editor(js: any) {
 
-    if (editor) {
-        ClassicEditor.create(editor, {
-            initialData: js.content,
-            language: js.lang,
-            
-            codeBlock: {
-                languages: [
-                    { language: 'python', label: 'Hedy', class: "hedy", },
-                ]
-            },
-        })
-        .then( editor => {
-            console.log( editor );
-            (window as any).ckEditor = editor;
-            // TODO: make sure to highlight Hedy code.
+let $editor: ClassicEditor;
+const editorContainer = document.querySelector('#adventure-editor') as HTMLElement;
+const lang = document.querySelector('html')?.getAttribute('lang') || 'en';
 
-        } )
-        .catch( error => {
-            console.error( error );
-        } );
-    }
+function initializeEditor(language: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if ($editor) {
+            $editor.destroy();
+        }
+
+        ClassicEditor
+            .create(editorContainer, {
+                codeBlock: {
+                    languages: [
+                        { language: 'python', label: 'Hedy', class: "language-python"},
+                    ],
+                },
+                language,
+            })
+            .then(editor => {
+                window.ckEditor = editor;
+                $editor = editor;
+                // hljs.highlightAll();
+
+                resolve();
+            })
+            .catch(error => {
+                console.error(error);
+                reject(error);
+            });
+    });
 }
+
+// Initialize the editor with the default language
+initializeEditor(lang);
+
+// Update the editor with new data
+// export async function update_adv_editor(js: any) {
+//     try {
+//         if (js.lang !== 'en') {
+//             await initializeEditor(js.lang);
+//         }
+//         $editor.setData(js.content);
+        
+        
+//         const prePattern = /<pre[^>]*>(.*?)<\/pre>/gs;
+//         const matches = js.content.match(prePattern);
+
+//         let content = "";
+//         if (matches) {
+//             for (const match of matches) {
+//                 // Extract content within <pre> tags
+//                 const contentMatch = /<pre[^>]*>(.*?)<\/pre>/s.exec(match);
+//                 if (contentMatch) {
+//                     content += contentMatch[1].trim();
+//                 }
+//             }
+//         }
+            
+//         console.log("content", content)
+
+        
+//         for (const codeBlock of document.querySelectorAll('.ck pre')) {
+//             console.log('a block here:', codeBlock)
+//             const exampleEditor = editorCreator.initializeReadOnlyEditor(codeBlock as HTMLElement, 'ltr');
+//             exampleEditor.contents = content.trimEnd();        
+//             for (const level of [1,2,3,4]) {
+//               initializeTranslation({
+//                 keywordLanguage: js.lang,
+//                 level: level,
+//               })
+//               exampleEditor.setHighlighterForLevel(level);                
+
+//             }
+//         }
+        
+
+
+//     } catch (error) {
+//         // Handle initialization error
+//         console.error(error);
+//     }
+// }
