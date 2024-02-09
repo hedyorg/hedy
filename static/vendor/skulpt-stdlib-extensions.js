70f3986315f7d040d56932175dcd8481a045aa84
Toni Skulj <48225550+ToniSkulj@users.noreply.github.com>
2024-02-09 13:02:07 +0100

    Change making of if_pressed_command
    - only for level 5

--- static/vendor/skulpt-stdlib-extensions.js
+++ static/vendor/skulpt-stdlib-extensions.js
@@ -1,7 +1,19 @@
 var $builtinmodule = function (name) {
     var mod = {};
+
     mod.clear = new Sk.builtin.func(function () {
         $('#output').text('');
     });
+
+    mod.if_pressed = new Sk.builtin.func(function (key, if_body, else_body) {
+        window.addEventListener("keydown", (event) => {
+          if (event.key === key){
+            sk.misceval.callOrSuspend(if_body);
+          } else {
+            sk.misceval.callOrSuspend(else_body);
+          }
+        }, { once: true });
+    });
+
     return mod;
 };
