86d194d9288350bddafdd9625f7b0b756dfea084
Mani Gudvardarson <m.gudvardarson@amsterdam.nl>
2024-02-09 15:52:14 +0100

    [CHORE] removed pygame dependencies from frontend if-pressed feature

--- static/vendor/skulpt-stdlib-extensions.js
+++ static/vendor/skulpt-stdlib-extensions.js
@@ -1,7 +1,31 @@
 var $builtinmodule = function (name) {
     var mod = {};
+
     mod.clear = new Sk.builtin.func(function () {
         $('#output').text('');
     });
+
+    function keyBoardInputPromise(key, if_body, else_body) {
+      $('#keybinding-modal').show();
+      return new Promise((resolve, reject) => {
+        window.addEventListener("keydown", (event) => {
+          if (event.key === key.v){
+            Sk.misceval.callOrSuspend(if_body);
+          } else {
+            Sk.misceval.callOrSuspend(else_body);
+          }
+
+          $('#keybinding-modal').hide();
+          resolve();
+        }, { once: true });
+      })
+    }
+
+    mod.if_pressed = new Sk.builtin.func(function (key, if_body, else_body) {
+        return new Sk.misceval.promiseToSuspension(keyBoardInputPromise(
+            key, if_body, else_body
+        ).then(() => Sk.builtin.none.none$));
+    });
+
     return mod;
 };
