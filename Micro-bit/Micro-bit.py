7452b9d8d41629fb790e0a47a7d1827d0c7038b7
Teodor Georgiev <t.georgiev@student.vu.nl>
2024-02-13 15:54:35 +0100

    commented unnecessary code

--- Micro-bit/Micro-bit.py
+++ Micro-bit/Micro-bit.py
@@ -1,6 +1,6 @@
-from microbit import *
-while True:
-    display.scroll('Hedy is the best! :)')
-    sleep(200)
-    display.show(Image.SAD)
-    sleep(2000)
+# from microbit import *
+# while True:
+#     display.scroll('Hedy is the best! :)')
+#     sleep(200)
+#     display.show(Image.SAD)
+#     sleep(2000)
