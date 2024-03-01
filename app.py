4e8dafaef05cd2e5f63487fa0f3ad20d2621bef5
annelein <anneleinvanreijen@gmail.com>
2024-03-01 11:30:22 +0100

    translations and correct link

--- app.py
+++ app.py
@@ -1992,9 +1992,11 @@ def main_page():
     custom_logo = False
     if os.path.isfile(f'static/images/hero-graphic/hero-graphic-{g.lang}.png'):
         custom_logo = True
+    
+    user = current_user()
 
     return render_template('main-page.html', page_title=gettext('title_start'), custom_logo=custom_logo,
-                           current_page='start', content=content)
+                           current_page='start', content=content, user=user)
 
 
 @app.route('/subscribe')
