3e4f8487894c080fd24378dad14d62ab2e0ba73a
rmagedon97 <77157289+rmagedon97@users.noreply.github.com>
2024-03-04 19:24:41 +0100

    Fixed for all levels
    the button works

--- Micro-bit/Micro-bit.py
+++ Micro-bit/Micro-bit.py
@@ -1,8 +1,3 @@
 from microbit import *
 while True:
-    display.scroll('Holdy is hard')
-    display.scroll('I am going to dye!!!')
-    display.scroll('send help')
-    display.scroll('i think its working')
-    display.scroll('does it')
-    display.scroll('???')
+    display.scroll('Helloo')
