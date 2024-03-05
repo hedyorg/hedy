3c2ac5506b5df860d56642e87e465344e4a6f775
hasan-sh <hasansh343@gmail.com>
2024-03-05 12:57:10 +0100

    if name given, change the default one

--- tests/cypress/e2e/tools/adventures/adventure.js
+++ tests/cypress/e2e/tools/adventures/adventure.js
@@ -1,11 +1,16 @@
 import { goToTeachersPage } from "../navigation/nav";
 
-export function createAdventure()
+export function createAdventure(name="")
 {
     goToTeachersPage();
 
     // Click 'Create new class' button
     cy.get('#create_adventure_button').click();
+
+    if (name) {
+        cy.get("#custom_adventure_name").clear().type(name);
+        cy.get("#save_adventure_button").click();
+    }
 
     cy.wait(500);
 }
