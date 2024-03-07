a7ecf238fc7cfba276f407cbd16cc6a78cb11d29
rmagedon97 <77157289+rmagedon97@users.noreply.github.com>
2024-03-07 20:25:32 +0100

    PEP 8

--- app.py
+++ app.py
@@ -1828,10 +1828,10 @@ def get_embedded_code_editor(level):
     return render_template("embedded-editor.html", fullWidth=fullWidth, run=run, language=language,
                            keyword_language=keyword_language, readOnly=readOnly,
                            level=level, javascript_page_options=dict(
-                                        page='view-program',
-                                        lang=language,
-                                        level=level,
-                                        code=program
+                             page='view-program',
+                             lang=language,
+                             level=level,
+                             code=program
                            ))
 
 
