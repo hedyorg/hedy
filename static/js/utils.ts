6bcbfe91a81b22b1506d030a8191cf64217541f4
Jesús Pelay <pelayjesus@gmail.com>
2024-03-20 17:56:25 -0400

    moving function to utils

--- static/js/utils.ts
+++ static/js/utils.ts
@@ -5,3 +5,20 @@ export function isLoggedIn() {
     }
     return false;
 }
+
+// convert an objet in a map
+export function convert(o:(object|undefined)) {
+    if (typeof o === 'object') {
+      let tmp:Map<string, object> = new Map(Object.entries(o));
+  
+      let ret:Map<string, (undefined|object)> = new Map();
+  
+      tmp.forEach((value, key) => {
+        ret.set(key, convert(value));
+      });
+  
+      return ret;
+    } else {
+      return o;
+    }
+  }
