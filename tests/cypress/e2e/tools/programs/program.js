d1ba0e80d50901e35fcaabe7668d829b8b0b04e7
Timon Bakker <timonbakker@hotmail.com>
2024-04-09 16:39:04 +0200

    Allowing unsetting of favourite program (#5151)
    Fixes #5132
    
    **How to test:**
    1. Login (have a public profile and a public program)
    2. Click on favourite a program and unfavourite the program
    
    
    https://github.com/hedyorg/hedy/assets/48122190/88bb0be2-16de-471b-ae10-92bfcaf07c55

--- tests/cypress/e2e/tools/programs/program.js
+++ tests/cypress/e2e/tools/programs/program.js
@@ -22,10 +22,15 @@ export function deleteProgram(name) {
     cy.get(".programs")
         .each(($program, i) => {
             if ($program.text().includes(name)) {
-                cy.get(`#more_options_${i+1}`).click();
-                cy.get(`#more_options_${i+1}`).should("be.visible");
-                cy.getBySel(`delete_non_submitted_program_${i+1}`).click();
-                cy.getBySel('modal_yes_button').click();
+                cy.get(`[data-name=${name}]`)
+                .first()
+                .then($el => {
+                    const programId = $el[0].getAttribute("data-id");
+                    cy.get(`#more_options_${programId}`).click();
+                    cy.get(`#more_options_${programId}`).should("be.visible");
+                    cy.getBySel(`delete_non_submitted_program_${programId}`).click();
+                    cy.getBySel('modal_yes_button').click();
+            })
             }
         })
 }
