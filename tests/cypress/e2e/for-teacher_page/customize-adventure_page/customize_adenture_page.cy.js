73bd05194fbe30ca57d21b37b30d8f97f91b52c0
Jesús Pelay <pelayjesus@gmail.com>
2024-04-11 09:38:49 -0400

    update tests

--- tests/cypress/e2e/for-teacher_page/customize-adventure_page/customize_adenture_page.cy.js
+++ tests/cypress/e2e/for-teacher_page/customize-adventure_page/customize_adenture_page.cy.js
@@ -9,14 +9,18 @@ describe('Test for the Customize Adventure Page', () => {
     })
     
     it('Writing an word with several possible keywords should show an alert message, that can be closed', () => {
-        cy.get('[data-cke-tooltip-text="Bold (Ctrl+B)"]').click()
-        cy.get('[data-cke-tooltip-text="Bold (Ctrl+B)"]').click()
-        cy.get('.ck-content[contenteditable=true]').realType('`to`')
-        // The warning should be shown now and contain the right suggestions
-        cy.get('#warnings_container').should('be.visible').and('contain.text', 'to, to_list')
-        // Once we click the close button
-        cy.get('p[class="close-dialog"]').click()
-        // The alert should not exist anymore
-        cy.getBySel('test_warning_message').should('not.exist')
+        cy.window().then(win => {
+            const editor = win.ckEditor;
+            cy.get('.ck-editor__editable')
+              .should('be.visible')
+              .should('not.be.disabled');
+            const data = "<code>to</code>";
+            editor.setData(data);
+            cy.get('#warnings_container').should('be.visible').and('contain.text', 'to, to_list')
+            // Once we click the close button
+            cy.get('p[class="close-dialog"]').click()
+            // The alert should not exist anymore
+            cy.getBySel('test_warning_message').should('not.exist')
+          })
     })   
 })
