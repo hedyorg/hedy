58d456a4c9a596e87564c850f679c1ef8f23801c
annelein <anneleinvanreijen@gmail.com>
2024-04-17 14:26:31 +0200

    splitting test to define and two its

--- tests/cypress/e2e/hedy_page/invalid_program.cy.js
+++ tests/cypress/e2e/hedy_page/invalid_program.cy.js
@@ -1,28 +1,37 @@
 import {goToHedyLevel5Page, goToHedyPage} from "../tools/navigation/nav";
 
 describe('Error code gives correct error', () => {
-    it('Misspelled Keyword', () => {
-        const error_code = "prnt Hello world!"
-        const error_code2 = "print Hello world!\nak Hello world?"
-        const error_message = `prnt is not a Hedy level 1 command. Did you mean print?`;
-        const error_message2 = `ak is not a Hedy level 1 command. Did you mean ask?`;
-        goToHedyPage();
+    describe('Misspelled Keyword', () => {
+        it('tests the print keyword', () => {
+            const error_code = "prnt Hello world!"
+            const error_message = `prnt is not a Hedy level 1 command. Did you mean print?`;
+            goToHedyPage();
 
-        cy.get('#editor > .cm-editor > .cm-scroller > .cm-content').click();
-        cy.focused().type(error_code);
+            cy.get('#editor > .cm-editor > .cm-scroller > .cm-content').click();
+            cy.focused().type(error_code);
 
-        cy.intercept('/parse').as('parse')
-        cy.get('#runit').click();
-        cy.wait('@parse')
-        cy.get('#errorbox').should('be.visible').should('contain', error_message);
+            cy.intercept('/parse').as('parse')
+            cy.get('#runit').click();
+            cy.wait('@parse')
+            cy.get('#errorbox').should('be.visible').should('contain', error_message);
+        })
 
-        cy.get('#editor > .cm-editor > .cm-scroller > .cm-content').clear();
-        cy.focused().type(error_code2);
-        cy.get('#runit').click();
-        cy.get('#errorbox').should('be.visible').should('contain', error_message2);
+        it('tests the ask keyword', () => {
+            const error_code = "print Hello world!\nak Hello world?"
+            const error_message = `ak is not a Hedy level 1 command. Did you mean ask?`;
+            goToHedyPage();
+
+            cy.get('#editor > .cm-editor > .cm-scroller > .cm-content').click();
+            cy.focused().type(error_code);
+
+            cy.intercept('/parse').as('parse')
+            cy.get('#runit').click();
+            cy.wait('@parse')
+            cy.get('#errorbox').should('be.visible').should('contain', error_message);
+        })
     })
 
-    it('Missing Command', () => {
+    describe('Missing Command', () => {
         const error_code = "hello world"
         const error_message = `It looks like you forgot to use a command on line 1.`;
         goToHedyPage();
@@ -36,7 +45,7 @@ describe('Error code gives correct error', () => {
         cy.get('#errorbox').should('be.visible').should('contain', error_message);
     })
 
-    it('Invalid Argument Type', () => {
+    idescribet('Invalid Argument Type', () => {
         const error_code = "forward lalala"
         const error_message = `You cannot use forward with lalala because it is text. Try changing lalala to a number or input from ask`;
         goToHedyPage();
@@ -50,7 +59,7 @@ describe('Error code gives correct error', () => {
         cy.get('#errorbox').should('be.visible').should('contain', error_message);
     })
 
-    it('Invalid Argument', () => {
+    describe('Invalid Argument', () => {
         const error_code = "turn test"
         const error_message = `You cannot use the command turn`;
         goToHedyPage();
