e556a45933b5821eebac46c66e8b67fd5bb26a0a
hasan-sh <hasansh343@gmail.com>
2024-04-23 10:38:08 +0200

    fix some ui and code + filtering by cat,page,username

--- website/feedback.py
+++ website/feedback.py
@@ -17,6 +17,10 @@ class FeedbackModule(WebsiteModule):
     def __init__(self, db: Database):
         super().__init__("feedback", __name__, url_prefix="/feedback")
         self.db = db
+        self.valid_categories = ["bug", "feedback", "feature"]
+
+    def is_valid_category(self, cat):
+        return cat in self.valid_categories
 
     @route("/", methods=["POST"])
     @requires_teacher
@@ -51,18 +55,32 @@ class FeedbackModule(WebsiteModule):
     @route("/", methods=["GET"])
     @requires_super_teacher
     def get_feedback(self, user):
+        category = request.args.get('category') or ""
+        if category and not self.is_valid_category(category):
+            return gettext('feedback_message_error'), 400
+        page = request.args.get('page') or ""
+        username = request.args.get('username') or ""
+        keys = {"category": category, "page": page, "username": username}
+        keys = {key: value for key, value in keys.items() if value}
+
         all_feedback = self.db.get_feedback()
-        category = request.args.get('category', default=None, type=str) or None
-        page = request.args.get('page', default=None, type=str) or None
-        user = request.args.get('user', default=None, type=str) or None
-        print('\n\n\n')
-        print(all_feedback)
         if not all_feedback:
             return render_template('feedback.html', feedback_by_category={})
+
         # Group feedback by category
         feedback_by_category = defaultdict(list)
+        categories = set()
+        users = set()
+        pages = set()
         for feedback in all_feedback:
-            print(feedback)
-            feedback_by_category[category].append(feedback)
+            # only if given filters are valid we add current feedback
+            if not keys or any([feedback.get(key) == value for key, value in keys.items()]):
+                feedback_by_category[feedback.get("category")].append(feedback)
+            categories.add(feedback.get("category"))
+            users.add(feedback.get("username"))
+            pages.add(feedback.get("page"))
 
-        return render_template('feedback.html', feedback_by_category=feedback_by_category)
+        return render_template('feedback.html', feedback_by_category=feedback_by_category,
+                               categories=categories,
+                               users=users,
+                               pages=pages,)
