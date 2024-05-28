9155a3a9e67440341a028cb82c76b84819863de8
Jesús Pelay <pelayjesus@gmail.com>
2024-05-28 12:32:09 -0400

    remove waits after login

--- tests/cypress/e2e/tools/login/login.js
+++ tests/cypress/e2e/tools/login/login.js
@@ -2,25 +2,22 @@ import { goToHome, goToLogin } from "../navigation/nav";
 
 export function loginForUser() {
     login("user1", "123456");
-    cy.wait(500);
 }
 
 export function loginForTeacher(username="teacher1") {
-    login(username, "123456");
-    cy.wait(500);
+    login(username, "123456");    
 }
 
 export function loginForStudent(student="student1") {
     login(student, "123456");
-    cy.wait(500);
 }
 
 export function loginForAdmin() {
     login("admin", "123456");
-    cy.wait(500);
 }
 
 export function login(username, password) {
+    cy.intercept('/auth/login').as('login')
     cy.clearCookies();
     cy.clearAllLocalStorage()
     cy.clearAllSessionStorage();
@@ -28,18 +25,18 @@ export function login(username, password) {
     cy.get('#username').type(username);
     cy.get('#password').type(password);
     cy.get('#login_button').click();
-    cy.wait(500);
+    cy.wait('@login');
 }
 
 export function logout()
 {
+    cy.intercept('/auth/logout').as('logout')
     goToHome();            
     cy.get('body').then($body => {
         if ($body.find(".menubar-text:contains('Log in')").length == 0) {
-            
             cy.get('.dropdown > .menubar-text').click();
             cy.get('#logout_button').click();
-            cy.wait(500);
+            cy.wait('@logout');
             
         } 
     });
