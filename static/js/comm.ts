67a513df1121b7e790f2e237fe8f8d289429ace7
Jesús Pelay <pelayjesus@gmail.com>
2024-06-07 12:40:58 -0400

    fix messages not being shown

--- static/js/comm.ts
+++ static/js/comm.ts
@@ -74,7 +74,7 @@ async function postJsonUsingFetch(url: string, data?: any): Promise<any> {
     });
   }
 
-  return response;
+  return response.json();
 }
 
 export function postNoResponse(url: string, data?: any): Promise<void> {
