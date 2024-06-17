5521ca5680784b85a43f79d94e7549fab09b6a10
annelein <anneleinvanreijen@gmail.com>
2024-06-17 10:53:09 +0200

    add tests for second teachers

--- tests/cypress/e2e/for-teacher_page/customize_class_page/preview_class.cy.js
+++ tests/cypress/e2e/for-teacher_page/customize_class_page/preview_class.cy.js
@@ -1,33 +1,36 @@
-import { createClass, openClassView, selectLevel } from '../../tools/classes/class.js';
+import { createClass, openClassView } from '../../tools/classes/class.js';
 import { loginForTeacher } from '../../tools/login/login.js'
 import { goToTeachersPage } from '../../tools/navigation/nav.js';
 
-// Do we also want this to be tested for a second_teacher?
-it('Is able to preview class', () => {
-    loginForTeacher();
-    // go to main hedy page in "normal mode"
-    cy.getDataCy('hedybutton').click();
-    cy.getDataCy('print_command').should("be.visible");
-    cy.getDataCy('ask_command').should("be.visible");
-    // assert that no preview_class_banner is shown right now
-    cy.getDataCy('preview_class_banner').should("not.exist");
+const teachers = ["teacher1", "teacher4"];
 
-    // create a custom class
-    let className = createClass();
-    goToTeachersPage();
-    openClassView(className);
-    cy.getDataCy('customize_class_button').click();
+teachers.forEach((teacher) => {
+    it(`${teacher } is able to preview class`, () => {
+        loginForTeacher(teacher);
+        // go to main hedy page in "normal mode"
+        cy.getDataCy('hedybutton').click();
+        cy.getDataCy('print_command').should("be.visible");
+        cy.getDataCy('ask_command').should("be.visible");
+        // assert that no preview_class_banner is shown right now
+        cy.getDataCy('preview_class_banner').should("not.exist");
 
-    cy.getDataCy('hide_adv_print_command').click();
-    cy.getDataCy('preview_class_link').click();
-    cy.getDataCy('preview_class_banner').should("be.visible");
-    cy.getDataCy('ask_command').should("be.visible");
-    // check that the print_command is absent
-    cy.getDataCy('print_command').should("not.exist");
-    cy.getDataCy('exit_preview_class_banner').click();
+        // create a custom class
+        let className = createClass();
+        goToTeachersPage();
+        openClassView(className);
+        cy.getDataCy('customize_class_button').click();
 
-    // we now expect the normal situation to be restored
-    cy.getDataCy('hedybutton').click();
-    cy.getDataCy('print_command');
-    cy.getDataCy('preview_class_banner').should("not.exist");
-})
+        cy.getDataCy('hide_adv_print_command').click();
+        cy.getDataCy('preview_class_link').click();
+        cy.getDataCy('preview_class_banner').should("be.visible");
+        cy.getDataCy('ask_command').should("be.visible");
+        // check that the print_command is absent
+        cy.getDataCy('print_command').should("not.exist");
+        cy.getDataCy('exit_preview_class_banner').click();
+
+        // we now expect the normal situation to be restored
+        cy.getDataCy('hedybutton').click();
+        cy.getDataCy('print_command');
+        cy.getDataCy('preview_class_banner').should("not.exist");
+    })
+});
--- tests/cypress/e2e/for-teacher_page/customize_class_page/quiz_parsons.cy.js
+++ tests/cypress/e2e/for-teacher_page/customize_class_page/quiz_parsons.cy.js
@@ -2,89 +2,92 @@ import { loginForStudent, loginForTeacher } from '../../tools/login/login.js'
 import { ensureClass, openClassView, removeCustomizations, selectLevel } from "../../tools/classes/class";
 import { goToHedyLevel2Page, goToHedyLevel5Page } from '../../tools/navigation/nav.js';
 
-// Do we also want this to be tested for a second_teacher?
-describe('customize class page', () => {
-    beforeEach(() => {
-      loginForTeacher();
-      ensureClass();
-      openClassView("CLASS1");
+const teachers = ["teacher1", "teacher4"];
 
-      // Remove any customizations that already exist to get the class into a predictable state
-      removeCustomizations();
-    });
+teachers.forEach((teacher) => {
+  describe(`customize class page - quiz parsons for ${teacher}`, () => {
+      beforeEach(() => {
+        loginForTeacher(teacher);
+        ensureClass();
+        openClassView("CLASS1");
 
-    it('Is able to remove the puzzle and quiz from level 2 and then add them back', () => {
-      selectLevel('2');
+        // Remove any customizations that already exist to get the class into a predictable state
+        removeCustomizations();
+      });
 
-      // remove the quiz
-      cy.getDataCy('hide_quiz').click();
-      cy.getDataCy('quiz').should("not.exist")
+      it('Is able to remove the puzzle and quiz from level 2 and then add them back', () => {
+        selectLevel('2');
 
-      // remove the puzzle
-      cy.getDataCy('hide_parsons').click();
-      cy.getDataCy('parsons').should("not.exist")
+        // remove the quiz
+        cy.getDataCy('hide_quiz').click();
+        cy.getDataCy('quiz').should("not.exist")
 
-      // add them from available list
-      cy.getDataCy('available_adventures_current_level').select("quiz")
-      cy.getDataCy('available_adventures_current_level').select("parsons")
+        // remove the puzzle
+        cy.getDataCy('hide_parsons').click();
+        cy.getDataCy('parsons').should("not.exist")
 
-      // Now the order should be quiz as last, then parsons.
-      cy.getDataCy('parsons').should("exist")
-      cy.getDataCy('quiz').last().should("exist");
+        // add them from available list
+        cy.getDataCy('available_adventures_current_level').select("quiz")
+        cy.getDataCy('available_adventures_current_level').select("parsons")
 
-      // make sure they are visible
-      loginForStudent();
-      goToHedyLevel2Page();
-      cy.getDataCy('quiz').scrollIntoView().should('be.visible');
-      cy.getDataCy('parsons').scrollIntoView().should('be.visible');
-    });
+        // Now the order should be quiz as last, then parsons.
+        cy.getDataCy('parsons').should("exist")
+        cy.getDataCy('quiz').last().should("exist");
 
-    it('Is able to disable all quizes and parsons', () => {
-      cy.intercept('/for-teachers/customize-class/*').as('updateCustomizations');      
+        // make sure they are visible
+        loginForStudent();
+        goToHedyLevel2Page();
+        cy.getDataCy('quiz').scrollIntoView().should('be.visible');
+        cy.getDataCy('parsons').scrollIntoView().should('be.visible');
+      });
 
-      cy.getDataCy('hide_quiz')
-          .should("not.be.checked")
-          .click()
+      it('Is able to disable all quizes and parsons', () => {
+        cy.intercept('/for-teachers/customize-class/*').as('updateCustomizations');      
 
-      cy.getDataCy('hide_quiz')
-          .should("be.checked")
+        cy.getDataCy('hide_quiz')
+            .should("not.be.checked")
+            .click()
 
-      cy.getDataCy('hide_parsons')
-          .should("not.be.checked")
-          .click()
+        cy.getDataCy('hide_quiz')
+            .should("be.checked")
 
-      cy.getDataCy('hide_parsons')
-          .should("be.checked")
+        cy.getDataCy('hide_parsons')
+            .should("not.be.checked")
+            .click()
 
-      cy.wait('@updateCustomizations');
+        cy.getDataCy('hide_parsons')
+            .should("be.checked")
 
-      selectLevel('3');
-      cy.getDataCy('level_3 quiz')
-        .should("not.exist")
+        cy.wait('@updateCustomizations');
 
-      cy.getDataCy('level_3 parsons')
-        .should("not.exist")
+        selectLevel('3');
+        cy.getDataCy('level_3 quiz')
+          .should("not.exist")
 
-      loginForStudent();
-      goToHedyLevel5Page();
-      cy.getDataCy('quiz').should('not.exist');
-      cy.getDataCy('parsons').should('not.exist');
-    });
+        cy.getDataCy('level_3 parsons')
+          .should("not.exist")
+
+        loginForStudent();
+        goToHedyLevel5Page();
+        cy.getDataCy('quiz').should('not.exist');
+        cy.getDataCy('parsons').should('not.exist');
+      });
 
 
-    it('Is able to hide the explore page', () => {
-      cy.intercept('/for-teachers/customize-class/*').as('updateCustomizations');      
+      it('Is able to hide the explore page', () => {
+        cy.intercept('/for-teachers/customize-class/*').as('updateCustomizations');      
 
-      cy.getDataCy('hide_explore')
-          .should("not.be.checked")
-          .click()
+        cy.getDataCy('hide_explore')
+            .should("not.be.checked")
+            .click()
 
-      cy.getDataCy('hide_explore')
-          .should("be.checked")
+        cy.getDataCy('hide_explore')
+            .should("be.checked")
 
-      cy.wait('@updateCustomizations');
-    
-      loginForStudent();
-      cy.getDataCy('explorebutton').should("not.exist")
-    });
+        cy.wait('@updateCustomizations');
+      
+        loginForStudent();
+        cy.getDataCy('explorebutton').should("not.exist")
+      });
+  });
 });
