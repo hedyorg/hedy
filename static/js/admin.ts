4ca493940dc3062033699fe79e1e79246beedc1e
Annelein <48122190+Annelein@users.noreply.github.com>
2024-06-29 02:54:35 +0200

    💻 Remove modal for make teacher as admin (#5631)
    As requested by @Felienne : remove modal for accepting teacher requests and remove reloading the page every time

--- static/js/admin.ts
+++ static/js/admin.ts
@@ -35,3 +35,34 @@ export function initializeAdminUserPage(_options: InitializeAdminUsersPageOption
       $('#filterform').submit();
   });
 }
+
+export function filter_admin() {
+    const params: Record<string, any> = {};
+  
+    const filter = $('#admin_filter_category').val();
+    params['filter'] = filter;
+  
+    if ($('#hidden_page_input').val()) {
+      params['page'] = $('#hidden_page_input').val();
+    }
+  
+    switch (filter) {
+      case 'email':
+      case 'username':
+        params['substring'] = $('#email_filter_input').val();
+        break;
+      case 'language':
+        params['language'] = $('#language_filter_input').val();
+        break;
+      case 'keyword_language':
+        params['keyword_language'] = $('#keyword_language_filter_input').val();
+        break;
+      default:
+        params['start'] = $('#admin_start_date').val();
+        params['end'] = $('#admin_end_date').val();
+        break;
+    }
+  
+    const queryString = Object.entries(params).map(([k, v]) => k + '=' + encodeURIComponent(v)).join('&');
+    window.open('?' + queryString, '_self');
+}
