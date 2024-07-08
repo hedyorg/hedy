7940681bc73a71e5c6fd6bce4c8f2f5e87ee1b26
annelein <anneleinvanreijen@gmail.com>
2024-07-08 14:39:16 +0200

    start buttons fix

--- tests/cypress/e2e/home_page/start_buttons.cy.js
+++ tests/cypress/e2e/home_page/start_buttons.cy.js
@@ -11,17 +11,20 @@ describe('Start buttons', () => {
     loginForTeacher();
     navigateHomeButton('start_learning_button', Cypress.env('hedy_page'))
     navigateHomeButton('start_teaching_button', Cypress.env('teachers_page'))
+    cy.getDataCy('teacher_mode_banner').should("not.exist");
   })
 
   it('As a student: Should be able to click on start buttons', () => {
     loginForStudent();
     navigateHomeButton('start_learning_button', Cypress.env('hedy_page'))
-    navigateHomeButton('start_teaching_button', Cypress.env('profile_page'))
+    navigateHomeButton('start_teaching_button', Cypress.env('teachers_page'))
+    cy.getDataCy('teacher_mode_banner').should("be.visible");
   })
 
   it('As a user: Should be able to click on start buttons', () => {
     loginForUser();
     navigateHomeButton('start_learning_button', Cypress.env('hedy_page'))
-    navigateHomeButton('start_teaching_button', Cypress.env('profile_page'))
+    navigateHomeButton('start_teaching_button', Cypress.env('teachers_page'))
+    cy.getDataCy('teacher_mode_banner').should("be.visible");
   })
 })
