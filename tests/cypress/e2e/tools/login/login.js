4db9ccde2b6b30a0cf62543154e6f10e31016074
Jesús Pelay <pelayjesus@gmail.com>
2024-07-17 12:02:49 -0400

    fix login cypress

--- tests/cypress/e2e/tools/login/login.js
+++ tests/cypress/e2e/tools/login/login.js
@@ -21,6 +21,7 @@ export function loginForAdmin() {
 }
 
 export function login(username, password) {
+    cy.intercept('/auth/login').as('login')
     cy.clearCookies();
     cy.clearAllLocalStorage()
     cy.clearAllSessionStorage();
@@ -28,14 +29,16 @@ export function login(username, password) {
     cy.getDataCy('username').type(username);
     cy.getDataCy('password').type(password);
     cy.getDataCy('login_button').click();
-    cy.wait(500);
+    cy.wait('@login');
 }
 
 export function logout()
 {
-    goToHome();            
+    cy.intercept('/auth/logout').as('logout')    
+    goToHome();
     cy.getDataCy('user_dropdown').click()
     cy.getDataCy('logout_button').click()
+    cy.wait('@logout')
 }
 
 export default {loginForUser};
