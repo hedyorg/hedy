9165cf116f025aa7ce343653bb868b771e6b2ca4
annelein <anneleinvanreijen@gmail.com>
2024-07-31 17:14:08 +0200

    enforce developers switch test

--- tests/cypress/e2e/hedy_page/developers_mode_switch.cy.js
+++ tests/cypress/e2e/hedy_page/developers_mode_switch.cy.js
@@ -5,41 +5,27 @@ import { createClassAndAddStudents, navigateToClass } from '../tools/classes/cla
 let classname;
 let students;
 
-describe('Developers mode', () => {
-    beforeEach(() => {
-      loginForTeacher();
-    })
-    it('Should toggle on and off', () => {
-      goToHedyPage();
-      
-      cy.get('#toggle_circle').click(); // Developers mode is switched on
-      cy.get('#adventures').should('not.be.visible');
+it('Should be able to enforce developer mode as a teacher', () => {
+  loginForTeacher();
+  cy.intercept('/for-teachers/customize-class/*').as('updateCustomizations');      
 
-      cy.get('#toggle_circle').click(); // Developers mode is switched off
-      cy.get('#adventures').should('be.visible');
-    })
+  ({classname, students} = createClassAndAddStudents());
+  navigateToClass(classname);
 
-    it('Should be enforced developer mode', () => {
-      cy.intercept('/for-teachers/customize-class/*').as('updateCustomizations');      
+  cy.getDataCy('customize_class_button').click();
+  cy.getDataCy('developers_mode')
+    .should("not.be.checked")
+    .click()
 
-      ({classname, students} = createClassAndAddStudents());
-      navigateToClass(classname);
+  cy.getDataCy('developers_mode')
+    .should("be.checked")
+  
+  cy.wait(1000)
+  cy.wait('@updateCustomizations').should('have.nested.property', 'response.statusCode', 200);
 
-      cy.getDataCy('customize_class_button').click();
-      cy.get("#developers_mode")
-        .should("not.be.checked")
-        .click()
-
-      cy.get("#developers_mode")
-        .should("be.checked")
-      
-      cy.wait(1000)
-      cy.wait('@updateCustomizations').should('have.nested.property', 'response.statusCode', 200);
-
-      logout();
-      loginForStudent(students[0]);
-      goToHedyPage();
-      
-      cy.get('#adventures').should('not.be.visible');
-    })
+  logout();
+  loginForStudent(students[0]);
+  goToHedyPage();
+  
+  cy.getDataCy('adventures_tab').should('not.exist');
 })
