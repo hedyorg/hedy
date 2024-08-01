4d52d27ffcfd6abb8b46724b8e9e950cab8d103c
annelein <anneleinvanreijen@gmail.com>
2024-08-01 16:43:01 +0200

    fix for programs_page test

--- tests/cypress/e2e/public_programs/programs_page.cy.js
+++ tests/cypress/e2e/public_programs/programs_page.cy.js
@@ -156,44 +156,44 @@ describe("General tests for my programs page (with both custom teacher and built
             // After selecting level 2 only the programs from level 2 should ve visible
             cy.getDataCy('levels_dropdown').select('2')
     
-            cy.getDataCy('program_3e8926c0515d47a5aeb116164b1278c9').should('be.visible')
-            cy.getDataCy('program_195d94e733ff49b08079848409e664b6').should('be.visible')
+            cy.get('#program_3e8926c0515d47a5aeb116164b1278c9').should('be.visible')
+            cy.get('#program_195d94e733ff49b08079848409e664b6').should('be.visible')
 
-            cy.getDataCy('program_e1d94726655947c5b0309abb18cc17ca').should('not.exist')
-            cy.getDataCy('program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('not.exist')
+            cy.get('#program_e1d94726655947c5b0309abb18cc17ca').should('not.exist')
+            cy.get('#program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('not.exist')
     
             // After selecting level 1 only the programs from level 1 should ve visible
             cy.getDataCy('levels_dropdown').select('1')
+
+            cy.get('#program_3e8926c0515d47a5aeb116164b1278c9').should('not.exist')
+            cy.get('#program_195d94e733ff49b08079848409e664b6').should('not.exist')
     
-            cy.getDataCy('program_3e8926c0515d47a5aeb116164b1278c9').should('not.exist')
-            cy.getDataCy('program_195d94e733ff49b08079848409e664b6').should('not.exist')
-    
-            cy.getDataCy('program_e1d94726655947c5b0309abb18cc17ca').should('be.visible')
-            cy.getDataCy('program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible')
+            cy.get('#program_e1d94726655947c5b0309abb18cc17ca').should('be.visible')
+            cy.get('#program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible')
     
             // Selecting the - Level - options should show every program
             cy.getDataCy('levels_dropdown').select(0);
-            cy.getDataCy('program_3e8926c0515d47a5aeb116164b1278c9').should('be.visible')
-            cy.getDataCy('program_195d94e733ff49b08079848409e664b6').should('be.visible')
+            cy.get('#program_3e8926c0515d47a5aeb116164b1278c9').should('be.visible')
+            cy.get('#program_195d94e733ff49b08079848409e664b6').should('be.visible')
 
-            cy.getDataCy('program_e1d94726655947c5b0309abb18cc17ca').should('be.visible')
-            cy.getDataCy('program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible')
+            cy.get('#program_e1d94726655947c5b0309abb18cc17ca').should('be.visible')
+            cy.get('#program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible')
         })
     
         it('The adventure filter show the appropiate programs', () => {
             cy.getDataCy('adventure_select').select('ask')
-            cy.getDataCy('program_e1d94726655947c5b0309abb18cc17ca').should('be.visible');
+            cy.get('#program_e1d94726655947c5b0309abb18cc17ca').should('be.visible');
             cy.wait(300)
             cy.getDataCy('adventure_select').select('print')
-            cy.getDataCy('program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible');
+            cy.get('#program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible');
     
             // Selecting the - Adventure - options should show every program
             cy.getDataCy('adventure_select').select(0);
-            cy.getDataCy('program_3e8926c0515d47a5aeb116164b1278c9').should('be.visible')
-            cy.getDataCy('program_195d94e733ff49b08079848409e664b6').should('be.visible')
+            cy.get('#program_3e8926c0515d47a5aeb116164b1278c9').should('be.visible')
+            cy.get('#program_195d94e733ff49b08079848409e664b6').should('be.visible')
          
-            cy.getDataCy('program_e1d94726655947c5b0309abb18cc17ca').should('be.visible')
-            cy.getDataCy('program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible')
+            cy.get('#program_e1d94726655947c5b0309abb18cc17ca').should('be.visible')
+            cy.get('#program_4c426ff4cd5a40d7bb65bfbb35907f8b').should('be.visible')
         })
     })
 
