619c2ffd537273e1b5fd7ab1caaedfd2e61a6166
Jesús Pelay <pelayjesus@gmail.com>
2024-12-02 14:30:15 -0400

    stop using english as bridge when translating

--- app.py
+++ app.py
@@ -1901,11 +1901,9 @@ def view_program(user, id):
     source_language = result.get("lang")
     target_language = g.keyword_lang
     if source_language != target_language and source_language in ALL_KEYWORD_LANGUAGES.keys():
-        try:
-            english_code = hedy_translation.translate_keywords(
-                code, from_lang=source_language, to_lang="en", level=level)
+        try:            
             code = hedy_translation.translate_keywords(
-                english_code, from_lang="en", to_lang=target_language, level=level)
+                code, from_lang=source_language, to_lang=target_language, level=level)
         except Exception as e:
             # Not really a good place to leave this error, but at least we don't
             # want it crashing the page load. Log it as a warning then.
