a33cf9aeebe01ab2dade8dfe3d7278657f9076e7
Rico Hermans <rix0rrr@gmail.com>
2024-12-19 09:10:38 +0100

    Leave me alone

--- website/html_validation.py
+++ website/html_validation.py
@@ -12,4 +12,4 @@ ID_RE = re.compile('id="([^"]+)"')
 def validate_no_duplicate_ids(html):
     ids = Counter(ID_RE.findall(html))
     duplicates = [id for id, count in ids.items() if count > 1]
-    assert not duplicates, f'There are multiple HTML elements with the same id="..." on this page. IDs should be unique: {", ".join(duplicates)}'
+    assert not duplicates, f'There are multiple HTML elements with the same id="..." on this page. IDs should be unique: {", ".join(duplicates)}' # noqa: E501
