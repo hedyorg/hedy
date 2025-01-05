aa535cdfd4224d3f7075ea63274f674367813715
Rico Hermans <rix0rrr@gmail.com>
2025-01-05 21:56:39 +0100

    Wait this can be simpler

--- tests/cypress/e2e/tools/lezer/lezer_tester.js
+++ tests/cypress/e2e/tools/lezer/lezer_tester.js
@@ -1,32 +1,11 @@
 import { testTree } from "@lezer/generator/dist/test"
 import { PARSER_FACTORIES } from '../../../../../static/js/lezer-parsers/language-packages';
 
-const parsers = {
-    1: PARSER_FACTORIES[1],
-    2: PARSER_FACTORIES[2],
-    3: PARSER_FACTORIES[3],
-    4: PARSER_FACTORIES[4],
-    5: PARSER_FACTORIES[5],
-    6: PARSER_FACTORIES[6],
-    7: PARSER_FACTORIES[7],
-    8: PARSER_FACTORIES[8],
-    9: PARSER_FACTORIES[9],
-    10: PARSER_FACTORIES[10],
-    11: PARSER_FACTORIES[11],
-    12: PARSER_FACTORIES[12],
-    13: PARSER_FACTORIES[13],
-    14: PARSER_FACTORIES[14],
-    15: PARSER_FACTORIES[15],
-    16: PARSER_FACTORIES[16],
-    17: PARSER_FACTORIES[17],
-    18: PARSER_FACTORIES[18],
-};
-
 export function multiLevelTester(testName, code, expectedTree, from, to, language = 'en') {
 
     for (let i = from; i <= to; i++) {
         it(`${testName} for level ${i}`, () => {
-            const parser = parsers[i](language);
+            const parser = PARSER_FACTORIES[i](language);
             testTree(parser.parse(code), expectedTree);
         })
     }
@@ -34,7 +13,7 @@ export function multiLevelTester(testName, code, expectedTree, from, to, languag
 
 export function singleLevelTester(testName, code, expectedTree, level, language = 'en') {
     it(testName, () => {
-        const parser = parsers[level](language);
+        const parser = PARSER_FACTORIES[level](language);
         testTree(parser.parse(code), expectedTree);
     })
 }
