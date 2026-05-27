7a2f12ce1ab2bbddc1c5572220043e62339fa8ac
vamshikrishnaramasamy <vamshikrishnaramasamy@gmail.com>
2026-05-26 20:11:28 -0700

    💻 Remove preview teacher mode and update landing page buttons

--- tests/cypress/e2e/home_page/start_buttons.cy.js
+++ tests/cypress/e2e/home_page/start_buttons.cy.js
@@ -2,29 +2,26 @@ import { loginForStudent, loginForTeacher, loginForUser } from "../tools/login/l
 import { navigateHomeButton } from "../tools/navigation/nav";
 
 describe('Start buttons', () => {
-  it('When not logged in: Should be able to click on start buttons', () => {
+  it('When not logged in: start_learning goes to hedy, start_teaching goes to signup', () => {
     navigateHomeButton('start_learning_button', Cypress.env('hedy_page'))
-    // start_teaching button is tested in teacher_mode.cy.js
+    navigateHomeButton('start_teaching_button', Cypress.env('signup_page'))
   })
 
-  it('As a teacher: Should be able to click on start buttons', () => {
+  it('As a teacher: start_learning goes to hedy, start_teaching goes to teachers page', () => {
     loginForTeacher();
     navigateHomeButton('start_learning_button', Cypress.env('hedy_page'))
     navigateHomeButton('start_teaching_button', Cypress.env('teachers_page'))
-    cy.getDataCy('teacher_mode_banner').should("not.exist");
   })
 
-  it('As a student: Should be able to click on start buttons', () => {
+  it('As a student: start_learning goes to hedy, start_teaching goes to signup', () => {
     loginForStudent();
     navigateHomeButton('start_learning_button', Cypress.env('hedy_page'))
-    navigateHomeButton('start_teaching_button', Cypress.env('teachers_page'))
-    cy.getDataCy('teacher_mode_banner').should("be.visible");
+    navigateHomeButton('start_teaching_button', Cypress.env('signup_page'))
   })
 
-  it('As a user: Should be able to click on start buttons', () => {
+  it('As a user: start_learning goes to hedy, start_teaching goes to signup', () => {
     loginForUser();
     navigateHomeButton('start_learning_button', Cypress.env('hedy_page'))
-    navigateHomeButton('start_teaching_button', Cypress.env('teachers_page'))
-    cy.getDataCy('teacher_mode_banner').should("be.visible");
+    navigateHomeButton('start_teaching_button', Cypress.env('signup_page'))
   })
 })
